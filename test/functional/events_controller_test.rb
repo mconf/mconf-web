df69c8624e4870daabf1d4ec0538f8c6c42eeb47
Enrique Barra Arias <ebarra@dit.upm.es>
2008-02-28 10:24:15 +0000

    cambios de kike
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@12905 3f47a204-8049-4147-aab0-d444c64e7564

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -3,10 +3,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 include AuthenticatedTestHelper
 
 class EventsControllerTest < ActionController::TestCase
-   fixtures :events, :machines, :participants, :users
+  
   #A non authenticated user should be redirected to create a new session and the message is "Please log in"
   def test_get_index_should_be_redirected
-    
     get :index
     assert_redirected_to :controller => "sessions", :action => "new"
     assert_equal "Please log in", flash[:notice]
--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -2,114 +2,21 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class EventTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  fixtures :events, :machines, :participants, :users, :event_datetimes
+  fixtures :events, :machines, :participants
   
   def test_name
    evento = events(:event_00038 )
     assert evento.valid?
     #assert_equal "should expecify a name", evento.errors.on(:name)
   end
-  def test_if_a_machines_is_in_an_event_simple
+  def test_if_a_machines_is_in_an_event
     evento = events(:event_00038 )    
     trapo = machines(:machine_00005).id    
     assert evento.uses_participant(trapo)
   end
-  def test_if_a_machines_is_in_an_event_complejo
-    evento = events(:event_00040 )    
-    trapo = machines(:machine_00005).id 
-    golpe = machines(:machine_00008).id
-    traste = machines(:machine_00009).id
-    assert evento.uses_participant(trapo)
-    assert evento.uses_participant(golpe)
-    assert evento.uses_participant(traste)
-  end
   
   def test_if_many_machines_are_in_an_event
-    evento = events(:event_00040 )  
-    trapo = machines(:machine_00005).id
-    golpe = machines(:machine_00008).id
-    traste = machines(:machine_00009).id
-    array_participants_this_event = []
-    array_participants_this_event << trapo
-    array_participants_this_event << golpe
-    array_participants_this_event << traste
-    array_participants_this_event << "54"
-    array_participants_this_event << "56"
-    coincidences = evento.contains_participants(array_participants_this_event)
-    assert_kind_of(Array, coincidences)
-    #assert_include(trapo, coincidences)
-    #assert_include golpe, coincidences
-    #assert_include traste, coincidences
-    assert_not_nil coincidences
-    assert_equal 5, coincidences[0]
-     assert_equal 8, coincidences[1]
-      assert_equal 9, coincidences[2]
-      assert coincidences.include?(5)
-      assert coincidences.include?(8)
-      assert coincidences.include?(9)
-  end
-  def test_get_participants_simple
     evento = events(:event_00038 )  
-    trapo = machines(:machine_00005).name
-    participants = evento.get_participants
-    assert_not_nil participants
-    assert_equal trapo,participants
-    
   end
-  
-  def test_get_participants_complejo
-   evento = events(:event_00040 )  
-    trapo = machines(:machine_00005).name
-    golpe = machines(:machine_00008).name
-    traste = machines(:machine_00009).name
-    participants = evento.get_participants
-    assert_not_nil participants
-    assert_equal trapo +" "+ golpe +" "+ traste,participants
-  end
-  
-  def test_get_url_simple
-    evento = events(:event_00038 )  
-    trapo = machines(:machine_00005).nickname
-    url = []
-    url[0]= "isabel://"+ trapo
-    urls = evento.get_urls
-     assert_not_nil urls
-    assert_equal url, urls
-  end
-  
-    def test_get_url_complejo
-      evento = events(:event_00040 )  
-    trapo = machines(:machine_00005).nickname
-     golpe = machines(:machine_00008).nickname
-    traste = machines(:machine_00009).nickname
-    url = []
-    url[0]= "isabel://"+ trapo
-     url[1]= "isabel://"+ golpe
-      url[2]= "isabel://"+ traste
-       urls = evento.get_urls
-       assert_not_nil urls
-    assert_equal url, urls
-  end
-  
-  def test_overlaps_with_event
-    evento = events(:event_00040 ) 
-    evento2 = events(:event_00042 ) 
-    evento3 = events(:event_00041 )
-    even = []
-    even[0]= evento2
-    even[1]= evento3
-    even2 = []
-    even2[0] = evento3
-    
-    assert evento.overlaps_with_event_in_array(even)
-    #assert evento.overlaps_with_event_in_array(even2)
-    
-  end
-  
-  def test_has_any_session_in_the_past
-    evento = events(:event_00040 ) 
-    assert evento.has_any_session_in_the_past
-  end
-  
   
 end
