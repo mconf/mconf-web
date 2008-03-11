dc4f67a199ddc7ab4c9455d59546f4302edb485e
Alfredo Solano Rodrigo <alsolano@dit.upm.es>
2008-03-11 14:35:37 +0000

    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@12999 3f47a204-8049-4147-aab0-d444c64e7564

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -41,15 +41,7 @@ class UserTest < Test::Unit::TestCase
     end
   end
 
-  def test_should_reset_password
-    users(:quentin).update_attributes(:password => 'new password', :password_confirmation => 'new password')
-    assert_equal users(:quentin), User.authenticate('quentin', 'new password')
-  end
-
-  def test_should_not_rehash_password
-    users(:quentin).update_attributes(:login => 'quentin2')
-    assert_equal users(:quentin), User.authenticate('quentin2', 'test')
-  end
+  
 
   def test_should_authenticate_user
     assert_equal users(:quentin), User.authenticate('quentin', 'test')
