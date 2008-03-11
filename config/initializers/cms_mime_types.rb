249e2fbcd2e87081e1d88fec2c4192a05f3ba56d
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-03-11 14:13:15 +0000

    Movido cms_mime_types.rb a mime_types.rb. El primero ya no es necesario. El CMSplugin carga el segundo para saber los mimes a los que tiene que contestar
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@12995 3f47a204-8049-4147-aab0-d444c64e7564

--- config/initializers/cms_mime_types.rb
+++
@@ -1,7 +0,0 @@
-# CMS Mime Types
-# TODO? Merge with Rails config/initializers/mime_types.rb
-
-Mime::Type.register "application/atomsvc+xml", :atomsvc
-Mime::Type.register "application/atomcat+xml", :atomcat
-Mime::Type.register "application/xrds+xml",    :xrds
-
---
+++ config/initializers/mime_types.rb
@@ -0,0 +1,5 @@
+# Mime Types
+#Mime::Type.register "application/atomsvc+xml", :atomsvc
+#Mime::Type.register "application/atomcat+xml", :atomcat
+#Mime::Type.register "application/xrds+xml",    :xrds
+
