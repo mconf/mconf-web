66e00d2d571020191b8969009b6ec912e785e934
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-04-03 15:15:08 +0000

    Resolver bug, la aplicación cascaba después de la activación del usuario
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@13143 3f47a204-8049-4147-aab0-d444c64e7564

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -4,7 +4,6 @@ class UserObserver < ActiveRecord::Observer
   end
 
   def after_save(user)
-    Notifier.deliver_activation(user) if user.pending?
     Notifier.deliver_forgot_password(user) if user.recently_forgot_password?
     Notifier.deliver_reset_password(user) if user.recently_reset_password?
   end
