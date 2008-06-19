680135989d478a98e44931be2c265d18af4b807d
Alfredo Solano Rodrigo <alsolano@dit.upm.es>
2008-06-19 15:07:38 +0000

    schema
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@13855 3f47a204-8049-4147-aab0-d444c64e7564

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 34) do
+ActiveRecord::Schema.define(:version => 35) do
 
   create_table "cms_categories", :force => true do |t|
     t.string   "name"
@@ -87,12 +87,12 @@ ActiveRecord::Schema.define(:version => 34) do
   end
 
   create_table "events", :force => true do |t|
-    t.string "name",        :limit => 40, :null => false
-    t.string "password",    :limit => 40, :null => false
-    t.string "service",     :limit => 40, :null => false
-    t.string "quality",     :limit => 8,  :null => false
+    t.string "name",        :limit => 40, :default => "", :null => false
+    t.string "password",    :limit => 40, :default => "", :null => false
+    t.string "service",     :limit => 40, :default => "", :null => false
+    t.string "quality",     :limit => 8,  :default => "", :null => false
     t.text   "description"
-    t.string "uri",         :limit => 80, :null => false
+    t.string "uri",         :limit => 80, :default => "", :null => false
   end
 
   create_table "events_users", :id => false, :force => true do |t|
@@ -131,10 +131,10 @@ ActiveRecord::Schema.define(:version => 34) do
     t.string  "scope",                 :limit => 1
   end
 
-  add_index "globalize_languages", ["rfc_3066"], :name => "index_globalize_languages_on_rfc_3066"
-  add_index "globalize_languages", ["iso_639_3"], :name => "index_globalize_languages_on_iso_639_3"
-  add_index "globalize_languages", ["iso_639_2"], :name => "index_globalize_languages_on_iso_639_2"
   add_index "globalize_languages", ["iso_639_1"], :name => "index_globalize_languages_on_iso_639_1"
+  add_index "globalize_languages", ["iso_639_2"], :name => "index_globalize_languages_on_iso_639_2"
+  add_index "globalize_languages", ["iso_639_3"], :name => "index_globalize_languages_on_iso_639_3"
+  add_index "globalize_languages", ["rfc_3066"], :name => "index_globalize_languages_on_rfc_3066"
 
   create_table "globalize_translations", :force => true do |t|
     t.string  "type"
@@ -149,12 +149,21 @@ ActiveRecord::Schema.define(:version => 34) do
     t.string  "namespace"
   end
 
-  add_index "globalize_translations", ["table_name", "item_id", "language_id"], :name => "globalize_translations_table_name_and_item_and_language"
   add_index "globalize_translations", ["tr_key", "language_id"], :name => "index_globalize_translations_on_tr_key_and_language_id"
+  add_index "globalize_translations", ["table_name", "item_id", "language_id"], :name => "globalize_translations_table_name_and_item_and_language"
+
+  create_table "invitations", :force => true do |t|
+    t.string   "email"
+    t.integer  "space_id"
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "machines", :force => true do |t|
-    t.string "name",     :limit => 40, :null => false
-    t.string "nickname", :limit => 40, :null => false
+    t.string "name",     :limit => 40, :default => "", :null => false
+    t.string "nickname", :limit => 40, :default => "", :null => false
   end
 
   create_table "machines_users", :id => false, :force => true do |t|
@@ -172,9 +181,9 @@ ActiveRecord::Schema.define(:version => 34) do
   end
 
   create_table "open_id_nonces", :force => true do |t|
-    t.string  "server_url", :null => false
-    t.integer "timestamp",  :null => false
-    t.string  "salt",       :null => false
+    t.string  "server_url", :default => "", :null => false
+    t.integer "timestamp",                  :null => false
+    t.string  "salt",       :default => "", :null => false
   end
 
   create_table "open_id_ownings", :force => true do |t|
@@ -184,12 +193,12 @@ ActiveRecord::Schema.define(:version => 34) do
   end
 
   create_table "participants", :force => true do |t|
-    t.integer "event_id",                                             :null => false
-    t.integer "machine_id",                                           :null => false
-    t.integer "machine_id_connected_to",                              :null => false
-    t.string  "role",                    :limit => 40,                :null => false
-    t.integer "fec",                     :limit => 2,  :default => 0, :null => false
-    t.integer "radiate_multicast",       :limit => 1,  :default => 0, :null => false
+    t.integer "event_id",                                              :null => false
+    t.integer "machine_id",                                            :null => false
+    t.integer "machine_id_connected_to",                               :null => false
+    t.string  "role",                    :limit => 40, :default => "", :null => false
+    t.integer "fec",                     :limit => 2,  :default => 0,  :null => false
+    t.integer "radiate_multicast",       :limit => 1,  :default => 0,  :null => false
     t.text    "description"
   end
 
@@ -225,15 +234,15 @@ ActiveRecord::Schema.define(:version => 34) do
   end
 
   create_table "taggings", :force => true do |t|
-    t.integer "tag_id",        :null => false
-    t.integer "taggable_id",   :null => false
-    t.string  "taggable_type", :null => false
+    t.integer "tag_id",                        :null => false
+    t.integer "taggable_id",                   :null => false
+    t.string  "taggable_type", :default => "", :null => false
   end
 
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type"], :name => "index_taggings_on_tag_id_and_taggable_id_and_taggable_type", :unique => true
 
   create_table "tags", :force => true do |t|
-    t.string "name", :null => false
+    t.string "name", :default => "", :null => false
   end
 
   add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
