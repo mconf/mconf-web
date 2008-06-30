37ddf03a0fbfddb3eef7255c619deab044edb005
Enrique Barra Arias <ebarra@dit.upm.es>
2008-06-30 13:11:16 +0000

    migraciones
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@13892 3f47a204-8049-4147-aab0-d444c64e7564

---
+++ db/migrate/036_load_data.rb
@@ -0,0 +1,19 @@
+require 'active_record/fixtures'
+
+class LoadData < ActiveRecord::Migration
+  def self.up
+    directory = File.join(File.dirname(__FILE__), "data")
+    Fixtures.create_fixtures(directory, "users")
+    Fixtures.create_fixtures(directory, "cms_roles")
+    Fixtures.create_fixtures(directory, "spaces")
+  end
+
+  def self.down
+    User.find_by_login("admin").destroy
+    Role.delete_all
+    if Space.find_by_id(0)
+      Space.find_by_id(0).destroy
+    end
+  end
+  
+end
