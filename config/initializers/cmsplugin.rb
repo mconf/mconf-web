78496f4b1ca2f4365a0dea16a387960fa0fcb690
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-10-22 12:24:50 +0000

    Move CMSplugin modifications to config/initializers/cmsplugin.rb
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@14882 3f47a204-8049-4147-aab0-d444c64e7564

--- config/initializers/cmsplugin.rb
+++ config/initializers/cmsplugin.rb
@@ -1,3 +1,53 @@
-require "#{ RAILS_ROOT }/app/models/anonymous_agent"
-require "#{ RAILS_ROOT }/app/models/entry"
+# Modifications of CMSplugin
+#
+# Load the class first, then add modifications to it
+
+Entry
+
+class Entry
+  acts_as_tree :order => "title"
+  acts_as_taggable
+
+  before_destroy { |entry| 
+    entry.children.map { |entry_children|  entry_children.content.destroy}
+  }
+
+  after_create { |entry|
+    if entry.parent_id != nil
+      entry.parent.update_attribute(:updated_at , Time.now)
+   end
+  }
+
+  after_update { |entry| 
+    entry.children.map { |entry_children| 
+      entry_children.update_attribute(:public_read, entry.public_read) 
+    } 
+  }
+end
+
+Tag
+
+class Tag
+  def self.cloud(args = {})
+    find(:all, :select => 'tags.* ,count(*) as popularity',
+    :limit => args[:limit] || 30,
+    :joins => "JOIN taggings ON taggings.tag_id = tags.id",
+    :conditions => args[:conditions],
+    :group => "taggings.tag_id",
+    :order => "id")
+  end
+end
+
+AnonymousAgent
+
+class AnonymousAgent
+  def superuser
+    false
+  end
+
+  def login
+    "Anonymous"
+  end
+end
+
 
