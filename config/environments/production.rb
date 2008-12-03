2ecd302a89c6b153e5dc59c4f02c491e1e0afcd8
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-12-03 11:37:29 +0000

    Restore classes caching in production environment
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15485 3f47a204-8049-4147-aab0-d444c64e7564

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = false
+config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
@@ -10,6 +10,7 @@ config.cache_classes = false
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
