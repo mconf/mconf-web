bd352319d14895c37a965d8f7fb23ebed39ef1f5
Marina de la Fuente <marinaf@dit.upm.es>
2008-12-05 16:03:07 +0000

    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15509 3f47a204-8049-4147-aab0-d444c64e7564

--- db/schema.rb
+++ db/schema.rb
@@ -121,10 +121,10 @@ ActiveRecord::Schema.define(:version => 20082030101012) do
     t.string  "scope",                 :limit => 1
   end
 
-  add_index "globalize_languages", ["rfc_3066"], :name => "index_globalize_languages_on_rfc_3066"
-  add_index "globalize_languages", ["iso_639_3"], :name => "index_globalize_languages_on_iso_639_3"
-  add_index "globalize_languages", ["iso_639_2"], :name => "index_globalize_languages_on_iso_639_2"
   add_index "globalize_languages", ["iso_639_1"], :name => "index_globalize_languages_on_iso_639_1"
+  add_index "globalize_languages", ["iso_639_2"], :name => "index_globalize_languages_on_iso_639_2"
+  add_index "globalize_languages", ["iso_639_3"], :name => "index_globalize_languages_on_iso_639_3"
+  add_index "globalize_languages", ["rfc_3066"], :name => "index_globalize_languages_on_rfc_3066"
 
   create_table "globalize_translations", :force => true do |t|
     t.string  "type"
@@ -139,8 +139,8 @@ ActiveRecord::Schema.define(:version => 20082030101012) do
     t.string  "namespace"
   end
 
-  add_index "globalize_translations", ["table_name", "item_id", "language_id"], :name => "globalize_translations_table_name_and_item_and_language"
   add_index "globalize_translations", ["tr_key", "language_id"], :name => "index_globalize_translations_on_tr_key_and_language_id"
+  add_index "globalize_translations", ["table_name", "item_id", "language_id"], :name => "globalize_translations_table_name_and_item_and_language"
 
   create_table "groups", :force => true do |t|
     t.string   "name"
