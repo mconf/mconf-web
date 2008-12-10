3ebd5084c5cfc3121be03b1dbaeb4ffe41e9d76b
Marina de la Fuente <marinaf@dit.upm.es>
2008-12-10 12:04:05 +0000

    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15574 3f47a204-8049-4147-aab0-d444c64e7564

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,9 +32,9 @@ end
   #with an image 
   def replace_image(atr)
      if atr == true
-  image_tag("/images/ok.jpg",:border=>0)
+  image_tag("/images/yes.png",:border=>0)
  else 
- image_tag("/images/cancel.gif",:border=>0)
+ image_tag("/images/delete22.png",:border=>0)
 end
 end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def replace_image(atr)
      if atr == true
-        image_tag("/images/ok.jpg",:border=>0)
+        image_tag("/images/yes.png",:border=>0)
      else 
-        image_tag("/images/cancel.gif",:border=>0)
+        image_tag("/images/delete22.png",:border=>0)
      end
   end
 
