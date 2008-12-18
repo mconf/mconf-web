8710f284abc940bae5420ef6c498747b96acfd08
Ivan Rojo Gargantilla <ivan.teleco@gmail.com>
2008-12-18 13:57:08 +0000

    fixes #67
    Cambiado en la pestaña manage que sale la opción Site para configurar los parámetros del site sólo cuando eres admin
    
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15816 3f47a204-8049-4147-aab0-d444c64e7564

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,6 +2,14 @@ class SitesController < ApplicationController
   before_filter :get_site
   authorization_filter :site, :update
 
+
+    # GET /site/edit
+  def edit
+    session[:current_tab] = "Manage"
+    session[:current_sub_tab] = "Site"
+    @site = current_site
+  end
+  
   def get_space
    @container = @space = Space.find_by_name("Public")
   end
