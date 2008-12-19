af63abaf9510710cd5e94634c240c5f1141117a2
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2008-12-19 11:10:52 +0000

    Modify User role permissions
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15851 3f47a204-8049-4147-aab0-d444c64e7564

--- db/fixtures/role.rb
+++ db/fixtures/role.rb
@@ -6,15 +6,15 @@ admin_role = Role.seed(:name) do |r|
    r.name= "Admin"
    r.stage_type = "Space"
 end
-admin_role.permissions << Permission.find_by_action_and_objective('read', 'self')
+admin_role.permissions << Permission.find_by_action_and_objective('read',   'self')
 admin_role.permissions << Permission.find_by_action_and_objective('update', 'self')
 admin_role.permissions << Permission.find_by_action_and_objective('delete', 'self')
 admin_role.permissions << Permission.find_by_action_and_objective('create', 'Content')
-admin_role.permissions << Permission.find_by_action_and_objective('read', 'Content')
+admin_role.permissions << Permission.find_by_action_and_objective('read', '  Content')
 admin_role.permissions << Permission.find_by_action_and_objective('update', 'Content')
 admin_role.permissions << Permission.find_by_action_and_objective('delete', 'Content')
 admin_role.permissions << Permission.find_by_action_and_objective('create', 'Performance')
-admin_role.permissions << Permission.find_by_action_and_objective('read', 'Performance')
+admin_role.permissions << Permission.find_by_action_and_objective('read',   'Performance')
 admin_role.permissions << Permission.find_by_action_and_objective('update', 'Performance')
 admin_role.permissions << Permission.find_by_action_and_objective('delete', 'Performance')
 
@@ -22,10 +22,11 @@ user_role = Role.seed(:name) do |r|
    r.name= "User"
    r.stage_type = "Space"
 end
-user_role.permissions << Permission.find_by_action_and_objective('read', 'self')
+user_role.permissions << Permission.find_by_action_and_objective('read',   'self')
 user_role.permissions << Permission.find_by_action_and_objective('create', 'Content')
-user_role.permissions << Permission.find_by_action_and_objective('read', 'Content')
-user_role.permissions << Permission.find_by_action_and_objective('read', 'Performance')
+user_role.permissions << Permission.find_by_action_and_objective('read',   'Content')
+user_role.permissions << Permission.find_by_action_and_objective('create', 'Performance')
+user_role.permissions << Permission.find_by_action_and_objective('read',   'Performance')
 
 invited_role = Role.seed(:name) do |r|
    r.name= "Invited"
