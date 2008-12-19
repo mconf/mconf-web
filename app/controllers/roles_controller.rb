2cfaea52a3573750640d86bb85c0387b64369d9e
Ivan Rojo Gargantilla <ivan.teleco@gmail.com>
2008-12-19 11:23:09 +0000

    Añadida la pestaña roles al Admin de usuario con los cambios asociados que lleva
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15852 3f47a204-8049-4147-aab0-d444c64e7564

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,4 +1,34 @@
 class RolesController < ApplicationController
   before_filter :current_site
   authorization_filter :current_site, :manage
+  
+  
+  # GET /roles
+  # GET /roles.xml
+  def index
+    session[:current_tab] = "Manage"
+    session[:current_sub_tab] = "Roles"
+    @roles = Role.column_sort(params[:order], params[:direction]).all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @roles }
+    end
+  end
+  
+  def get_space
+    if params[:space_id]
+      @container = @space = Space.find_by_name(params[:space_id])
+    elsif params[:id]
+      @container = @space = Space.find_by_name(params[:id])
+    elsif session[:space_id]
+      @container = @space = Space.find_by_name(session[:space_id])
+    else
+      @container = @space = Space.find_by_name("Public")
+    end 
+    @space = @container = Space.find_by_id(1) if @space == nil
+    session[:space_id] = @space.name
+    @space_thumbnail = Logotype.find(:first, :conditions => {:parent_id => @space.logotype, :thumbnail => 'space'})
+  end
+  
 end
