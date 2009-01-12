e160c8c01249b4bf42fcdc2ef2af3e904d47af8d
Ivan Rojo Gargantilla <ivan.teleco@gmail.com>
2009-01-12 16:01:04 +0000

    Subidos los test de users controller a falta de create y update... Hay fallos en las vistas que no se a qué son debidos y fallos en los test por culpa de una variable @user que no tiene lo que tiene que tener
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@15977 3f47a204-8049-4147-aab0-d444c64e7564

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 
 describe UsersController do
+  integrate_views
   include CMS::AuthenticationTestHelper
   fixtures :users , :spaces
   
@@ -150,16 +151,16 @@ describe UsersController do
         describe "in a private space" do
           before(:each) do
             @space = spaces(:private_no_roles)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 200
           end
           
           it "should redirect to the associated user view" do
-            get :show , :id => @test_user.id
+            get :show , :id => @user.id
             response.should render_template("show")
           end
           
@@ -168,16 +169,16 @@ describe UsersController do
         describe "in the public space" do
           before(:each) do
             @space = spaces(:public)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 200
           end
           
           it "should redirect to the associated user view" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             response.should render_template("show")
           end    
         end
@@ -190,11 +191,11 @@ describe UsersController do
         describe "in a private space where the user has the role Admin " do
           before(:each) do
             @space = spaces(:private_admin)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should not let the user to see the account information of a user of this space" do
-            get :show , :id => @test_user.id
+            get :show , :id => @user.id
             assert_response 403
           end
           
@@ -203,11 +204,11 @@ describe UsersController do
         describe "in a private space where the user has the role User" do
           before(:each) do
             @space = spaces(:private_user)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should not let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 403
           end
           
@@ -215,11 +216,11 @@ describe UsersController do
         describe "in a private space where the user has the role Invited " do
           before(:each) do
             @space = spaces(:private_invited)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should not let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 403
           end
           
@@ -228,22 +229,22 @@ describe UsersController do
         describe "in a private space where the user has not any roles" do
           before(:each) do
             @space = spaces(:private_no_roles)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should NOT let the user to see the account information of a user of this space" do
-            get :show , :id => @test_user.id
+            get :show , :id => @user.id
             assert_response 403
           end       
         end
         
         describe "without space" do
           before(:each) do
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should NOT let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 403
           end     
         end
@@ -253,11 +254,11 @@ describe UsersController do
           
           before(:each) do
             @space = spaces(:public)
-            @test_user = users(:user_normal2)
+            @user = users(:user_normal2)
           end
           
           it "should NOT let the user to see the account information of a user of this space" do
-            get :show, :id => @test_user.id
+            get :show, :id => @user.id
             assert_response 403
           end   
         end 
@@ -268,11 +269,11 @@ describe UsersController do
       describe "in a private space" do
         before(:each) do
           @space = spaces(:private_no_roles)
-          @test_user = users(:user_normal2)
+          @user = users(:user_normal2)
         end
         
         it "should NOT let the user to see the account information of a user of this space" do
-          get :show, :id => @test_user.id
+          get :show, :id => @user.id
           assert_response 403
         end
       end
@@ -280,11 +281,11 @@ describe UsersController do
       describe "in the public space" do
         before(:each) do
           @space = spaces(:public)
-          @test_user = users(:user_normal2)
+          @user = users(:user_normal2)
         end
         
         it "should NOT let the user to see the account information of a user of this space" do
-          get :show,  :id => @test_user.id
+          get :show,  :id => @user.id
           assert_response 403
         end  
       end 
@@ -294,15 +295,188 @@ describe UsersController do
   ##########################
   
   describe "responding to GET new" do
-    
-    
+    describe "when you are logged in" do
+      describe "as super Admin" do
+        before(:each)do
+          login_as(:user_admin)
+        end
+        
+        it "should let the user to create a new account" do
+          get :new
+          assert_response 200
+        end    
+      end
+      
+      describe "as a normal user" do
+        before(:each)do
+          login_as(:user_normal)
+        end
+        
+        it "should let the user to create a new account" do
+          get :new
+          assert_response 200
+        end
+        
+      end
+    end
+    describe "if you are not logged in" do
+      it "should let the user to create a new account" do
+        get :new
+        assert_response 200
+      end
+    end
   end
   
   
   ##########################
   describe "responding to GET edit" do
+    describe "when you are logged in" do
+      describe "as SuperAdmin" do
+        before(:each) do
+          login_as(:user_admin)
+        end
+        describe "in a private space" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+          
+          it "should let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 200
+          end
+          
+          it "should redirect to the associated user view" do
+            get :edit , :id => @user.id
+            response.should render_template("edit")
+          end
+          
+        end
+        
+        describe "in the public space" do
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+          
+          it "should let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 200
+          end
+          
+          it "should redirect to the associated user view" do
+            get :edit, :id => @user.id
+            response.should render_template("edit")
+          end    
+        end
     
+      end
+      describe "as normal_user" do
+        before(:each) do
+          login_as(:user_normal)
+        end
+        describe "in a private space where the user has the role Admin " do
+          before(:each) do
+            @space = spaces(:private_admin)
+            @user = users(:user_normal2)
+          end
+          
+          it "should not let the user to edit the account information of a user of this space" do
+            get :edit , :id => @user.id
+            assert_response 403
+          end
+          
+        end
+        
+        describe "in a private space where the user has the role User" do
+          before(:each) do
+            @space = spaces(:private_user)
+            @user = users(:user_normal2)
+          end
+          
+          it "should not let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 403
+          end
+          
+        end
+        describe "in a private space where the user has the role Invited " do
+          before(:each) do
+            @space = spaces(:private_invited)
+            @user = users(:user_normal2)
+          end
+          
+          it "should not let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 403
+          end
+          
+        end
+        
+        describe "in a private space where the user has not any roles" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to edit the account information of a user of this space" do
+            get :edit , :id => @user.id
+            assert_response 403
+          end       
+        end
+        
+        describe "without space" do
+          before(:each) do
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 403
+          end     
+        end
+        
+        
+        describe "in the public space" do
+          
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to edit the account information of a user of this space" do
+            get :edit, :id => @user.id
+            assert_response 403
+          end   
+        end 
+      end 
+    end
     
+    describe "if you are not logged in" do
+      describe "in a private space" do
+        before(:each) do
+          @space = spaces(:private_no_roles)
+          @user = users(:user_normal2)
+        end
+        
+        it "should NOT let the user to edit the account information of a user of this space" do
+          get :edit, :id => @user.id
+          assert_response 401
+        end
+      end
+      
+      describe "in the public space" do
+        before(:each) do
+          @space = spaces(:public)
+          @user = users(:user_normal2)
+        end
+        
+        it "should NOT let the user to edit the account information of a user of this space" do
+          get :edit,  :id => @user.id
+           assert_response 401
+        end  
+      end 
+    end            
   end
   
   #################
@@ -339,7 +513,170 @@ describe UsersController do
   ##################################
   
   describe "responding to DELETE destroy" do
+    describe "when you are logged in" do
+      describe "as SuperAdmin" do
+        before(:each) do
+          login_as(:user_admin)
+        end
+        describe "in a private space" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+          
+          it "should let the user to DELETE the account information of a user of this space" do
+            assert_difference 'User.count'do
+              delete :destroy, :id => @user.id
+              assert_response 200
+            end
+          end
+          
+          it "should redirect to the space's users list" do
+            delete :destroy , :id => @user.id
+            response.should redirect_to(space_users_path(@space)) 
+          end
+          
+        end
+        
+        describe "in the public space" do
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+          
+          it "should let the user to DELETE the account information of a user of this space" do
+            assert_difference 'User.count' do
+              delete :destroy, :id => @user.id
+              assert_response 200
+            end
+          end
+          
+          it "should redirect to the space's users list" do
+            delete :destroy, :id => @user.id
+            response.should redirect_to(space_users_path(@space)) 
+          end    
+        end    
+      end
+      describe "as normal_user" do
+        before(:each) do
+          login_as(:user_normal)
+        end
+        describe "in a private space where the user has the role Admin " do
+          before(:each) do
+            @space = spaces(:private_admin)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy , :id => @user.id
+              assert_response 403
+            end
+          end          
+        end
+        
+        describe "in a private space where the user has the role User" do
+          before(:each) do
+            @space = spaces(:private_user)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy, :id => @user.id
+              assert_response 403
+            end
+          end
+          
+        end
+        describe "in a private space where the user has the role Invited " do
+          before(:each) do
+            @space = spaces(:private_invited)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy, :id => @user.id
+              assert_response 403
+            end
+          end
+          
+        end
+        
+        describe "in a private space where the user has not any roles" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy , :id => @user.id
+              assert_response 403
+            end
+          end       
+        end
+        
+        describe "without space" do
+          before(:each) do
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy, :id => @user.id
+              assert_response 403
+            end
+          end     
+        end
+        
+        
+        describe "in the public space" do
+          
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+          
+          it "should NOT let the user to DELETE the account information of a user of this space" do
+            assert_no_difference 'User.count' do
+              delete :destroy, :id => @user.id
+              assert_response 403
+            end
+          end   
+        end 
+      end 
+    end
     
-    
+    describe "if you are not logged in" do
+      describe "in a private space" do
+        before(:each) do
+          @space = spaces(:private_no_roles)
+          @user = users(:user_normal2)
+        end
+        
+        it "should NOT let the user to DELETE the account information of a user of this space" do
+          assert_no_difference 'User.count' do
+            delete :destroy, :id => @user.id
+            assert_response 401
+          end
+        end
+      end
+      
+      describe "in the public space" do
+        before(:each) do
+          @space = spaces(:public)
+          @user = users(:user_normal2)
+        end
+        
+        it "should NOT let the user to DELETE the account information of a user of this space" do
+          assert_no_difference 'User.count' do
+            delete :destroy,  :id => @user.id
+            assert_response 401
+          end
+        end  
+      end 
+    end         
   end   
 end
