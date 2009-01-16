dbe1ea786f6974a4d9b760aaaea4306c6c24fad2
Ivan Rojo Gargantilla <ivan.teleco@gmail.com>
2009-01-16 10:44:30 +0000

    subidos test finalizados de users_controller a falta de depurarlos bien
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@16096 3f47a204-8049-4147-aab0-d444c64e7564

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 
 describe UsersController do
-  integrate_views
+  #integrate_views
   include CMS::AuthenticationTestHelper
   fixtures :users , :spaces
   
@@ -484,14 +484,87 @@ describe UsersController do
   describe "responding to POST create" do
     
     describe "with valid params" do
+      before(:each)do
+        @valid_attributes = {:email => 'pepe@gmail.com'}
+      end
+      describe "when you are logged in" do
+        describe "as super Admin" do
+          before(:each)do
+            login_as(:user_admin)
+          end
+        
+          it "should let the user to create a new account" do
+            assert_difference 'User.count' do
+              post :create, :user=> @valid_attributes
+              assert_response 200
+            end
+          end    
+        end
       
-      
+        describe "as a normal user" do
+          before(:each)do
+            login_as(:user_normal)
+          end
+        
+          it "should let the user to create a new account" do
+            assert_difference 'User.count' do
+              post :create, :user=> @valid_attributes
+              assert_response 200
+            end
+          end        
+        end
+      end
+      describe "if you are not logged in" do
+        it "should let the user to create a new account" do
+          assert_difference 'User.count' do
+            post :create, :user => @valid_attributes
+            assert_response 200
+          end
+        end
+      end              
     end
     
     describe "with invalid params" do
       
+      before(:each)do
+        @invalid_attributes = {}
+      end
       
+      describe "when you are logged in" do
+        describe "as super Admin" do
+          before(:each)do
+            login_as(:user_admin)
+          end
+        
+          it "should NOT let the user to create a new account" do
+            assert_no_difference 'User.count' do
+              post :create, :user=> @invalid_attributes
+              response.should render_template("new")
+            end
+          end    
+        end
       
+        describe "as a normal user" do
+          before(:each)do
+            login_as(:user_normal)
+          end
+        
+          it "should NOT let the user to create a new account" do
+            assert_no_difference 'User.count' do
+              post :create, :user=> @invalid_attributes
+              response.should render_template("new")
+            end
+          end        
+        end
+      end
+      describe "if you are not logged in" do
+        it "should NOT let the user to create a new account" do
+          assert_no_difference 'User.count' do
+            post :create, :user => @invalid_attributes
+            response.should render_template("new")            
+          end
+        end
+      end                  
     end
   end
   
@@ -501,12 +574,298 @@ describe UsersController do
   describe "responding to PUT udpate" do
     
     describe "with valid params" do
+      before(:each)do
+        @valid_attributes = {:email => 'pepe@gmail.com'}
+      end      
+      describe "when you are logged in" do
+        describe "as SuperAdmin" do
+          before(:each) do
+            login_as(:user_admin)
+          end
+          describe "in a private space" do
+            before(:each) do
+              @space = spaces(:private_no_roles)
+              @user = users(:user_normal2)
+            end
+          
+            it "should let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              response.should redirect_to(space_users_path(@space))
+            end
+          
+            it "should redirect to the associated list of users" do
+              put :update , :id => @user.id, :user =>@valid_attributes
+              response.should redirect_to(space_users_path(@space))
+            end
+          
+          end
+        
+          describe "in the public space" do
+            before(:each) do
+              @space = spaces(:public)
+              @user = users(:user_normal2)
+            end
+          
+            it "should let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              assert_response 200
+            end
+          
+            it "should redirect to the associated list of users " do
+              put :update, :id => @user.id
+              response.should redirect_to(space_users_path(@space))
+            end    
+          end
+    
+        end
+        describe "as normal_user" do
+          before(:each) do
+            login_as(:user_normal)
+          end
+          describe "in a private space where the user has the role Admin " do
+            before(:each) do
+              @space = spaces(:private_admin)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update , :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end
+          
+          end
+        
+          describe "in a private space where the user has the role User" do
+            before(:each) do
+              @space = spaces(:private_user)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end
+          
+          end
+          describe "in a private space where the user has the role Invited " do
+            before(:each) do
+              @space = spaces(:private_invited)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end
+          
+          end
+        
+          describe "in a private space where the user has not any roles" do
+            before(:each) do
+              @space = spaces(:private_no_roles)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update , :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end       
+          end
+        
+          describe "without space" do
+            before(:each) do
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end     
+          end
+        
+        
+          describe "in the public space" do
+          
+            before(:each) do
+              @space = spaces(:public)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @valid_attributes
+              assert_response 403
+            end   
+          end 
+        end 
+      end
+    
+      describe "if you are not logged in" do
+        describe "in a private space" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+        
+          it "should NOT let the user to UPDATE the account information of a user of this space" do
+            put :update, :id => @user.id, :user => @valid_attributes
+            assert_response 401
+          end
+        end
       
-      
+        describe "in the public space" do
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+        
+          it "should NOT let the user to UDATE the account information of a user of this space" do
+            put :update,  :id => @user.id, :user => @valid_attributes
+            assert_response 401
+          end  
+        end 
+      end            
     end
     
     describe "with invalid params" do
+      before(:each)do
+         @invalid_attributes = {}
+      end
+      describe "when you are logged in" do
+        describe "as SuperAdmin" do
+          before(:each) do
+            login_as(:user_admin)
+          end
+          describe "in a private space" do
+            before(:each) do
+              @space = spaces(:private_no_roles)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              response.should render_template("edit")
+            end
+          end
+        
+          describe "in the public space" do
+            before(:each) do
+              @space = spaces(:public)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              response.should render_template("edit")
+            end  
+          end
+    
+        end
+        describe "as normal_user" do
+          before(:each) do
+            login_as(:user_normal)
+          end
+          describe "in a private space where the user has the role Admin " do
+            before(:each) do
+              @space = spaces(:private_admin)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update , :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end
+          
+          end
+        
+          describe "in a private space where the user has the role User" do
+            before(:each) do
+              @space = spaces(:private_user)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end
+          
+          end
+          describe "in a private space where the user has the role Invited " do
+            before(:each) do
+              @space = spaces(:private_invited)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end
+          
+          end
+        
+          describe "in a private space where the user has not any roles" do
+            before(:each) do
+              @space = spaces(:private_no_roles)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update , :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end       
+          end
+        
+          describe "without space" do
+            before(:each) do
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end     
+          end
+        
+        
+          describe "in the public space" do
+          
+            before(:each) do
+              @space = spaces(:public)
+              @user = users(:user_normal2)
+            end
+          
+            it "should NOT let the user to UPDATE the account information of a user of this space" do
+              put :update, :id => @user.id, :user => @invalid_attributes
+              assert_response 403
+            end   
+          end 
+        end 
+      end
+    
+      describe "if you are not logged in" do
+        describe "in a private space" do
+          before(:each) do
+            @space = spaces(:private_no_roles)
+            @user = users(:user_normal2)
+          end
+        
+          it "should NOT let the user to UPDATE the account information of a user of this space" do
+            put :update, :id => @user.id, :user => @invalid_attributes
+            assert_response 401
+          end
+        end
       
+        describe "in the public space" do
+          before(:each) do
+            @space = spaces(:public)
+            @user = users(:user_normal2)
+          end
+        
+          it "should NOT let the user to UDATE the account information of a user of this space" do
+            put :update,  :id => @user.id, :user => @invalid_attributes
+            assert_response 401
+          end  
+        end 
+      end            
     end
   end
   
@@ -525,7 +884,7 @@ describe UsersController do
           end
           
           it "should let the user to DELETE the account information of a user of this space" do
-            assert_difference 'User.count'do
+            assert_difference 'User.count', -1 do
               delete :destroy, :id => @user.id
               assert_response 200
             end
@@ -545,7 +904,7 @@ describe UsersController do
           end
           
           it "should let the user to DELETE the account information of a user of this space" do
-            assert_difference 'User.count' do
+            assert_difference 'User.count', -1 do
               delete :destroy, :id => @user.id
               assert_response 200
             end
