d0c5c2c91c15fadbc6e7f3edcc9515e5a8336c43
Ivan Rojo Gargantilla <ivan.teleco@gmail.com>
2009-02-02 10:50:00 +0000

    cambiados los test añadiendo más fixtures al spaces controller para que no de fallos al hacer un rake spec
    git-svn-id: http://seta.dit.upm.es/svn/ging/sir2.0/trunk@16326 3f47a204-8049-4147-aab0-d444c64e7564

--- spec/controllers/spaces_controller_spec.rb
+++ spec/controllers/spaces_controller_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SpacesController do
   include CMS::AuthenticationTestHelper
-  fixtures :users , :spaces
+  fixtures :users , :spaces , :performances, :roles, :permissions
   
   
   
