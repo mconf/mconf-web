6eacfd893d990ecc32b6b4a68141e0e157b39645
Iván Rojo <irojo@dit.upm.es>
2009-03-10 17:04:45 +0100

    fixes an error in post_controller

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,7 @@
 module PostsHelper
 
   def time_update(post)
-    diff_time2 = (Time.now - post.updated_at.to_time)/60
-    diff_time = Time.ago(post.updated_at)
+    diff_time = (Time.now - post.updated_at.to_time)/60
     return diff_time/60
   end
 end
