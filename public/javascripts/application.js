3b77f5ba02ba643a2f7ccf9fa6037f17d7509074
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-03-17 17:03:53 +0100

    Basic jQuery support

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
+jQuery.fn.submitWithAjax = function() {
+  this.submit(function() {
+    $.post(this.action, $(this).serialize(), null, "script");
+    return false;
+  })
+  return this;
+};
