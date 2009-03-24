3ffe9fabd933088d9952d95a0b606bbcf59a3ca2
david <david@global2.(none)>
2009-03-24 16:39:27 +0100

    edit and delete groups added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,7 +14,41 @@ class GroupsController < ApplicationController
       end
     else
       flash[:error] = "The group is not valid"
-      redirect_to space_users_path(@space)
+      redirect_to space_users_path(@space, :list_by => 'groups')
+    end
+  end
+  
+  def edit
+    redirect_to space_users_path(@space, :list_by => 'groups', :edit_group => params[:id])
+  end
+  
+  def update
+    @group = @space.groups.find(params[:id])
+    if @group.update_attributes(params[:group])
+      respond_to do |format|
+      format.html {
+      flash[:success] = "The group " + @group.name + " has been successfully updated"
+        redirect_to space_users_path(@space, :list_by => 'groups')
+      }
+      end
+    else
+      flash[:error] = "The group is not valid"
+      redirect_to space_users_path(@space, :list_by => 'groups')
+    end
+  end
+  
+  def destroy
+    group = @space.groups.find(params[:id])
+    if group.destroy
+    respond_to do |format|
+      format.html {
+        flash[:success] = "The group has been successfully deleted"
+        redirect_to space_users_path(@space, :list_by => 'groups')
+      }
+      end
+    else
+      flash[:error] = "Error deleting the group"
+      redirect_to space_users_path(@space, :list_by => 'groups')
     end
   end
 end
