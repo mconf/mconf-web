b5207bee34981cf14ec22acbba1901acc6772579
david <david@global2.(none)>
2009-05-06 17:59:55 +0200

    expand/collapse posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,4 +10,15 @@ module PostsHelper
       space_posts_path(@space,:edit => comment)
     end   
   end
+  
+  def first_words( text, size )
+    if text.length > size
+      cutted_text = text[0..size]
+      cutted_text.chop! until cutted_text[-1,1] == " "
+      cutted_text.chop!
+      cutted_text << "..."
+    else
+      text
+    end
+  end
 end
