adadfaabe49e0e8c800aa0cb208f540ef31f55d6
Iván Rojo <irojo@dit.upm.es>
2009-05-25 13:12:04 +0200

    incoming word changed by upcoming in events. fixed #371

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -38,7 +38,7 @@
     @posts = @space.posts
     @lastest_posts=@posts.find(:all, :conditions => {"parent_id" => nil}, :order => "updated_at DESC").first(5)
     @lastest_users=@space.actors.sort {|x,y| y.created_at <=> x.created_at }.first(5)
-    @incoming_events=@space.events.find(:all, :order => "start_date DESC").select{|e| e.start_date.future?}.first(5)
+    @upcoming_events=@space.events.find(:all, :order => "start_date DESC").select{|e| e.start_date.future?}.first(5)
     @performance=Performance.find(:all, :conditions => {:agent_id => current_user, :stage_id => @space})
     respond_to do |format|
       format.html{
