e73a9a4455262ec5477a3a659f14d360aa2531cf
Iván Rojo <irojo@dit.upm.es>
2009-06-15 15:47:58 +0200

    e-mail validation on send us feedback lightbox

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -9,12 +9,21 @@ class FeedbackController < ApplicationController
   
   def create
     if (params[:feedback][:subject].present? and params[:feedback][:from].present? and params[:feedback][:body].present?)
-      Notifier.deliver_feedback_email(params[:feedback][:from],params[:feedback][:subject], params[:feedback][:body] )
-      respond_to do |format|
-        format.html {
-          flash[:success] = "your feedback information e-mail has been successfully sent. Thanks you"
-          redirect_to root_path()
-        }
+      if (params[:feedback][:from]).match(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i)
+        Notifier.deliver_feedback_email(params[:feedback][:from],params[:feedback][:subject], params[:feedback][:body] )
+        respond_to do |format|
+          format.html {
+            flash[:success] = "your feedback information e-mail has been successfully sent. Thanks you"
+            redirect_to root_path()
+          }
+        end
+      else
+        respond_to do |format|
+          format.html {
+            flash[:error] = "please, check your email"
+            render :action => "new" 
+          }
+        end
       end
     else
       respond_to do |format|
