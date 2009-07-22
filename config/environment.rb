20fc793a4724241c832108237b455bbce2793425
ebarra <enrique.barra@agora-2000.com>
2009-07-22 06:55:40 -0700

    sanitized now admits css

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ Rails::Initializer.run do |config|
 
   # Allowed html tags for sanitize  
   config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td', 'embed'
+  config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
