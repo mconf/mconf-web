1c05c01fd7370d945508e0908f48f2667e813a3a
ebarra <enrique.barra@agora-2000.com>
2009-07-29 16:22:06 -0700

    edit event with user registration

--- public/javascripts/user_filter.js
+++ public/javascripts/user_filter.js
@@ -10,6 +10,27 @@
     $("#show_all_users_link").hide();
   };
 	
+  select_all_users = function(){
+    $("#unselected_users .user_checkbox input").each(function() {
+        $(this).attr('checked', true);
+        $(this).allocate();
+    });
+    $("#show_all_users_link").hide();
+    $("#select_all_users_link").hide();
+    $("#deselect_all_users_link").show();
+  };
+  
+  deselect_all_users = function(){
+    $("#selected_users .user_checkbox input").each(function() {
+        $(this).attr('checked', false);
+        $(this).allocate();
+    });
+    $("#show_all_users_link").show();
+    $("#select_all_users_link").show();
+    $("#deselect_all_users_link").hide();
+    clear_users();
+  }; 
+  
 	selected_users_check = function(){
     if($(".user_checkbox input:checked").length > 0){
       $("#selected_users label:first").show();
@@ -61,7 +82,10 @@
     $(".user_checkbox input").each(function() {
       $(this).allocate();
     });
-		$("#user_filter").append("<a href=\"javascript:show_all_users()\" id=\"show_all_users_link\">Show all users</a>");
+		$("#user_filter").append("<a href=\"javascript:show_all_users()\" id=\"show_all_users_link\">Show all users</a>  <a href=\"javascript:select_all_users()\" id=\"select_all_users_link\">Select all users</a>");
+		$("#user_filter").append("<a href=\"javascript:hide_all_users()\" id=\"hide_all_users_link\">Hide all users</a>  <a href=\"javascript:deselect_all_users()\" id=\"deselect_all_users_link\">Deselect all users</a>");
+		$("#hide_all_users_link").hide();
+    $("#deselect_all_users_link").hide();
 		clear_users();
   });
   
