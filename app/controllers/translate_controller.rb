1cfe85482c7a40909fdafe867fcceaa8ffd83d91
david <david@global2.(none)>
2009-09-14 11:05:44 +0200

    working on translate

--- app/controllers/translate_controller.rb
+++ app/controllers/translate_controller.rb
@@ -2,7 +2,12 @@
 require_dependency "#{ RAILS_ROOT }/vendor/plugins/translate/app/controllers/translate_controller"
 
 class TranslateController
+  
+  include LocaleControllerModule
+  before_filter :set_vcc_locale
+  
   authorization_filter :tranlate, :site
   
   layout 'translate'
+ 
 end
