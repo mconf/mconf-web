1cbcb50b8c7a2cd8df95683a41260b5ec3da27bc
amendo <antonio@dit.upm.es>
2009-12-16 18:44:39 +0100

    addind an image to the preview

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091217135100) do
+ActiveRecord::Schema.define(:version => 20091214104211) do
 
   create_table "admissions", :force => true do |t|
     t.string   "type"
