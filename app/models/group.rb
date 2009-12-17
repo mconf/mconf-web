5ab11e22db980bfe530cbf46ae5c4cd902e1a995
ebarra <ebarra@pizarro.(none)>
2009-12-17 14:06:39 +0100

    bug solved, now the mailing lists are without accents

--- app/models/group.rb
+++ app/models/group.rb
@@ -93,7 +93,7 @@ class Group < ActiveRecord::Base
     def mail_list
        str =""
        self.users.each do |person|
-       str << "#{person.login}  <#{person.email}> \n"
+         str << "#{Group.remove_accents(person.login)}  <#{person.email}> \n"
        end
        str
    end
@@ -134,5 +134,32 @@ class Group < ActiveRecord::Base
   def temp_file
      @temp_file ||= "/tmp/sir-grupostemp-#{ rand }"
   end
+
+   def self.remove_accents(str)    
+    accents = { 
+      ['á','à','â','ä','ã'] => 'a',
+      ['Ã','Ä','Â','À'] => 'A',
+      ['é','è','ê','ë'] => 'e',
+      ['Ë','É','È','Ê'] => 'E',
+      ['í','ì','î','ï'] => 'i',
+      ['Î','Ì'] => 'I',
+      ['ó','ò','ô','ö','õ'] => 'o',
+      ['Õ','Ö','Ô','Ò','Ó'] => 'O',
+      ['ú','ù','û','ü'] => 'u',
+      ['Ú','Û','Ù','Ü'] => 'U',
+      ['ç'] => 'c', ['Ç'] => 'C',
+      ['ñ'] => 'n', ['Ñ'] => 'N'
+      }
+    accents.each do |ac,rep|
+      ac.each do |s|
+      str = str.gsub(s, rep)
+      end
+    end
+    str = str.gsub(/[^a-zA-Z0-9 ]/,"")    
+    str = str.gsub(/[ ]+/," ")
+    str = str.gsub(/ /,"-")    
+    #str = str.downcase
+  end
+
 
 end
