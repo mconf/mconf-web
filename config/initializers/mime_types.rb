3f70f6ffdbb71e7656009f5128d3dca39b01d852
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2009-12-22 18:15:48 +0100

    New oxygen icons

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -15,9 +15,9 @@ Mime::Type.register "application/postscript", :ps, [ "application/ps" ]
 Mime::Type.register "application/vnd.oasis.opendocument.text", :odt
 Mime::Type.register "application/vnd.oasis.opendocument.presentation", :odp
 Mime::Type.register "application/rtf", :rtf
-Mime::Type.register "application/vnd.ms-word", :doc, [ "application/msword" ]
+Mime::Type.register "application/vnd.ms-word", :doc, [ "application/msword", "application/x-msword" ]
 Mime::Type.register "application/vnd.ms-powerpoint", :ppt, [ "application/mspowerpoint" ]
 Mime::Type.register "application/vnd.ms-excel", :xls, [ "application/msexcel" ]
 Mime::Type.register "text/calendar", :ical
-Mime::Type.register "application/zip", :zip
+Mime::Type.register "application/zip", :zip, [ "application/x-zip" ]
 Mime::Type.register "application/rar", :rar, [ "application/x-rar" ]
