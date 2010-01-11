3e06f351d6fc74b57da548bdbf42112059499a57
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-01-11 15:28:41 +0100

    Make unix_file_mime_type compatible with file versions < 5

--- config/initializers/attachment_fu_file_mime_type.rb
+++ config/initializers/attachment_fu_file_mime_type.rb
@@ -7,7 +7,16 @@ module Technoweenie::AttachmentFu::InstanceMethods
     tmp_file = self.uploaded_data_without_unix_file_mime_type=(file_data)
 
     if tmp_file.present? && (unix_file = `which file`.chomp).present? && File.exists?(unix_file)
-      self.content_type = `#{ unix_file } -b --mime-type #{ tmp_file.path }`.chomp
+      `#{ unix_file } -v` =~ /^file-(.*)$/
+      version = $1.to_i
+
+      self.content_type = case version
+                          when 5
+                            `#{ unix_file } -b --mime-type #{ tmp_file.path }`.chomp
+                          else
+                            `#{ unix_file } -bi #{ tmp_file.path }`.chomp =~ /(\w*\/[\w+-\.]*)/
+                            $1
+                          end
     end
 
     tmp_file
