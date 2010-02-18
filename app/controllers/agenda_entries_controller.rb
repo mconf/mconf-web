d7a2e46fd28fab308ee7cffbf15bc645be445844
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-02-18 11:14:14 +0100

    Clean commented code

--- app/controllers/agenda_entries_controller.rb
+++ app/controllers/agenda_entries_controller.rb
@@ -131,7 +131,6 @@ class AgendaEntriesController < ApplicationController
   
   def event
     @event = Event.find_by_permalink(params[:event_id])
-   
   end
   
   
@@ -164,91 +163,4 @@ class AgendaEntriesController < ApplicationController
     end
     unknown_users
   end
-  
-  
-=begin
-  # GET /agenda_entries
-  # GET /agenda_entries.xml
-  def index
-    @agenda_entries = AgendaEntry.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @agenda_entries }
-    end
-  end
-
-  # GET /agenda_entries/1
-  # GET /agenda_entries/1.xml
-  def show
-    @agenda_entry = AgendaEntry.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @agenda_entry }
-    end
-  end
-
-  # GET /agenda_entries/new
-  # GET /agenda_entries/new.xml
-  def new
-    @agenda_entry = AgendaEntry.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @agenda_entry }
-    end
-  end
-
-  # GET /agenda_entries/1/edit
-  def edit
-    @agenda_entry = AgendaEntry.find(params[:id])
-  end
-
-  # POST /agenda_entries
-  # POST /agenda_entries.xml
-  def create
-    @agenda_entry = AgendaEntry.new(params[:agenda_entry])
-
-    respond_to do |format|
-      if @agenda_entry.save
-        flash[:notice] = 'AgendaEntry was successfully created.'
-        format.html { redirect_to(@agenda_entry) }
-        format.xml  { render :xml => @agenda_entry, :status => :created, :location => @agenda_entry }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @agenda_entry.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /agenda_entries/1
-  # PUT /agenda_entries/1.xml
-  def update
-    @agenda_entry = AgendaEntry.find(params[:id])
-
-    respond_to do |format|
-      if @agenda_entry.update_attributes(params[:agenda_entry])
-        flash[:notice] = 'AgendaEntry was successfully updated.'
-        format.html { redirect_to(@agenda_entry) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @agenda_entry.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /agenda_entries/1
-  # DELETE /agenda_entries/1.xml
-  def destroy
-    @agenda_entry = AgendaEntry.find(params[:id])
-    @agenda_entry.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(agenda_entries_url) }
-      format.xml  { head :ok }
-    end
-  end
-=end
 end
