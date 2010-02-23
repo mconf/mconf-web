e2750742ab909f85919ceb2b18351c30576286ab
Jaime <jaime@jaime-desktop.(none)>
2010-02-23 17:06:06 +0100

    iCalendar Import/Export

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -25,7 +25,25 @@ class AgendasController < ApplicationController
     @agenda_entry = AgendaEntry.new
   end
   
-  
+  # POST /agendas
+  # POST /agendas.xml
+  def create
+    if params[:icalendar_file].present?
+     #flash[:notice] = t('icalendar.succes_import')
+
+     import_icalendar
+          
+     redirect_to(space_event_path(@space, @event))
+     return
+    end
+    
+   respond_to do |format|
+        format.html { redirect_to(space_event_path(@space, @event, :show_day => 1)) }
+   end
+
+
+    
+  end
   
 private
   
@@ -78,23 +96,6 @@ private
     @agenda = Agenda.find(params[:id])
   end
 
-  # POST /agendas
-  # POST /agendas.xml
-  def create
-    @agenda = Agenda.new(params[:agenda])
-
-    respond_to do |format|
-      if @agenda.save
-        flash[:notice] = 'Agenda was successfully created.'
-        format.html { redirect_to(@agenda) }
-        format.xml  { render :xml => @agenda, :status => :created, :location => @agenda }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @agenda.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /agendas/1
   # PUT /agendas/1.xml
   def update
@@ -124,4 +125,109 @@ private
     end
   end
 =end
+
+
+
+def import_icalendar
+  
+    begin
+      @icalendar = params[:icalendar_file][:data]    
+      @icalendar = Vpim::Icalendar.decode(@icalendar)
+      overwrite = false;     
+      
+      if params[:overwrite].present? #overwrite
+        overwrite = true;     
+        @event.agenda.destroy    
+        agenda = Agenda.new()
+        @event.agenda = agenda
+      else #o just add
+        agenda = @event.agenda
+      end
+      
+      has_updated = false;
+      has_conflict = false;
+      has_outofbounds = false;
+      updated_entries = Array.new();
+      conflictive_entries = Array.new();
+      outofbounds_entries = Array.new();
+      total_entries = Array.new();
+      
+      @icalendar.each do |cal|
+     
+        entries = cal.events
+        
+        
+        entries.each do |e|
+          
+          if !overwrite && !(agenda_entry = AgendaEntry.find(:first, :conditions => ["agenda_id = ? AND uid = ?", agenda.id, e.uid])).nil?
+            if agenda_entry.updated_at == agenda_entry.created_at #Not modified on VCC
+              has_updated = true;
+              updated_entries.push(agenda_entry.title)
+              agenda_entry.destroy; #update
+            else #Modified on VCC, conflict and error
+              has_conflict = true;
+              conflictive_entries.push(agenda_entry.title)
+              next;
+            end
+          end
+          
+                             
+          agenda_entry = AgendaEntry.new()
+          
+          agenda_entry.agenda = agenda
+          
+          agenda_entry.title = e.summary.to_s
+          agenda_entry.description = e.description.to_s
+          agenda_entry.start_time = e.dtstart.to_s
+          agenda_entry.end_time = e.dtend.to_s
+          agenda_entry.speakers = e.organizer.to_s
+          agenda_entry.uid = e.uid        
+          
+          if (!((@event.start_date < agenda_entry.start_time)&&(@event.end_date > agenda_entry.end_time)))
+            has_outofbounds = true;
+            outofbounds_entries.push(agenda_entry.title)
+            next;
+          end           
+           
+          agenda_entry.save!
+          total_entries.push(agenda_entry.title);
+         
+        end     
+        
+      end
+      
+    flash[:notice] = t("icalendar.succes_import")  + "<br>"
+    
+    if has_updated && !overwrite
+      if updated_entries.length == 1 
+        flash[:notice] = flash[:notice] + "<br>" + t("icalendar.updated1")
+      else        
+        flash[:notice] = flash[:notice] + "<br>" + updated_entries.length.to_s + t("icalendar.updatedn")
+      end 
+    end    
+    
+    if has_conflict && !overwrite
+      if conflictive_entries.length == 1 
+        flash[:notice] = flash[:notice] + "<br>" + t("icalendar.conflictive1")
+      else        
+        flash[:notice] = flash[:notice] + "<br>" + conflictive_entries.length.to_s + t("icalendar.conflictiven")
+      end 
+    end 
+    
+    if has_outofbounds
+      if outofbounds_entries.length == 1 
+        flash[:notice] = flash[:notice] + "<br>" + t("icalendar.outbounds1")
+      else        
+        flash[:notice] = flash[:notice] + "<br>" + outofbounds_entries.length.to_s + t("icalendar.outboundsn")
+      end 
+    end 
+ 
+    flash[:notice] = flash[:notice] + "<br>"+ t("icalendar.importMessage1") + (outofbounds_entries.length + conflictive_entries.length + total_entries.length).to_s +
+     t("icalendar.importMessage2") + total_entries.length.to_s  + t("icalendar.importMessage3")
+
+  
+    rescue Exception => exc
+      flash[:error] = t("icalendar.error_import") + "<br>" + t("icalendar.importMessage4") + exc.message
+    end
+  end
 end
