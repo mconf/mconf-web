5a183a0cb98459d75c3f3c73a09b78fa7344df6c
david <dprieto@dit.upm.es>
2010-03-16 18:45:23 +0100

    Solved some problems in my home

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -7,4 +7,15 @@ module HomesHelper
   
     intervals = [today, yesterday, last_week, older]
   end
+  
+  def path_for_home(p={})
+    per_page = p[:per_page].present? ? p[:per_page] : params[:per_page]
+    contents = (params[:contents].present? ? params[:contents].split(",") : Space.contents.map(&:to_s)) + [p[:add_content]] -[p[:rm_content]]
+    
+    url_for(:per_page => per_page, :contents => contents.join(","))
+  end
+  
+  def home_menu_checkbox(name)
+    check_box_tag name, name , @contents.map(&:to_s).include?(name), :class => 'home_menu_checkbox' 
+  end
 end
