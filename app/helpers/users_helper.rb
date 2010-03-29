f366ca3b90680e477b28275c7522797ffc3ac590
Álvaro Martín <amartin@dit.upm.es>
2010-03-29 13:27:02 +0200

    Created partial for spaces sidebar to avoid repetition. Changed join request link to allow editing the request in the near future. Cleaned and moved is_last_admin method.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,26 +1,11 @@
 module UsersHelper
+
   def replace_image(atr)
      if atr == true
         image_tag("/images/yes.png",:border=>0)
      else 
         image_tag("/images/delete22.png",:border=>0)
      end
-  end
-
-  #method to know if an user is the only admin in the space
-  def is_last_admin(space, performance)
-    #if the performance is nil it means that there is no current_user, it is anonymous
-    if performance == nil
-      return false
-    end
-    #we check first if the performance is "Admin"
-    if performance.role_id != Role.find_by_name("Admin").id
-      return false
-    end
-    #And now if it is the last one
-    if Performance.find_all_by_stage_id_and_stage_type_and_role_id(space.id, "Space", Role.find_by_name("Admin")).size < 2
-      return true
-    end
   end
   
 end
