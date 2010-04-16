5c607a65f97422ffa7ce060bd95ee46433682fb6
Álvaro Martín <amartin@dit.upm.es>
2010-04-16 15:23:12 +0200

    Updated admission and performance tests.

--- spec/models/performance_spec.rb
+++ spec/models/performance_spec.rb
@@ -23,5 +23,50 @@ describe Performance do
       @admin_performance.destroy.should be_false
     end
   end
+  
+  describe ", regarding Event performances," do
+
+    before(:each) do
+  
+      @space = Factory(:space)
+      @event = Factory(:event)
+      @event.update_attribute(:space, @space)
+      @role = Role.find_by_name("Invitedevent")
+    
+    end
+    
+    it "after creating a performance for a user of an event, it should create the performance Invited for the " +
+      "space of that event, for the same user, if that user had no performance in that space before" do
+
+      @registered_user = Factory(:user)
+      assert_nil Performance.find_by_agent_id_and_stage_id(@registered_user.id,@event.id)
+      assert_nil Performance.find_by_agent_id_and_stage_id(@registered_user.id,@space.id)
+
+      perf = Performance.new(:agent => @registered_user, :role => @role, :stage => @event)
+      perf.save
+
+      event_performance = Performance.find_by_agent_id_and_stage_id(@registered_user.id,@event.id)
+      assert_equal(@registered_user,event_performance.agent)
+      assert_equal(Role.find(@role),event_performance.role)
+      assert_equal(@event,event_performance.stage)
+      
+      space_performance = Performance.find_by_agent_id_and_stage_id(@registered_user.id,@space.id)
+      assert_equal(@registered_user,space_performance.agent)
+      assert_equal(Role.find_by_name("Invited"),space_performance.role)
+      assert_equal(@space,space_performance.stage)
+  
+      event_performance.destroy
+      space_performance.destroy
+      @registered_user.destroy
+      
+    end
+  
+    after(:each) do
+  
+      @space.destroy
+      @event.destroy
+  
+    end
+  end
 end
 
