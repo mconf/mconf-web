848582ae298373e924bf177c6bed6c8ba1f7aeb6
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-04-27 13:33:58 +0200

    Change session cookie name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_prueba_session',
+  :key         => '_vcc_session',
   :secret      => 'c11f3c6e1ae902099ec9ec43f0dd382e2b1b329d26cc21736c01809497ee36e31b61b76054b161891460e6109774f832c1a000f32ece8fcb6fbf1f334d2d3e13',
   :session_http_only => false
 }
