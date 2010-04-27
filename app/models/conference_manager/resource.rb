95d5e5bfe96a12453c89c8a586ed2d47e32c6515
ebarra <ebarra@pizarro.(none)>
2010-04-27 17:18:27 +0200

    added vcc-test as conference manager

--- app/models/conference_manager/resource.rb
+++ app/models/conference_manager/resource.rb
@@ -2,7 +2,7 @@ module ConferenceManager
   class Resource < ActiveResource::Base
     class << self
       def domain
-        Site.current.cm_domain.present? ? Site.current.cm_domain : "http://vcc.globalplaza.co.cc:8080"
+        Site.current.cm_domain.present? ? Site.current.cm_domain : "http://vcc-test.dit.upm.es:8080"
       end
       
       def subclasses
