bd25a7ec528d33cf8227920b2b46d3bb6d5a8a8c
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-04-27 14:57:46 +0200

    Fix disable_chat_bar migration

--- db/migrate/20100413075244_disable_chat_bar.rb
+++ db/migrate/20100413075244_disable_chat_bar.rb
@@ -2,6 +2,9 @@ class DisableChatBar < ActiveRecord::Migration
   def self.up
     change_column :users, :chat_activation, :boolean, :default => false
     
+    User.reset_column_information
+    User.record_timestamps = false
+
     User.all.each do |u|
       u.update_attribute :chat_activation, false
     end
