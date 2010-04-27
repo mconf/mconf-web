67197b2d3d82293c0910d28923cec8fbc1cf67a7
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-04-27 13:32:49 +0200

    Try to fix profile migration

--- db/migrate/20100323113745_fix_prefix_key.rb
+++ db/migrate/20100323113745_fix_prefix_key.rb
@@ -3,7 +3,9 @@ class FixPrefixKey < ActiveRecord::Migration
     change_column :profiles, :prefix_key, :string, :default=>""
     change_column :profiles, :user_id, :integer
     
+    Profile.reset_column_information
     Profile.record_timestamps=false
+
     Profile.all.each do |p|
       p.update_attribute(:prefix_key,"") if p.prefix_key.nil?
     end
