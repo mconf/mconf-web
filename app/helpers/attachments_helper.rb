113bb37a295278caa43d0208394c090124f38d39
Álvaro Martín <amartin@dit.upm.es>
2010-05-05 17:41:00 +0200

    Fixed the title of the new_version button in the repository.

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -15,9 +15,9 @@ module AttachmentsHelper
               image_tag("icons/download_doc20.png", :title => t('login_request' + 'download'),:class=>"icon fade")
             end
     html << if interactive && attachment.current_version? && attachment.authorize?(:update,:to => current_user)
-            link_to(image_tag("icons/new_version_doc20.png", :title=> t('login_request'), :class=>"icon"), edit_space_attachment_path(attachment.space, attachment), :class => "repository_sidebar_action no-dot")
+              link_to(image_tag("icons/new_version_doc20.png", :title=> t('version.new'), :class=>"icon"), edit_space_attachment_path(attachment.space, attachment), :class => "repository_sidebar_action no-dot")
             else
-          image_tag("icons/new_version_doc20.png", :title=> t('login_request'), :class=>"icon fade")
+              image_tag("icons/new_version_doc20.png", :title=> t('login_request'), :class=>"icon fade")
             end
     html << if interactive && attachment.authorize?(:update,:to => current_user)
               attachment.tags.size>0 ? (link_to(image_tag("icons/pencil.png", :title=> t('tag.edit'),:class=>"icon"),edit_tags_space_attachment_path(attachment.space, attachment), :class=>"repository_sidebar_action no-dot")) : (link_to(image_tag('icons/add_tag20.png', :title => t('tag.add'),:class=>"icon"), edit_tags_space_attachment_path(@space, attachment), :class=>"repository_sidebar_action no-dot"))
