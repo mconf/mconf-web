7cc0319a558257cf48c950bbc551d36cbf6f94bd
david <dprieto@dit.upm.es>
2010-05-19 17:46:23 +0200

    Fixed search bugs originated by disabled spaces

--- app/models/space.rb
+++ app/models/space.rb
@@ -49,7 +49,8 @@ class Space < ActiveRecord::Base
   
   before_validation :update_logo
 
-  is_indexed :fields => ['name','description']
+  is_indexed :fields => ['name','description'],
+             :conditions => "disabled = 0"
 
   validates_presence_of :name, :description
   validates_uniqueness_of :name
