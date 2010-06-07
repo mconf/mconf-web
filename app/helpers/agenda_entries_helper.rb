1d7f902d0fb90f587b1b1b948e17b9dda13d326e
david <dprieto@dit.upm.es>
2010-06-07 17:53:11 +0200

    Changed search displaying order

--- app/helpers/agenda_entries_helper.rb
+++ app/helpers/agenda_entries_helper.rb
@@ -4,7 +4,7 @@ module AgendaEntriesHelper
   def entry_speakers(entry)
     (entry.actors + [entry.speakers]).compact.map{ |a|
                            a.is_a?(User) ? 
-                           link_to(highlight(a.name,@query),user_path(a),:class=>"unified_user") :
+                           link_to(highlight(a.name,params[:query]),user_path(a),:class=>"unified_user") :
                            (a=="" ? nil : a)
                         }.compact.join(",")
   end
