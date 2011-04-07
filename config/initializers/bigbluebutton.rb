0137e5a0f2ccec6a08fd5879b2bfee14000ddc60
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-04-07 12:31:48 -0300

    Fixing bug in the initializer when BigbluebuttonServer does not exists.

--- config/initializers/bigbluebutton.rb
+++ config/initializers/bigbluebutton.rb
@@ -2,5 +2,7 @@ require 'bigbluebutton-api'
 
 #BBB_CONFIG = YAML.load_file(File.join(Rails.root, "config", "bigbluebutton_conf.yml"))[RAILS_ENV]
 #BBB_API = BigBlueButton::BigBlueButtonApi.new(BBB_CONFIG["server"], BBB_CONFIG["salt"], BBB_CONFIG["version"], RAILS_ENV != :production)
-server = BigbluebuttonServer.first
-BBB_API = BigBlueButton::BigBlueButtonApi.new(server.url, server.salt, server.version, RAILS_ENV != :production)
+if defined?(BigbluebuttonServer)
+  server = BigbluebuttonServer.first
+  BBB_API = BigBlueButton::BigBlueButtonApi.new(server.url, server.salt, server.version, RAILS_ENV != :production)
+end
