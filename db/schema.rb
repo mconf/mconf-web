13e8323cc32795e4fd4eb915d2aaedb2b82c1b34
arthurrauter <arthurraute@gmail.com>
2011-04-15 15:13:05 -0300

    Many broken tags fixed all over GlobalPlaza

--- db/schema.rb
+++ db/schema.rb
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
 
   create_table "chat_logs", :force => true do |t|
     t.integer  "event_id"
-    t.text     "content",    :limit => 2147483647
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -154,13 +154,13 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
-    t.text     "description"
+    t.text     "description",             :limit => 255
     t.string   "place"
     t.boolean  "isabel_event"
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer  "machine_id"
-    t.string   "colour",                  :default => ""
+    t.string   "colour",                                 :default => ""
     t.string   "repeat"
     t.integer  "at_job"
     t.integer  "parent_id"
@@ -171,19 +171,19 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
     t.integer  "space_id"
     t.integer  "author_id"
     t.string   "author_type"
-    t.boolean  "marte_event",             :default => false
+    t.boolean  "marte_event",                            :default => false
     t.boolean  "marte_room"
-    t.boolean  "spam",                    :default => false
+    t.boolean  "spam",                                   :default => false
     t.text     "notes"
     t.text     "location"
     t.text     "other_streaming_url"
     t.string   "permalink"
     t.integer  "cm_event_id"
-    t.integer  "vc_mode",                 :default => 0
+    t.integer  "vc_mode",                                :default => 0
     t.text     "other_participation_url"
-    t.boolean  "web_interface",           :default => false
-    t.boolean  "isabel_interface",        :default => false
-    t.boolean  "sip_interface",           :default => false
+    t.boolean  "web_interface",                          :default => false
+    t.boolean  "isabel_interface",                       :default => false
+    t.boolean  "sip_interface",                          :default => false
     t.datetime "generate_pdf_at"
     t.datetime "generate_scorm_at"
     t.text     "isabel_bw"
@@ -191,9 +191,9 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
     t.integer  "recording_bw"
     t.datetime "generate_pdf_small_at"
     t.integer  "chat_log_id"
-    t.boolean  "streaming_by_default",    :default => true
-    t.boolean  "manual_configuration",    :default => false
-    t.integer  "recording_type",          :default => 0
+    t.boolean  "streaming_by_default",                   :default => true
+    t.boolean  "manual_configuration",                   :default => false
+    t.integer  "recording_type",                         :default => 0
   end
 
   create_table "groups", :force => true do |t|
@@ -327,13 +327,13 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
     t.integer  "sender_id"
     t.integer  "receiver_id"
     t.integer  "parent_id"
-    t.boolean  "checked",             :default => false
+    t.boolean  "checked",                            :default => false
     t.string   "title"
-    t.text     "body"
+    t.text     "body",                :limit => 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "deleted_by_sender",   :default => false
-    t.boolean  "deleted_by_receiver", :default => false
+    t.boolean  "deleted_by_sender",                  :default => false
+    t.boolean  "deleted_by_receiver",                :default => false
   end
 
   create_table "profiles", :force => true do |t|
@@ -346,13 +346,13 @@ ActiveRecord::Schema.define(:version => 20110406155303) do
     t.string  "zipcode"
     t.string  "province"
     t.string  "country"
-    t.integer "user_id"
-    t.string  "prefix_key",   :default => ""
+    t.integer "user_id",      :limit => 255
+    t.string  "prefix_key",                  :default => ""
     t.text    "description"
     t.string  "url"
     t.string  "skype"
     t.string  "im"
-    t.integer "visibility",   :default => 3
+    t.integer "visibility",                  :default => 3
     t.string  "full_name"
   end
 
