a6fc845ef30ea97bf2a82cb390d2a091f7de2426
Fernando <fbottin@gmail.com>
2011-05-19 17:18:18 -0300

    Verify wrong formatted emails.

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -93,19 +93,27 @@ class InvitesController < ApplicationController
       for receiver in params[:invite][:email_tokens].split(/;|,/)
         priv_email[:email_receiver] = receiver
         email_message = Notifier.webconference_invite_email(priv_email)
-        if email_message.deliver
-          if success.size == 0
-            success = t('invite.invitation_successfully') << "<li>" << t('invite.email') << receiver << "</li>"
+        if (receiver =~ /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i)
+          if email_message.deliver
+            if success.size == 0
+              success = t('invite.invitation_successfully') << "<li>" << t('invite.email') << receiver << "</li>"
+            else
+              success << "<li>" <<  t('invite.email') << receiver << "</li>"
+            end
           else
-            success << "<li>" <<  t('invite.email') << receiver << "</li>"
+            if error.size == 0
+              error = t('invite.invitation_unsuccessfully') << "<li>" <<  t('invite.email') << receiver << "</li>"
+            else
+              error << "<li>" <<  t('invite.email') << receiver << "</li>"
+            end
+            @fail_email << email_message
           end
         else
           if error.size == 0
-            error = t('invite.invitation_unsuccessfully') << "<li>" <<  t('invite.email') << receiver << "</li>"
+            error = t('invite.invitation_unsuccessfully') << "<li>" <<  t('invite.email') << receiver << t('invite.wrong_formatted') << "</li>"
           else
-            error << "<li>" <<  t('invite.email') << receiver << "</li>"
+            error << "<li>" <<  t('invite.email') << receiver << t('invite.wrong_formatted') << "</li>"
           end
-          @fail_email << email_message
         end
       end
     end
