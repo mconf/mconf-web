e9c37509a2d721224b8198936c64a7a33ea7573d
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-05-20 16:59:31 -0300

    Added links to join_mobile, a new action in bigbluebutton_rails that shows a QR code to enter the conference. Will only work in the next version of bigbluebutton_rails. refs #87.

--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -1,4 +1,5 @@
 class CustomBigbluebuttonRoomsController < Bigbluebutton::RoomsController
   before_filter :authentication_required, :except => [:invite, :auth, :running]
   authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join]
+  layout :application, :except => [:join_mobile]
 end
