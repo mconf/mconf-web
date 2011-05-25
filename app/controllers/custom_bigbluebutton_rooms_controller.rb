b4087f9fff20972c1e66a8b795d6a859ccc83a26
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-05-25 14:09:14 -0300

    Small error in the rooms controllers was causing rendering errors. Also after removing a room in his home page, the user stays in the home page instead of begin redirected to server#index. Fixes issue 115.

--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -2,5 +2,5 @@ class CustomBigbluebuttonRoomsController < Bigbluebutton::RoomsController
   before_filter :authentication_required, :except => [:invite, :auth, :running]
   # TODO review it, see Issue #107
   # authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join]
-  layout :application, :except => [:join_mobile]
+  layout 'application', :except => [:join_mobile]
 end
