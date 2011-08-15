da3f72f357243e60dcad577b0614db19b1d82f10
Fernando <fbottin@gmail.com>
2011-08-15 16:15:25 -0300

    Modified the custom bbb room controller, now only the super user can modify or see the BBB, servers and rooms, page.

--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -1,6 +1,6 @@
 class CustomBigbluebuttonRoomsController < Bigbluebutton::RoomsController
   before_filter :authentication_required, :except => [:invite, :auth, :running]
   # TODO review it, see Issue #107
-  # authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join]
+  authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join]
   layout 'application', :except => [:join_mobile]
 end
