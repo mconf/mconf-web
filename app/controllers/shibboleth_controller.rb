f88e04782b7dfbf3ee25c728d8b4bd78ca784b71
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-09-21 17:49:29 -0300

    Improving the shibboleth authentication. The basic features are now working.

--- app/controllers/shibboleth_controller.rb
+++ app/controllers/shibboleth_controller.rb
@@ -21,33 +21,59 @@ require "net/http"
 
 class ShibbolethController < ApplicationController
 
+  respond_to :html
+  layout 'clean'
+
+  # Log in a user using his shibboleth information
+  # The application should only reach this point after authenticating using Shibboleth
+  # The authentication is currently made with the Apache module mod_shib
   def create
 
-    # TODO: accept any Shib_ key
-    @data = { }
-    keys = ["Shib-Application-ID", "Shib-Session-ID", "Shib-Identity-Provider", "Shib-Authentication-Instant",
-            "Shib-Authentication-Method", "Shib-AuthnContext-Class", "Shib-eduPerson-eduPersonPrincipalName",
-            "Shib-eduPerson-eduPersonAffiliation", "Shib-brPerson-brPersonCPF", "Shib-brPerson-brPersonPassport",
-            "Shib-inetOrgPerson-cn", "Shib-inetOrgPerson-sn", "Shib-inetOrgPerson-mail", "Shib-brEduPerson-brEduAffiliationType"]
-    keys.each do |k|
-      request.env.has_key?(k)
-      @data[k] = request.env[k]
-    end
-
-    # TODO temp
+    #################################
+    # FAKE TEST DATA
     # @data["Shib-inetOrgPerson-sn"] = "JOAO DA SILVA"
     # @data["Shib-inetOrgPerson-mail"] = "invalido@ufrgs.br"
+    # shib_name = "JOAO DA SILVA"
+    # shib_email = "invalido@ufrgs.br"
+    # request.env["Shib-Application-ID"] = "default"
+    # request.env["Shib-Session-ID"] = "09a612f952cc5995e4a86ddd87fd9f2a"
+    # request.env["Shib-Identity-Provider"] = "https://login.teste.ufrgs.br/idp/shibboleth"
+    # request.env["Shib-Authentication-Instant"] = "2011-09-21T19:11:58.039Z"
+    # request.env["Shib-Authentication-Method"] = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+    # request.env["Shib-AuthnContext-Class"] = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport"
+    # request.env["Shib-brEduPerson-brEduAffiliationType"] = "student;position;faculty"
+    # request.env["Shib-eduPerson-eduPersonPrincipalName"] = "ef775988943825d2871e1cfa75473ec0@ufrgs.br"
+    # request.env["Shib-inetOrgPerson-cn"] = "JOAO DA SILVA"
+    # request.env["Shib-inetOrgPerson-mail"] = "invalido@ufrgs.br"
+    # request.env["Shib-inetOrgPerson-sn"] = "JOAO DA SILVA"
+    #################################
 
-    # create the user based on the info returned by shibboleth
-    password = SecureRandom.hex(16)
-    user = User.create!(:login => @data["Shib-inetOrgPerson-sn"].downcase.parameterize,
-                        :email => @data["Shib-inetOrgPerson-mail"],
-                        :password => password, :password_confirmation => password)
-    user.activate
-    user.profile.update_attributes(:full_name => @data["Shib-inetOrgPerson-sn"].titleize)
+    # stores any "Shib-" variable in the session
+    shib_data = {}
+    request.env.each do |key, value|
+      shib_data[key] = value if key.to_s.downcase =~ /^shib-/
+    end
+    session[:shib_data] = shib_data
 
-    # TODO: create associated table
-    # TODO: check duplicated email/login
+    # uses the fed email to check if the user already has an account
+    user = User.find_by_email(shib_email)
+
+    # the user already has an account but it was not activated yet
+    if user and !user.active?
+      @user = user
+      render "need_activation"
+      return
+    end
+
+    # the fed user has no account yet
+    # create one based on the info returned by shibboleth
+    if user.nil?
+      password = SecureRandom.hex(16)
+      user = User.create!(:login => shib_name.clone, :email => shib_email,
+                          :password => password, :password_confirmation => password)
+      user.activate
+      user.profile.update_attributes(:full_name => shib_name)
+    end
 
     # login and go to home
     self.current_agent = user
