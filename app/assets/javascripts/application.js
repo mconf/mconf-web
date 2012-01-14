66d0db63c510cfa77ccc97a5b8d8323ed97677f8
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-01-14 14:39:18 -0200

    New tooltips using qTip 2. Still need to update the old tooltips to the new format. Also updated the images in the slider in the frontpage.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,12 +3,10 @@
 //= require jquery/jquery.livequery
 //= require jquery/jquery.easing.1.3
 //= require jquery/jquery.timers-1.2
-//= require jquery/jquery.qtip-1.0.0-rc3
+//= require jquery/jquery.qtip.2.0.0pre
 //= require jquery/jquery.fancybox
 //= require jquery/jquery-ui
 //= require jquery/jquery.form
 //= require jquery/jquery.tokeninput
 //= require anythingslider/jquery.anythingslider.min
-//= require application/base
-//= require application/document_ready
-//= require application/notifications
+//= require_tree ./application/
--- app/assets/javascripts/frontpage.js
+++ app/assets/javascripts/frontpage.js
@@ -3,7 +3,7 @@
 //= require jquery/jquery.livequery
 //= require jquery/jquery.easing.1.3
 //= require jquery/jquery.timers-1.2
-//= require jquery/jquery.qtip-1.0.0-rc3
+//= require jquery/jquery.qtip.2.0.0pre
 //= require anythingslider/jquery.anythingslider.min
 //= require application
-//= require frontpage/components
+//= require_tree ./frontpage/
