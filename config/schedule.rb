80372693dc06c5e9c88b05c06a0634deb1a00463
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-01-31 14:31:33 -0200

    Updating the schedule for digest emails to be sent at a decent time.

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,16 +7,22 @@
 # for some reason the default 'rake' wasn't using 'bundle exec', so we're redefining it here
 job_type :rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
 
-every :day, :at => '2am' do
+every :day, :at => '1am' do
   # updates the stats - will only increment stats from the past day
   rake "mconf:analytics:update"
-  # send daily emails
+end
+
+every :sunday, :at => '2am' do
+  # restart the analytics stats every week
+  rake "mconf:analytics:init"
+end
+
+every :day, :at => '2pm' do
+  # send daily digest emails
   runner "Mconf::DigestEmail.send_daily_digest"
 end
 
-every :sunday, :at => '3am' do
-  # restart the analytics stats every week
-  rake "mconf:analytics:init"
-  # send weekly emails
+every :monday, :at => '2pm' do
+  # send weekly digest emails
   runner "Mconf::DigestEmail.send_weekly_digest"
 end
