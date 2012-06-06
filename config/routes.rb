f025555ac1adf80722d5daf74ecd29fb4125145f
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-06-06 15:06:07 -0300

    New page "service description" and some changes in "terms-cafe".
    Signed-off-by: Leonardo Crauss Daronco <leonardodaronco@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -221,7 +221,8 @@ Mconf::Application.routes.draw do
   match 'faq', :to => 'faq#index', :as => 'faq'
   #match 'perf_indicator', :to => 'frontpage#performance', :as => 'perf_indicator'
   #match 'about', :to => 'frontpage#about', :as => 'about' # TODO Create an about for Mconf
-  match 'terms_cafe', :to => 'terms_cafe#index', :as => 'terms'
+  match 'about/terms-cafe', :to => 'about#terms_cafe', :as => 'terms'
+  match 'about/service', :to => 'about#service_description', :as => 'service_description'
 
   resource :session
 
