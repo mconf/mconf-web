e28dfc06eafeaa126ff4e8f5bc9f608f277dd814
mconf <mconf@mconf-www.belnet.be>
2012-06-11 16:20:59 +0200

    change mail system to enable any type of mail infrastructure and not only gmail

--- db/migrate/20120611122802_add_smtp_sender_to_sites.rb
+++ db/migrate/20120611122802_add_smtp_sender_to_sites.rb
@@ -1,7 +1,9 @@
 class AddSmtpSenderToSites < ActiveRecord::Migration
   def self.up
+   add_column :sites , :smtp_sender, :string
   end
 
   def self.down
+   remove_column :sites, :smtp_sender
   end
 end
