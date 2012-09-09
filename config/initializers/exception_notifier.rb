046f4c5ea63110a7df76a321dc033c00080801b1
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-09-09 20:58:35 -0300

    Refactor exception notifier's init: fix errors if Site doesn't have all columns

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -4,15 +4,40 @@
 # This file is licensed under the Affero General Public License version
 # 3 or later. See the LICENSE file.
 
+def check_conditions
+  if Rails.env.production? and Site.table_exists?
+    site = Site.current
+    return site &&
+      site.respond_to?(:exception_notifications) &&
+      site.exception_notifications
+  else
+    false
+  end
+end
+
+def get_sender_address
+  site = Site.current
+  if site.respond_to?(:name) && site.respond_to?(:email)
+    %("#{site.name}" <#{site.email}>)
+  else
+    "Undefined"
+  end
+end
+
 # Initializes the ExceptionNotifier using the information stored in the current Site
 # Works only in production
-if Rails.env.production? and Site.table_exists?
+def configure_exception_notifier
   site = Site.current
-  if site.respond_to?(:exception_notifications) && site.exception_notifications
-    recipients = site.exception_notifications_email.split(/[\s,;]/).reject(&:empty?) # accepts " ", "," and ";" as separators
-    Mconf::Application.config.middleware.use ExceptionNotifier,
-      :email_prefix => site.exception_notifications_prefix + " ",
-      :sender_address => %("#{site.name}" <#{site.email}>),
-      :exception_recipients => recipients
-  end
+
+  # accepts " ", "," and ";" as separators
+  recvs = site.exception_notifications_email.split(/[\s,;]/).reject(&:empty?)
+
+  Mconf::Application.config.middleware.use ExceptionNotifier,
+  :email_prefix => site.exception_notifications_prefix + " ",
+  :sender_address => get_sender_address(),
+  :exception_recipients => recvs
+end
+
+if check_conditions()
+  configure_exception_notifier()
 end
