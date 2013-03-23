172324e8229e4e1bfee0687a9e2f2322bc79eecd
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-03-23 18:19:57 -0300

    Set 'reply_to' in all emails and fix target language in some
    Also fixed the locale used to send webconf invites to use the receiver's
    language.

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -46,26 +46,29 @@ class InvitesController < ApplicationController
       priv_msg[:sender_id] = current_user.id
       priv_email[:sender_id] = current_user.id
 
-      # message body
-      body = t('invite.message', :sender => current_user.name, :name => params[:invite][:room_name],
-               :invite_url => params[:invite][:room_url], :message => params[:invite][:message],
-               :mobile_url => params[:invite][:mobile_url]).html_safe
       unless params[:invite][:message].empty?
         priv_email[:body] = params[:invite][:message]
       end
-      priv_msg[:body] = body
-
-      priv_msg[:title] = t('invite.title')
-      priv_email[:title] = t('invite.title')
       priv_email[:room_name] = params[:invite][:room_name]
       priv_email[:room_url] = params[:invite][:room_url]
       priv_email[:mobile_url] = params[:invite][:mobile_url]
       priv_email[:user_name] = current_user.name
-      priv_email[:locale] = current_user.locale
+      priv_email[:locale] = get_user_locale(current_user)
 
       if params[:invite][:im_check] != "0"
         for receiver in params[:invite][:members_tokens].split(",")
           priv_msg[:receiver_id] = receiver
+          user = User.find(receiver)
+          locale = get_user_locale(user, false)
+          I18n.with_locale(locale) do
+            priv_msg[:title] = t('invite.title')
+            body = t('invite.message', :sender => current_user.name, :name => params[:invite][:room_name],
+                     :invite_url => params[:invite][:room_url], :message => params[:invite][:message],
+                     :mobile_url => params[:invite][:mobile_url],
+                     :email_sender => current_user.email).html_safe
+            priv_msg[:body] = body
+          end
+
           private_message = PrivateMessage.new(priv_msg)
           if private_message.save
             @success_messages << private_message
@@ -78,10 +81,11 @@ class InvitesController < ApplicationController
       end
 
       if params[:invite][:email_check] != "0"
-        emailSended = true
         for receiver in params[:invite][:members_tokens].split(",")
           user = User.find(receiver)
           priv_email[:email_receiver] = user.email
+          priv_email[:email_sender] = current_user.email
+          priv_email[:locale] = get_user_locale(user, false)
           Notifier.delay.webconference_invite_email(priv_email)
           if success.size == 0
             success = t('invite.invitation_successfully') << " " << t('invite.email', :email => user.email)
@@ -94,6 +98,7 @@ class InvitesController < ApplicationController
       if params[:invite][:email_tokens].size != 0
         for receiver in params[:invite][:email_tokens].split(/;|,/)
           priv_email[:email_receiver] = receiver
+          priv_email[:email_sender] = current_user.email
           Notifier.delay.webconference_invite_email(priv_email)
           if (receiver =~ /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i)
             if success.size == 0
