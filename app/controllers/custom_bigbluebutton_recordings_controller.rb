1a5fda7bbacb5075dd3d777ac23294d83f1c2abb
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-04-15 15:45:29 -0300

    Enable non admins to fetch recordings and play recordings

--- app/controllers/custom_bigbluebutton_recordings_controller.rb
+++ app/controllers/custom_bigbluebutton_recordings_controller.rb
@@ -1,3 +1,4 @@
 class CustomBigbluebuttonRecordingsController < Bigbluebutton::RecordingsController
-  authorization_filter :manage, :current_site
+  before_filter :authentication_required
+  authorization_filter :manage, :current_site, :except => [:play]
 end
--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -1,5 +1,5 @@
 class CustomBigbluebuttonRoomsController < Bigbluebutton::RoomsController
   before_filter :authentication_required, :except => [:invite, :auth, :running, :external]
-  authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join, :end, :external, :join_mobile]
+  authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join, :end, :external, :join_mobile, :fetch_recordings]
   layout 'application', :except => [:join_mobile]
 end
