mconf/mconf-web

commit 73b390347d28dd03ccd500b04065b1bd517b6a2e

Fix SpacesController to accept changes in it's webconf room

app/controllers/spaces_controller.rb
@@ -174,40 +174,52 @@ class SpacesController < ApplicationController
   # PUT /spaces/1.xml
   # PUT /spaces/1.atom
   def update
-    unless params[:space][:bigbluebutton_room_attributes].blank?
-      params[:space][:bigbluebutton_room_attributes][:id] = @space.bigbluebutton_room.id
-    end
-
-    if @space.update_attributes(params[:space])
+    if params[:bbb_room]
+      # TODO: treat possible errors
+      @space.bigbluebutton_room.update_attributes(params[:bigbluebutton_room])
       respond_to do |format|
         format.html {
-          flash[:success] = t('space.updated')
           redirect_to request.referer
         }
-        format.atom { head :ok }
-        format.js{
-          if params[:space][:name] or params[:space][:description]
-            @result = params[:space][:name] ? nil : params[:space][:description]
-            flash[:success] = t('space.updated')
-            render "result.js"
-          elsif !params[:space][:bigbluebutton_room_attributes].blank?
-            if params[:space][:bigbluebutton_room_attributes][:moderator_password] or params[:space][:bigbluebutton_room_attributes][:attendee_password]
-              @result = params[:space][:bigbluebutton_room_attributes][:moderator_password] ? params[:space][:bigbluebutton_room_attributes][:moderator_password] : params[:space][:bigbluebutton_room_attributes][:attendee_password]
-              flash[:success] = t('space.updated')
-              render "result.js"
-            end
-          else
-            render "update.js"
-          end
+        format.json {
+          render :json => @space.bigbluebutton_room.to_json(:include => :metadata)
         }
       end
     else
-      respond_to do |format|
-        flash[:error] = @space.errors.full_messages.to_sentence
-        format.js { render :text => t('error.change') }
-        format.html { redirect_to edit_space_path() }
-        format.xml  { render :xml => @space.errors, :status => :unprocessable_entity }
-        format.atom { render :xml => @space.errors.to_xml, :status => :not_acceptable }
+      if params[:space][:bigbluebutton_room_attributes].blank?
+        params[:space][:bigbluebutton_room_attributes][:id] = @space.bigbluebutton_room.id
+      end
+      if @space.update_attributes(params[:space])
+        respond_to do |format|
+          format.html {
+            flash[:success] = t('space.updated')
+            redirect_to request.referer
+          }
+          format.atom { head :ok }
+          format.js{
+            if params[:space][:name] or params[:space][:description]
+              @result = params[:space][:name] ? nil : params[:space][:description]
+              flash[:success] = t('space.updated')
+              render "result.js"
+            elsif !params[:space][:bigbluebutton_room_attributes].blank?
+              if params[:space][:bigbluebutton_room_attributes][:moderator_password] or params[:space][:bigbluebutton_room_attributes][:attendee_password]
+                @result = params[:space][:bigbluebutton_room_attributes][:moderator_password] ? params[:space][:bigbluebutton_room_attributes][:moderator_password] : params[:space][:bigbluebutton_room_attributes][:attendee_password]
+                flash[:success] = t('space.updated')
+                render "result.js"
+              end
+            else
+              render "update.js"
+            end
+          }
+        end
+      else
+        respond_to do |format|
+          flash[:error] = @space.errors.full_messages.to_sentence
+          format.js { render :text => t('error.change') }
+          format.html { redirect_to edit_space_path() }
+          format.xml  { render :xml => @space.errors, :status => :unprocessable_entity }
+          format.atom { render :xml => @space.errors.to_xml, :status => :not_acceptable }
+        end
       end
     end
   end

commit 9875b132e7be4246229ba2962ccb84f8ae52ae79

Fix error introduced by 73b390347d28d in SpacesController
fixes #766

app/controllers/spaces_controller.rb
@@ -186,7 +186,7 @@ class SpacesController < ApplicationController
         }
       end
     else
-      if params[:space][:bigbluebutton_room_attributes].blank?
+      unless params[:space][:bigbluebutton_room_attributes].blank?
         params[:space][:bigbluebutton_room_attributes][:id] = @space.bigbluebutton_room.id
       end
       if @space.update_attributes(params[:space])