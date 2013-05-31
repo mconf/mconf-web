26430d23ea9c3053766c0dc89603084a419a1e94
Fernando Bottin <fbottin@gmail.com>
2013-05-31 14:23:57 -0300

    Remodeling the invite page of a space room.

--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -2,4 +2,5 @@ class CustomBigbluebuttonRoomsController < Bigbluebutton::RoomsController
   before_filter :authentication_required, :except => [:invite, :auth, :running, :external]
   authorization_filter :manage, :current_site, :except => [:invite, :auth, :running, :join, :end, :external, :join_mobile, :fetch_recordings]
   layout 'application', :except => [:join_mobile]
+  layout 'application_without_sidebar', :only => [:invite]
 end
