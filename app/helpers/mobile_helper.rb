ebb8e8a5bc15f7bc79c10bd88d03d06ac8bacaf6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-06-19 16:49:24 -0300

    Better selection of mobile/desktop in the invite view, refs #789
    More information about each option and making it easier to select.
    Also included information on how to download Mconf-Mobile.

---
+++ app/helpers/mobile_helper.rb
@@ -0,0 +1,13 @@
+require 'version'
+
+module MobileHelper
+
+  def mobile_google_play_link
+    "https://play.google.com/store/apps/details?id=org.mconf.android.mconfmobile"
+  end
+
+  def mobile_google_play_image
+    "https://developer.android.com/images/brand/#{I18n.locale}_generic_rgb_wo_45.png"
+  end
+
+end
