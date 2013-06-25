a55d888be1698917d56f1a40a37b6fe458106899
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-06-24 22:03:02 -0300

    Set default timezone to UTC in the migration, forgotten at ffae80678a1

--- db/migrate/20090608143621_timezones.rb
+++ db/migrate/20090608143621_timezones.rb
@@ -1,6 +1,6 @@
 class Timezones < ActiveRecord::Migration
   def self.up
-    add_column :users, :timezone, :string
+    add_column :users, :timezone, :string, :default => 'UTC'
   end
 
   def self.down
