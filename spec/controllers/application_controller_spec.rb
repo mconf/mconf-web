d16bf94300ad9c22aa8e4b343be7c671c680d0fa
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-06-24 21:34:25 -0300

    Block access to rooms of disabled users or spaces
    cherry-pick of 81ccea0e587 and 6f9260debb with a lot of changes.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -11,18 +11,51 @@ describe ApplicationController do
   describe "#set_time_zone" do
 
     # TODO: not sure if tested here or in every action in every controller (sounds bad)
-    pending "is called before every action"
+    it "is called before every action"
 
-    pending "uses the user timezone if specified"
-    pending "uses the site timezone if the user's timezone is not specified"
-    pending "uses UTC if everything fails"
-    pending "ignores the user if there's no current user"
-    pending "ignores the user if the user is not an instance of User"
-    pending "ignores the user if his timezone is not defined"
-    pending "ignores the user if his timezone is an empty string"
-    pending "ignores the site if there's no current site"
-    pending "ignores the site if its timezone is not defined"
-    pending "ignores the site if its timezone is an empty string"
+    it "uses the user timezone if specified"
+    it "uses the site timezone if the user's timezone is not specified"
+    it "uses UTC if everything fails"
+    it "ignores the user if there's no current user"
+    it "ignores the user if the user is not an instance of User"
+    it "ignores the user if his timezone is not defined"
+    it "ignores the user if his timezone is an empty string"
+    it "ignores the site if there's no current site"
+    it "ignores the site if its timezone is not defined"
+    it "ignores the site if its timezone is an empty string"
+  end
+
+  describe "#bigbluebutton_role" do
+    context "for user rooms" do
+      it "if the user is disabled returns nil"
+      context "if the room is private" do
+        it "if the user is the owner returns :moderator"
+        it "if the user is not the owner returns :password"
+        it "if there's no user logged returns :password"
+      end
+      context "if the room is public" do
+        it "if the user is the owner returns :moderator"
+        it "if the user is not the owner returns :guest"
+        it "if there's no user logged returns :guest"
+      end
+    end
+    context "for space rooms" do
+      it "if the space is disabled returns nil"
+      context "if the room is private" do
+        it "if the user is a member of the space returns :moderator"
+        it "if the user is not a member of the space :password"
+      end
+      context "if the room is public" do
+        it "if the user is a member of the space returns :moderator"
+        it "if the user is not a member of the space :guest"
+      end
+    end
+  end
+
+  describe "#bigbluebutton_user" do
+    it "if current_user is defined and is an instance of User, returns it"
+    it "if current_user is not defined returns nil"
+    it "if current_user is not an instance of User returns nil"
   end
 
 end
