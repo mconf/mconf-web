4462cd4690b7423fa31365758480ad8dbaa25820
Lucas Zawacki <lfzawacki@gmail.com>
2013-08-16 11:02:56 -0300

    Using syck for parsing YAML and making delayed_job work again

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require 'yaml'
+YAML::ENGINE.yamler = 'syck'
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
