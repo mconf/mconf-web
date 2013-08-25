b7199685e7c9b2a7013c838c72801316aaa0b85c
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-08-25 12:08:03 -0300

    Fix test errors after cdf5ed1899 and remove more useless images

--- spec/factories/attachment.rb
+++ spec/factories/attachment.rb
@@ -10,6 +10,6 @@ FactoryGirl.define do
   factory :attachment do |a|
     a.association :space, :repository => true
     a.association :author, :factory => :user
-    a.uploaded_data { fixture_file_upload "#{PathHelpers.assets_full_path}/images/vcc-logo.png", "image/png" }
+    a.uploaded_data { fixture_file_upload "#{PathHelpers.assets_full_path}/images/logos/mconf-white-transp.png", "image/png" }
   end
 end
