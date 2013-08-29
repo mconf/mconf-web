af7c641fb287ef6bc297ef37ec29755e668fb255
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-08-29 13:29:35 -0300

    Add gem configatron, refs #800
    For now only to store the available locales, but should be used in the future
    for a lot more.

---
+++ config/initializers/configatron.rb
@@ -0,0 +1,6 @@
+# Put all your default configatron settings here.
+
+# List of locales available in the application.
+# We can't use `I18n.available_locales` because it returns all locales available including the
+# ones included by gems, so if a gem has any locale the application doesn't it, would show up.
+configatron.i18n.default_locales = [:en, :"pt-br"]
