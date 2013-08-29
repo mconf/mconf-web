85e1ffffe2cd645e189f6860d9016bc329c91306
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-08-29 16:04:27 -0300

    Move metadata key names to global configs, refs #804

--- app/helpers/webconference_helper.rb
+++ app/helpers/webconference_helper.rb
@@ -1,6 +1,6 @@
 require 'version'
 
-module MobileHelper
+module WebconferenceHelper
 
   def mobile_google_play_link
     "https://play.google.com/store/apps/details?id=org.mconf.android.mconfmobile"
@@ -8,6 +8,12 @@ module MobileHelper
 
   def mobile_google_play_image
     "https://developer.android.com/images/brand/#{I18n.locale}_generic_rgb_wo_45.png"
+  end
+
+  # Returns a BigbluebuttonMetadata model from the BigbluebuttonRoom `room` that has the
+  # name `name`.
+  def get_room_metadata(room, name)
+    room.metadata.all.select{ |m| m.name == name }.first
   end
 
 end
