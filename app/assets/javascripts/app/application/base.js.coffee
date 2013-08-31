mconf/mconf-web

commit bd94de9e3dbbd992bfce5a17dc6bb4acbff22376

Refactor base.js as a class to rebind events when modals are opened
It still has a lot of simple methods and bindings, but they are now all organized
in a method that's called when modals are shown to bind the events again.
Bindings are also scoped and unbound before binding them again.

app/assets/javascripts/app/application/base.js.coffee
@@ -4,80 +4,102 @@
 # Global object to store our classes, methods, etc.
 window.mconf = {}
 
-$ ->
+# Contains several small javascript components or blocks of code that don't need
+# their own class/file.
+class mconf.Base
 
+  @bind: ->
+    @unbind()
+
+    # Links to open the webconference
+    # Open it in a new borderless window
+    $("a.webconf-start-link:not(.disabled)").on "click.mconfBase", (e) ->
+      window.open $(this)[0].href, "_blank", "resizable=yes"
+      e.preventDefault()
+
+    # Disable the click in any link with the 'disabled' class
+    $("a.disabled").on "click.mconfBase", (e) ->
+      false
+      e.preventDefault()
+
+    # Add a title and tooltip to elements that can only be used by a logged user
+    $(".login-to-enable").each ->
+      $(this).attr("title", "You need to be logged in") # TODO: get from i18n
+      $(this).addClass("tooltipped")
+      $(this).addClass("upwards")
+
+    # Use jquery for placeholders in browsers that don't support it
+    if jQuery().placeholder
+      $('input[placeholder], textarea[placeholder]').placeholder()
+
+    # auto focus the first element with the attribute 'autofocus' (in case the
+    # browser doesn't do it)
+    $('[autofocus]').first().focus()
+
+    # links that automatically collapse or expand blocks inside a parent
+    # div. Ex:
+    # <div id="event_123">
+    #   <div class"block-collapsed">
+    #     i'm collapsed
+    #     <a href="#event_123" class="link-to-expand">more</a>
+    #   </div>
+    #   <div class"block-expanded">
+    #     i'm expanded
+    #     <a href="#event_123" class="link-to-collapse">less</a>
+    #   </div>
+    # </div>
+    $('a.link-to-expand').on "click.mconfBase", (e) ->
+      e.preventDefault()
+      parent = $("#" + $(this).attr("href"))
+      parent.find(".block-collapsed").hide()
+      parent.find(".block-expanded").show()
+    $('a.link-to-collapse').on "click.mconfBase", (e) ->
+      e.preventDefault()
+      parent = $("#" + $(this).attr("href"))
+      parent.find(".block-collapsed").show()
+      parent.find(".block-expanded").hide()
+
+    # Items with this class will only be visible when the item defined
+    # by the id in the 'data-hover-tracked' attribute is hovered. Ex:
+    # <div class="visible-on-hover" data-hover-tracked="event_123"></div>
+    $('.visible-on-hover').each ->
+      target = $(this)
+      tracked = $("#" + $(this).attr("data-hover-tracked"))
+      tracked.on "hover.mconfBase", (e) ->
+        target.show()
+      , (e) ->
+        target.hide()
+
+    # Links with 'data-open-file' will trigger a click
+    # in the input[type=file] element pointed by 'href'
+    $("a[data-open-file]").on "click.mconfBase", (e) ->
+      e.preventDefault()
+      $($(this).attr("href")).click()
+
+    # Links with 'submit-form' will trigger a submit
+    # in the form pointed by 'href'.
+    $("a.submit-form, button.submit-form").on "click.mconfBase", (e) ->
+      e.preventDefault()
+      $($(this).attr("href")).submit()
+
+  @unbind: ->
+    $("a.webconf-start-link:not(.disabled)").off "click.mconfBase"
+    $("a.disabled").off "click.mconfBase"
+    $('a.link-to-expand').off "click.mconfBase"
+    $('a.link-to-collapse').off "click.mconfBase"
+    $('.visible-on-hover').each ->
+      target = $(this)
+      tracked = $("#" + $(this).attr("data-hover-tracked"))
+      tracked.off "hover.mconfBase"
+    $("a[data-open-file]").off "click.mconfBase"
+    $("a.submit-form, button.submit-form").off "click.mconfBase"
+
+
+$ ->
   # Setting I18n-js with the user language
   I18n.locale = $('html').attr "lang"
 
-  # Links to open the webconference
-  # Open it in a new borderless window
-  $("a.webconf-start-link:not(.disabled)").on "click", (e) ->
-    window.open $(this)[0].href, "_blank", "resizable=yes"
-    e.preventDefault()
-
-  # Disable the click in any link with the 'disabled' class
-  $("a.disabled").on "click", (e) ->
-    false
-
-  # Add a title and tooltip to elements that can only be used by a logged user
-  $(".login-to-enable").each (idx) ->
-    $(this).attr("title", "You need to be logged in") # TODO: get from i18n
-    $(this).addClass("tooltipped")
-    $(this).addClass("upwards")
-
-  # Use jquery for placeholders in browsers that don't support it
-  if jQuery().placeholder
-    $('input[placeholder], textarea[placeholder]').placeholder()
-
-  # auto focus the first element with the attribute 'autofocus' (in case the
-  # browser doesn't do it)
-  $('[autofocus]').first().focus()
-
-  # links that automatically collapse or expand blocks inside a parent
-  # div. Ex:
-  # <div id="event_123">
-  #   <div class"block-collapsed">
-  #     i'm collapsed
-  #     <a href="#event_123" class="link-to-expand">more</a>
-  #   </div>
-  #   <div class"block-expanded">
-  #     i'm expanded
-  #     <a href="#event_123" class="link-to-collapse">less</a>
-  #   </div>
-  # </div>
-  $('a.link-to-expand').on "click", (e) ->
-    e.preventDefault()
-    parent = $("#" + $(this).attr("href"))
-    parent.find(".block-collapsed").hide()
-    parent.find(".block-expanded").show()
-  $('a.link-to-collapse').on "click", (e) ->
-    e.preventDefault()
-    parent = $("#" + $(this).attr("href"))
-    parent.find(".block-collapsed").show()
-    parent.find(".block-expanded").hide()
-
-  # Items with this class will only be visible when the item defined
-  # by the id in the 'data-hover-tracked' attribute is hovered. Ex:
-  # <div class="visible-on-hover" data-hover-tracked="event_123"></div>
-  $('.visible-on-hover').each (idx) ->
-    target = $(this)
-    tracked = $("#" + $(this).attr("data-hover-tracked"))
-    tracked.hover (e) ->
-      target.show()
-    , (e) ->
-      target.hide()
-
-  # Links with 'data-open-file' will trigger a click
-  # in the input[type=file] element pointed by 'href'
-  $("a[data-open-file]").on "click", (e) ->
-    e.preventDefault()
-    $($(this).attr("href")).click()
-
-  # Links with 'submit-form' will trigger a submit
-  # in the form pointed by 'href'.
-  $("a.submit-form, button.submit-form").on "click", (e) ->
-    e.preventDefault()
-    $($(this).attr("href")).submit()
+  mconf.Base.bind()
 
 
 # Returns true if we're currently in the view 'action' inside 'controller'

app/assets/javascripts/app/application/resources.js.coffee
@@ -6,6 +6,7 @@
 #
 class mconf.Resources
   @bind: ->
+    mconf.Base.bind()
     mconf.Tooltip.bind()
     mconf.Crop.bind()
     mconf.Dropdown.bind()

commit fa02788ffad986f70314718e09adcb9ab36f0b8d

Fix error on .visible-on-hover
Was binding to the wrong event on jQuery. Error introduced by bd94de9e3d.

app/assets/javascripts/app/application/base.js.coffee
@@ -63,12 +63,10 @@ class mconf.Base
     # by the id in the 'data-hover-tracked' attribute is hovered. Ex:
     # <div class="visible-on-hover" data-hover-tracked="event_123"></div>
     $('.visible-on-hover').each ->
-      target = $(this)
-      tracked = $("#" + $(this).attr("data-hover-tracked"))
-      tracked.on "hover.mconfBase", (e) ->
-        target.show()
-      , (e) ->
-        target.hide()
+      $target = $(this)
+      $tracked = $("#" + $(this).attr("data-hover-tracked"))
+      $tracked.on "mouseenter.mconfBase", (e) -> $target.show()
+      $tracked.on "mouseleave.mconfBase", (e) -> $target.hide()
 
     # Links with 'data-open-file' will trigger a click
     # in the input[type=file] element pointed by 'href'
@@ -88,9 +86,9 @@ class mconf.Base
     $('a.link-to-expand').off "click.mconfBase"
     $('a.link-to-collapse').off "click.mconfBase"
     $('.visible-on-hover').each ->
-      target = $(this)
-      tracked = $("#" + $(this).attr("data-hover-tracked"))
-      tracked.off "hover.mconfBase"
+      $tracked = $("#" + $(this).attr("data-hover-tracked"))
+      $tracked.off "mouseenter.mconfBase"
+      $tracked.off "mouseleave.mconfBase"
     $("a[data-open-file]").off "click.mconfBase"
     $("a.submit-form, button.submit-form").off "click.mconfBase"
 