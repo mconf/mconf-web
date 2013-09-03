c675e8023a42e01be71b9d373e6f2b0a4b09f7bb
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-09-03 18:19:42 -0300

    Update db schema after merging "public_activity", refs #906

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(:version => 20130903173529) do
 
+  create_table "activities", :force => true do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], :name => "index_activities_on_owner_id_and_owner_type"
+  add_index "activities", ["recipient_id", "recipient_type"], :name => "index_activities_on_recipient_id_and_recipient_type"
+  add_index "activities", ["trackable_id", "trackable_type"], :name => "index_activities_on_trackable_id_and_trackable_type"
+
   create_table "attachments", :force => true do |t|
     t.string   "type"
     t.integer  "size"
