mconf/mconf-web

commit 5343a3830c01e2772c81267f9a4d2d7763e3f984

Fix wrong full name set to users on shibboleth login
Was using references to strings in the session when creating a new user. Some
of these strings would be changed internally in the User model and data in
the session would change as well, making the user's full name be actually set
to the value of his login.

app/controllers/shibboleth_controller.rb
@@ -187,7 +187,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get email from session, " +
       "trying field #{current_site.shib_email_field} " +
       "in #{session[:shib_data].inspect}" if email.nil?
-    email
+    email.clone
   end
 
   # Returns the shibboleth user name from the data stored in the session.
@@ -200,7 +200,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get name from session, " +
       "trying field #{current_site.shib_name_field} " +
       "in #{session[:shib_data].inspect}" if name.nil?
-    name
+    name.clone
   end
 
   # Returns the shibboleth login from the data stored in the session.
@@ -213,7 +213,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get login from session, " +
       "trying field #{current_site.shib_login_field} " +
       "in #{session[:shib_data].inspect}" if login.nil?
-    login
+    login.clone
   end
 
   # Returns the shibboleth data stored in the session.
@@ -232,6 +232,11 @@ class ShibbolethController < ApplicationController
 
   def create_account(name, email, login)
     unless User.find_by_email(email)
+      # TODO: if the user is disabled he won't be found and the create below will fail
+
+      logger.info "Shibboleth: creating a new account with name: #{name}, " +
+        "email: #{email}, login: #{login}"
+
       password = SecureRandom.hex(16)
       user = User.create!(:login => login, :email => email,
                           :password => password, :password_confirmation => password)

spec/controllers/shibboleth_controller_spec.rb
@@ -5,6 +5,7 @@ describe ShibbolethController do
   describe "#associate" do
     it "creates a new user account if params[:new_account] is set"
     it "works if the user's name has non UTF-8 characters"
+    it "doesn't change the shibboleth data in the session when creating a new user"
   end
 
 end

commit 4c8ac733db42ec79105f365b96188ba092e96aa2

Fix error introduced by 5343a3830, refs #973
The variables could be nil and then cloning it would throw an error.

app/controllers/shibboleth_controller.rb
@@ -187,6 +187,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get email from session, " +
       "trying field #{current_site.shib_email_field} " +
       "in #{session[:shib_data].inspect}" if email.nil?
+    email = email.clone unless email.nil?
     email.clone
   end
 
@@ -200,6 +201,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get name from session, " +
       "trying field #{current_site.shib_name_field} " +
       "in #{session[:shib_data].inspect}" if name.nil?
+    name = name.clone unless name.nil?
     name.clone
   end
 
@@ -213,6 +215,7 @@ class ShibbolethController < ApplicationController
     logger.info "Shibboleth: couldn't get login from session, " +
       "trying field #{current_site.shib_login_field} " +
       "in #{session[:shib_data].inspect}" if login.nil?
+    login = login.clone unless login.nil?
     login.clone
   end
 