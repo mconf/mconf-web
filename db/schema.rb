3cf671b9e8f53d5f4b72130af26757f10e0af8e5
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-10-27 02:18:59 -0200

    Add a migration to create recent activity entries
    The migration creates activities for the 'create' action of some models.
    This way the list of recent activity won't look so empty after the migration
    to PublicActivity.
    
    refs #906

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019000553) do
+ActiveRecord::Schema.define(:version => 20131027032331) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
