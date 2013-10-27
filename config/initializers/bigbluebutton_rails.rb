85f108b444ea05b7049738fe2b93699fe7cac904
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2013-10-27 02:41:31 -0200

    Fix undefined method 'default' for BigbluebuttonServer
    Was hapenning in development only, because rails unloads initializers and
    doesn't reaload them again. Fixed adding the initializer inside a 'to_prepare'
    block.
    Note that it won't work with 'ActiveSupport.on_load(:after_initialize)' as
    done in some other initializers.
    
    More at: http://stackoverflow.com/questions/5852580/why-do-includes-in-rails-engine-initializers-malfunction-when-cache-classes-fahttp://stackoverflow.com/questions/5852580/why-do-includes-in-rails-engine-initializers-malfunction-when-cache-classes-fahttp://stackoverflow.com/questions/5852580/why-do-includes-in-rails-engine-initializers-malfunction-when-cache-classes-fa

--- config/initializers/bigbluebutton_rails.rb
+++ config/initializers/bigbluebutton_rails.rb
@@ -1,39 +1,43 @@
-# Monkey-patches to add support for guest users in bigbluebutton_rails.
-# TODO: This is not standard in BBB yet, so this should be temporary and might
-#       be moved to bigbluebutton_rails in the future.
-BigbluebuttonRoom.instance_eval do
-  @guest_support = true
-  class << self; attr_reader :guest_support; end
-end
-BigbluebuttonRoom.class_eval do
-  # Copy of the default Bigbluebutton#join_url with support to :guest
-  def join_url(username, role, password=nil)
-    require_server
+Rails.application.config.to_prepare do
 
-    case role
-    when :moderator
-      self.server.api.join_meeting_url(self.meetingid, username, self.moderator_password)
-    when :attendee
-      self.server.api.join_meeting_url(self.meetingid, username, self.attendee_password)
-    when :guest
-      params = { :guest => true }
-      self.server.api.join_meeting_url(self.meetingid, username, self.attendee_password, params)
-    else
-      self.server.api.join_meeting_url(self.meetingid, username, password)
+  # Monkey-patches to add support for guest users in bigbluebutton_rails.
+  # TODO: This is not standard in BBB yet, so this should be temporary and might
+  #       be moved to bigbluebutton_rails in the future.
+  BigbluebuttonRoom.instance_eval do
+    @guest_support = true
+    class << self; attr_reader :guest_support; end
+  end
+  BigbluebuttonRoom.class_eval do
+    # Copy of the default Bigbluebutton#join_url with support to :guest
+    def join_url(username, role, password=nil)
+      require_server
+
+      case role
+      when :moderator
+        self.server.api.join_meeting_url(self.meetingid, username, self.moderator_password)
+      when :attendee
+        self.server.api.join_meeting_url(self.meetingid, username, self.attendee_password)
+      when :guest
+        params = { :guest => true }
+        self.server.api.join_meeting_url(self.meetingid, username, self.attendee_password, params)
+      else
+        self.server.api.join_meeting_url(self.meetingid, username, password)
+      end
     end
   end
-end
 
-BigbluebuttonServer.instance_eval do
+  BigbluebuttonServer.instance_eval do
 
-  # The server used on Mconf-Web is always the first one. We add this method
-  # to wrap it and make it easier to change in the future.
-  def default
-    self.first
+    # The server used on Mconf-Web is always the first one. We add this method
+    # to wrap it and make it easier to change in the future.
+    def default
+      self.first
+    end
+
   end
 
-end
+  BigbluebuttonMeeting.instance_eval do
+    include PublicActivity::Common
+  end
 
-BigbluebuttonMeeting.instance_eval do
-  include PublicActivity::Common
 end
