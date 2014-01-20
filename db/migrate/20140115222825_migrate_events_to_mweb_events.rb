ae163fb20b7ce1ac5b1eafa1eaddeedd4f50cec4
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-01-20 17:48:43 -0200

    Add migration to migrate events to MwebEvents
    Had to rename a few migrations because we have to migrate the events after
    creating tables for MwebEvents but before removing the old events.
    
    refs #1115

---
+++ db/migrate/20140115222825_migrate_events_to_mweb_events.rb
@@ -0,0 +1,85 @@
+# position of each field in the array of an Event
+EVENT_ID = 0
+EVENT_NAME = 1
+EVENT_DESCRIPTION = 2
+EVENT_PLACE = 3
+EVENT_START_DATE = 4
+EVENT_END_DATE = 5
+EVENT_PARENT_ID = 6
+EVENT_CREATED_AT = 7
+EVENT_UPDATED_AT = 8
+EVENT_SPACE_ID = 9
+EVENT_AUTHOR_ID = 10
+EVENT_AUTHOR_TYPE = 11
+EVENT_SPAM = 12
+EVENT_NOTES = 13
+EVENT_LOCATION = 14
+EVENT_PERMALINK = 15
+EVENT_OTHER = 16
+
+# position of each field in the array of a Participant
+PART_ID = 0
+PART_EMAIL = 1
+PART_USER_ID = 2
+PART_EVENT_ID = 3
+PART_CREATED_AT = 4
+PART_UPDATED_AT = 5
+PART_ATTEND = 6
+
+class MigrateEventsToMwebEvents < ActiveRecord::Migration
+  def up
+    sql = "SELECT * FROM events"
+    events = ActiveRecord::Base.connection.execute(sql)
+    events.each do |event|
+      puts "Migrating the event: #{event}"
+      new_event = MwebEvents::Event.new
+      new_event.location = event[EVENT_LOCATION]
+      new_event.start_on = event[EVENT_START_DATE]
+      new_event.end_on = event[EVENT_END_DATE]
+      new_event.description = event[EVENT_DESCRIPTION]
+      new_event.name = event[EVENT_NAME]
+      if event[EVENT_SPACE_ID]
+        space = Space.find_by_id(event[EVENT_SPACE_ID])
+        if space
+          new_event.owner_id = space.id
+          new_event.owner_type = 'Space'
+        end
+      else
+        new_event.owner_id = event[EVENT_AUTHOR_ID]
+        new_event.owner_type = event[EVENT_AUTHOR_TYPE]
+        unless event[EVENT_AUTHOR_TYPE] == "User"
+          puts "*** WARNING: author type is not 'User', new event might have a wrong owner"
+        end
+      end
+      new_event.description = event[EVENT_DESCRIPTION]
+      new_event.permalink = event[EVENT_PERMALINK]
+      new_event.time_zone = Site.current.timezone
+      if new_event.save
+        sql = "SELECT * FROM participants WHERE event_id = #{event[EVENT_ID]}"
+        participants = ActiveRecord::Base.connection.execute(sql)
+        participants.each do |participant|
+          if participant[PART_ATTEND] == 1
+            new_participant = MwebEvents::Participant.new
+            new_participant.owner = User.find_by_id(participant[PART_USER_ID])
+            new_participant.event_id = new_event.id
+            new_participant.email = participant[PART_EMAIL]
+            unless new_participant.save
+              puts "Could not migrate the participant: #{participant}"
+            else
+              puts "Successfully migrated the participant: #{participant[PART_EMAIL]}"
+            end
+          else
+            puts "Participant was not attending, won't be migrated: #{participant[PART_EMAIL]}"
+          end
+        end
+        puts "Successfully migrated the event: #{event[EVENT_NAME]}"
+      else
+        puts "Could not migrate the event: #{event}"
+      end
+    end
+  end
+
+  def down
+    throw ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140115222824) do
+ActiveRecord::Schema.define(:version => 20140115222827) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
