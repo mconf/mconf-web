b86e37196ba1cf6d2bf8268179ebc72239294115
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-02-09 20:13:17 -0200

    Adjustments for posts and priv messages by disabled users
    Prevent breaks and show information that the user was removed.

--- app/helpers/logo_images_helper.rb
+++ app/helpers/logo_images_helper.rb
@@ -2,20 +2,32 @@ module LogoImagesHelper
 
   # TODO: If `options[:size]` is wrong, the image will not be found and the application
   #   will crash in production. This method should check this option to prevent this error.
-  def logo_image(resource, options = {})
+  def logo_image(resource, options={})
     if resource.is_a?(User)
       options[:size] = '128' if options[:size] > '32'
-      path_no_image = "default_logos/" + options[:size] + "/user.png"
+      model_type = :user
     elsif resource.is_a?(Space)
-      path_no_image = "default_logos/" + options[:size] + "/space.png"
+      model_type = :space
     end
     size = ("logo" + options[:size]).to_sym
     resource.logo_image.present? ?
       image_tag(resource.logo_image_url(size), options) :
-      image_tag(path_no_image, :class => options[:class], :title => options[:title])
+      empty_logo_image(model_type, options)
   end
 
-  def link_logo_image(resource, options = {})
+  def empty_logo_image(resource, options={})
+    case resource
+    when :user
+      options[:size] = '128' if options[:size] > '32'
+      path_no_image = "default_logos/" + options[:size] + "/user.png"
+    when :space
+      path_no_image = "default_logos/" + options[:size] + "/space.png"
+    end
+    size = ("logo" + options[:size]).to_sym
+    image_tag(path_no_image, :class => options[:class], :title => options[:title])
+  end
+
+  def link_logo_image(resource, options={})
     optionsImg = options.clone
     options[:url] ||= resource # url defaults to the resource's show
     link_to options[:url], :class => options[:class], :id => options[:id] do
@@ -23,7 +35,7 @@ module LogoImagesHelper
     end
   end
 
-  def logo_image_removed(options = {})
+  def logo_image_removed(options={})
     options[:class] = options.has_key?(:class) ? "#{options[:class]} logo logo-removed" : 'logo logo-removed'
     image_tag("icons/image_removed.png", :class => options[:class], :title => options[:title], :size => options[:size])
   end
