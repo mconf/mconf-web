af1d2f017ce0b70b0a2730a1644358a4395b7aa1
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-02-20 14:59:40 -0300

    Fix merge errors from ba6ac8af7b

--- db/migrate/20140120194245_create_recent_activities.rb
+++ db/migrate/20140120194245_create_recent_activities.rb
@@ -31,11 +31,11 @@ class CreateRecentActivities < ActiveRecord::Migration
     end
 
     puts "CreateRecentActivities: creating activities for Events"
-    Event.all.each do |event|
+    MwebEvents::Event.all.each do |event|
       unless PublicActivity::Activity.where(:key => 'event.create', :trackable_id => event.id).length > 0
-        owner = event.author
+        owner = event.owner
         if owner
-          activity = event.create_activity "create", :owner => event.space, :parameters => { :user_id => owner.id, :username => owner.name }
+          activity = event.create_activity "create", :owner => owner, :parameters => { :user_id => owner.id, :username => owner.name }
           activity.created_at = event.created_at
           activity.updated_at = event.created_at
           activity.save
