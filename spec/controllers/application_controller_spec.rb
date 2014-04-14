mconf/mconf-web

commit c66a9a41ab7755715dd671674030dbb79c8609a9

Update bigbluebutton_rails
Improves the way we prevent people from recording meetings.

app/controllers/application_controller.rb
@@ -135,31 +135,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # This method is called from BigbluebuttonRails
+  # This method is called from BigbluebuttonRails.
+  # Returns whether the current user can create a meeting in 'room'.
   def bigbluebutton_can_create?(room, role)
     ability = Abilities.ability_for(current_user)
-    can_create = ability.can?(:create_meeting, room)
+    ability.can?(:create_meeting, room)
+  end
 
-    # if the user can create the meeting we have to check whether the record flag will be
-    # set or not
-    # TODO: this would be better if it was possible to send this flag in the create call to
-    #   BigbluebuttonRails, not by changing the attribute in the db.
-    if can_create
-      can_record = ability.can?(:record_meeting, room)
+  # This method is called from BigbluebuttonRails.
+  # Returns a hash with options to override the options saved in the database when creating
+  # a meeting in the room 'room'.
+  def bigbluebutton_create_options(room)
+    ability = Abilities.ability_for(current_user)
 
-      # with this option set, we always set record the flag according to the user's permissions
-      if Site.current.webconf_auto_record
-        room.update_attribute(:record, can_record)
-
-      # in this case the user has to set or unset the recording flag himself, so we just make
-      # sure that if he can't record the flag is unset, otherwise leave it as it is
-      else
-        room.update_attribute(:record, false) unless can_record
-      end
-
-    end
-
-    can_create
+    # only enable recording if the room is set to record and if the user has permissions to
+    # used to forcibly disable recording if a user has no permission but the room is set to record
+    can_record = ability.can?(:record_meeting, room)
+    record = room.record && can_record
+    { :record => record }
   end
 
   # loads the web conference room for the current space into `@webconf_room` and fetches information

spec/controllers/application_controller_spec.rb
@@ -99,63 +99,69 @@ describe ApplicationController do
         end
       end
 
-      context "when the flag 'auto record flag' is set in the site" do
-        before { Site.current.update_attributes(:webconf_auto_record => true) }
+    end
+  end
 
-        context "if the user cannot record, automatically sets the record flag to false" do
-          before {
-            room.update_attributes(:record => true)
-            @ability.can :create_meeting, room
-            BigbluebuttonRoom.stub(:find_by_id).and_return(room)
-          }
-          before(:each) { get :index, :room_id => room.id, :role => :moderator }
-          it { room.record.should be_false }
+  # TODO: We are comparing the results with hashes using string keys, even though
+  #   the method returns keys using symbols. Using `assigns()` automatically converts
+  #   the symbols to strings, so we can't check it exactly as we should. See:
+  #   http://stackoverflow.com/questions/4348195/unwanted-symbol-to-string-conversion-of-hash-key
+  describe "#bigbluebutton_create_options" do
+    controller do
+      def index
+        room = BigbluebuttonRoom.find_by_id(params[:room_id])
+        @result = bigbluebutton_create_options(room).freeze
+        render :nothing => true
+      end
+    end
+
+    context "if there's no user logged returns false" do
+      let(:room) { FactoryGirl.create(:bigbluebutton_room) }
+      before(:each) { get :index, :room_id => room.id }
+      it { assigns(:result).should eql({ "record" => false }) }
+    end
+
+    context "if there's a user logged" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:room) { FactoryGirl.create(:bigbluebutton_room) }
+      before {
+        # a custom ability to control what the user can do
+        @ability = Object.new
+        @ability.extend(CanCan::Ability)
+        Abilities.stub(:ability_for).and_return(@ability)
+      }
+
+      context "when the user can record" do
+        before { @ability.can :record_meeting, room }
+
+        context "and the room is set to record" do
+          before { room.update_attributes("record" => true) }
+          before(:each) { get :index, :room_id => room.id }
+          it { assigns(:result).should eql({ "record" => true }) }
         end
 
-        context "if the user can record, automatically sets the record flag to true" do
-          before {
-            room.update_attributes(:record => false)
-            @ability.can :create_meeting, room
-            @ability.can :record_meeting, room
-            BigbluebuttonRoom.stub(:find_by_id).and_return(room)
-          }
-          before(:each) { get :index, :room_id => room.id, :role => :moderator }
-          it { room.record.should be_true }
+        context "and the room is not set to record" do
+          before { room.update_attributes("record" => false) }
+          before(:each) { get :index, :room_id => room.id }
+          it { assigns(:result).should eql({ "record" => false }) }
         end
       end
 
-      context "when the flag 'auto record flag' is not set in the site" do
-        before { Site.current.update_attributes(:webconf_auto_record => false) }
+      context "when the user cannot record" do
+        before { @ability.cannot :record_meeting, room }
 
-        context "if the user cannot record, sets the record flag to false" do
-          before {
-            room.update_attributes(:record => true)
-            @ability.can :create_meeting, room
-            BigbluebuttonRoom.stub(:find_by_id).and_return(room)
-          }
-          before(:each) { get :index, :room_id => room.id, :role => :moderator }
-          it { room.record.should be_false }
+        context "and the room is set to record" do
+          before { room.update_attributes("record" => true) }
+          before(:each) { get :index, :room_id => room.id }
+          it { assigns(:result).should eql({ "record" => false }) }
         end
 
-        context "if the user can record, leaves the record flag as it was before" do
-          [false, true].each do |value|
-            context "when it was #{value}" do
-              before {
-                room.update_attributes(:record => value) # initial value
-                @ability.can :create_meeting, room
-                @ability.can :record_meeting, room
-                BigbluebuttonRoom.stub(:find_by_id).and_return(room)
-              }
-              before(:each) {
-                room.should_not_receive(:update_attribute).with(:record, false)
-                get :index, :room_id => room.id, :role => :moderator
-              }
-              it { room.record.should be(value) }
-            end
-          end
+        context "and the room is not set to record" do
+          before { room.update_attributes("record" => false) }
+          before(:each) { get :index, :room_id => room.id }
+          it { assigns(:result).should eql({ "record" => false }) }
         end
       end
-
     end
   end
 

app/models/abilities.rb
@@ -245,8 +245,7 @@ module Abilities
       # some actions in rooms should be accessible to any logged user
       # some of them will do the authorization themselves (e.g. permissions for :join
       # will change depending on the user and the target room)
-      can [:invite, :invite_userid, :auth, :running,
-           :join, :external, :external_auth, :join_mobile], BigbluebuttonRoom
+      can [:invite, :invite_userid, :auth, :running, :join, :join_mobile], BigbluebuttonRoom
 
       # a user can play recordings of his own room or recordings of
       # rooms of either public spaces or spaces he's a member of

spec/controllers/custom_bigbluebutton_rooms_controller_spec.rb
@@ -103,9 +103,8 @@ describe CustomBigbluebuttonRoomsController do
           BigbluebuttonRoom.stub(:find_by_param) { room }
 
           # to guide the behavior of #auth, copied from the tests in BigbluebuttonRails
-          room.should_receive(:fetch_is_running?)
-          room.should_receive(:is_running?).and_return(false)
-          room.should_receive(:create_meeting).with(user.name, user.id, anything)
+          room.should_receive(:fetch_is_running?).and_return(false)
+          room.should_receive(:create_meeting).with(user, anything, anything)
           room.should_receive(:fetch_new_token)
           room.should_receive(:join_url).and_return("http://test.com/attendee/join")
         end
@@ -277,22 +276,6 @@ describe CustomBigbluebuttonRoomsController do
     end
   end
 
-  describe "#external" do
-    context "template and layout" do
-      let(:server) { FactoryGirl.create(:bigbluebutton_server) }
-      before(:each) { login_as(FactoryGirl.create(:superuser)) }
-      before(:each) { get :external, :meeting => "my-meeting-id", :server_id => server.id }
-      it { should render_template(:external) }
-      it { should render_with_layout("application") }
-    end
-  end
-
-  describe "#external_auth" do
-    context "template and layout" do
-      pending "render with layout application"
-    end
-  end
-
   # TODO: this view is not in the application yet, only in the gem
   # describe "#recordings" do
   #   context "template and layout" do
@@ -373,8 +356,6 @@ describe CustomBigbluebuttonRoomsController do
         it { should allow_access_to(:auth, hash).via(:post) }
         it { should allow_access_to(:invite, hash) }
         it { should allow_access_to(:invite_userid, hash).redirecting_to(invite_bigbluebutton_room_path(room)) }
-        it { should allow_access_to(:external, hash_with_server) }
-        it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
         it { should allow_access_to(:end, hash) }
         it { should allow_access_to(:join_mobile, hash) }
         it { should allow_access_to(:running, hash) }
@@ -443,8 +424,6 @@ describe CustomBigbluebuttonRoomsController do
         it { should allow_access_to(:auth, hash).via(:post) }
         it { should allow_access_to(:invite, hash) }
         it { should allow_access_to(:invite_userid, hash).redirecting_to(invite_bigbluebutton_room_path(room)) }
-        it { should allow_access_to(:external, hash_with_server) }
-        it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
         it { should allow_access_to(:end, hash) }
         it { should allow_access_to(:join_mobile, hash) }
         it { should allow_access_to(:running, hash) }
@@ -460,8 +439,6 @@ describe CustomBigbluebuttonRoomsController do
         it { should_not allow_access_to(:edit, hash) }
         it { should_not allow_access_to(:update, hash).via(:put) }
         it { should_not allow_access_to(:destroy, hash).via(:delete) }
-        it { should allow_access_to(:external, hash_with_server) }
-        it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
         it { should allow_access_to(:join, hash) }
         it { should allow_access_to(:auth, hash).via(:post) }
         it { should allow_access_to(:invite, hash) }
@@ -485,8 +462,6 @@ describe CustomBigbluebuttonRoomsController do
           it { should_not allow_access_to(:edit, hash) }
           it { should_not allow_access_to(:update, hash).via(:put) }
           it { should_not allow_access_to(:destroy, hash).via(:delete) }
-          it { should allow_access_to(:external, hash_with_server) }
-          it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
           it { should allow_access_to(:join, hash) }
           it { should allow_access_to(:auth, hash).via(:post) }
           it { should allow_access_to(:invite, hash) }
@@ -505,8 +480,6 @@ describe CustomBigbluebuttonRoomsController do
           it { should_not allow_access_to(:edit, hash) }
           it { should_not allow_access_to(:update, hash).via(:put) }
           it { should_not allow_access_to(:destroy, hash).via(:delete) }
-          it { should allow_access_to(:external, hash_with_server) }
-          it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
           it { should allow_access_to(:join, hash) }
           it { should allow_access_to(:auth, hash).via(:post) }
           it { should allow_access_to(:invite, hash) }
@@ -531,8 +504,6 @@ describe CustomBigbluebuttonRoomsController do
           it { should_not allow_access_to(:edit, hash) }
           it { should_not allow_access_to(:update, hash).via(:put) }
           it { should_not allow_access_to(:destroy, hash).via(:delete) }
-          it { should allow_access_to(:external, hash_with_server) }
-          it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
           it { should allow_access_to(:join, hash) }
           it { should allow_access_to(:auth, hash).via(:post) }
           it { should allow_access_to(:invite, hash) }
@@ -551,8 +522,6 @@ describe CustomBigbluebuttonRoomsController do
           it { should_not allow_access_to(:edit, hash) }
           it { should_not allow_access_to(:update, hash).via(:put) }
           it { should_not allow_access_to(:destroy, hash).via(:delete) }
-          it { should allow_access_to(:external, hash_with_server) }
-          it { should allow_access_to(:external_auth, hash_with_server).via(:post) }
           it { should allow_access_to(:join, hash) }
           it { should allow_access_to(:auth, hash).via(:post) }
           it { should allow_access_to(:invite, hash) }
@@ -588,8 +557,6 @@ describe CustomBigbluebuttonRoomsController do
         it { should allow_access_to(:auth, hash).via(:post) }
         it { should allow_access_to(:invite, hash).redirecting_to(join_webconf_path(room)) }
         it { should allow_access_to(:invite_userid, hash) }
-        it { should require_authentication_for(:external, hash_with_server) }
-        it { should require_authentication_for(:external_auth, hash_with_server).via(:post) }
         it { should require_authentication_for(:end, hash) }
         it { should require_authentication_for(:join_mobile, hash) }
         it { should allow_access_to(:running, hash) }

spec/controllers/space_events_controller_spec.rb
@@ -9,7 +9,6 @@ require "spec_helper"
 describe SpaceEventsController, :events => true do
 
   before(:each, :events => true) do
-    puts "======================================"
     Site.current.update_attributes(:events_enabled => true)
   end
 

spec/models/bigbluebutton_room_spec.rb
@@ -20,8 +20,8 @@ describe BigbluebuttonRoom do
   # so we test them here.
   describe "abilities", :abilities => true do
     set_custom_ability_actions([ :end, :join_options, :create_meeting, :fetch_recordings,
-                                 :invite, :invite_userid, :auth, :running, :join, :external,
-                                 :external_auth, :join_mobile, :record_meeting, :invitation,
+                                 :invite, :invite_userid, :auth, :running, :join,
+                                 :join_mobile, :record_meeting, :invitation,
                                  :send_invitation ])
 
     subject { ability }
@@ -87,8 +87,8 @@ describe BigbluebuttonRoom do
       context "in his own room" do
         let(:target) { user.bigbluebutton_room }
         let(:allowed) { [:end, :join_options, :create_meeting, :fetch_recordings,
-                         :invite, :invite_userid, :auth, :running, :join, :external,
-                         :external_auth, :join_mobile, :update, :invitation, :send_invitation] }
+                         :invite, :invite_userid, :auth, :running, :join,
+                         :join_mobile, :update, :invitation, :send_invitation] }
         it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
         context "with permission to record" do
@@ -100,8 +100,7 @@ describe BigbluebuttonRoom do
       context "in another user's room" do
         let(:another_user) { FactoryGirl.create(:user) }
         let(:target) { another_user.bigbluebutton_room }
-        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :external,
-                         :external_auth, :join_mobile] }
+        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :join_mobile] }
         it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
         context "with permission to record" do
@@ -115,8 +114,7 @@ describe BigbluebuttonRoom do
         let(:target) { space.bigbluebutton_room }
 
         context "he doesn't belong to" do
-          let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :external,
-                           :external_auth, :join_mobile] }
+          let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :join_mobile] }
           it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
           context "with permission to record" do
@@ -128,8 +126,8 @@ describe BigbluebuttonRoom do
         context "he belongs to" do
           before { space.add_member!(user) }
           let(:allowed) { [:end, :join_options, :create_meeting, :fetch_recordings,
-                           :invite, :invite_userid, :auth, :running, :join, :external,
-                           :external_auth, :join_mobile, :invitation, :send_invitation] }
+                           :invite, :invite_userid, :auth, :running, :join, :join_mobile,
+                           :invitation, :send_invitation] }
           it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
           context "with permission to record" do
@@ -144,8 +142,7 @@ describe BigbluebuttonRoom do
         let(:target) { space.bigbluebutton_room }
 
         context "he doesn't belong to" do
-          let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :external,
-                           :external_auth, :join_mobile] }
+          let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :join_mobile] }
           it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
           context "with permission to record" do
@@ -157,8 +154,8 @@ describe BigbluebuttonRoom do
         context "he belongs to" do
           before { space.add_member!(user) }
           let(:allowed) { [:end, :join_options, :create_meeting, :fetch_recordings,
-                           :invite, :invite_userid, :auth, :running, :join, :external,
-                           :external_auth, :join_mobile, :invitation, :send_invitation] }
+                           :invite, :invite_userid, :auth, :running, :join, :join_mobile,
+                           :invitation, :send_invitation] }
           it { should_not be_able_to_do_anything_to(target).except(allowed) }
 
           context "with permission to record" do
@@ -170,15 +167,13 @@ describe BigbluebuttonRoom do
 
       context "for a room without owner" do
         let(:target) { FactoryGirl.create(:bigbluebutton_room, :owner => nil) }
-        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :external,
-                         :external_auth, :join_mobile] }
+        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :join_mobile] }
         it { should_not be_able_to_do_anything_to(target).except(allowed) }
       end
 
       context "for a room with an invalid owner_type" do
         let(:target) { FactoryGirl.create(:bigbluebutton_room, :owner_type => "invalid type") }
-        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :external,
-                         :external_auth, :join_mobile] }
+        let(:allowed) { [:invite, :invite_userid, :auth, :running, :join, :join_mobile] }
         it { should_not be_able_to_do_anything_to(target).except(allowed) }
       end
     end

commit 868ab53a5fa7e468971efc1bf929b064c1d2f47e

Fix setting the record flag in webconfs, broken by c66a9a41ab
Wasn't considering the Site's flag "webconf_auto_record" as it should.

app/controllers/application_controller.rb
@@ -138,11 +138,16 @@ class ApplicationController < ActionController::Base
   def bigbluebutton_create_options(room)
     ability = Abilities.ability_for(current_user)
 
-    # only enable recording if the room is set to record and if the user has permissions to
-    # used to forcibly disable recording if a user has no permission but the room is set to record
     can_record = ability.can?(:record_meeting, room)
-    record = room.record && can_record
-    { :record => record }
+    if Site.current.webconf_auto_record
+      # show the record button if the user has permissions to record
+      { :record => can_record }
+    else
+      # only enable recording if the room is set to record and if the user has permissions to
+      # used to forcibly disable recording if a user has no permission but the room is set to record
+      record = room.record && can_record
+      { :record => record }
+    end
   end
 
   # loads the web conference room for the current space into `@webconf_room` and fetches information

spec/controllers/application_controller_spec.rb
@@ -134,33 +134,76 @@ describe ApplicationController do
       context "when the user can record" do
         before { @ability.can :record_meeting, room }
 
-        context "and the room is set to record" do
-          before { room.update_attributes("record" => true) }
-          before(:each) { get :index, :room_id => room.id }
-          it { assigns(:result).should eql({ "record" => true }) }
+        context "and the site is set to auto set the record flag" do
+          before { Site.current.update_attributes(:webconf_auto_record => true) }
+
+          context "and the room is set to record" do
+            before { room.update_attributes("record" => true) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => true }) }
+          end
+
+          context "and the room is not set to record" do
+            before { room.update_attributes("record" => false) }
+            before(:each) { get :index, :room_id => room.id }
+            # uses the user's permission only, ignores that the room is not set to record
+            it { assigns(:result).should eql({ "record" => true }) }
+          end
         end
 
-        context "and the room is not set to record" do
-          before { room.update_attributes("record" => false) }
-          before(:each) { get :index, :room_id => room.id }
-          it { assigns(:result).should eql({ "record" => false }) }
+        context "and the site is not set to auto set the record flag" do
+          before { Site.current.update_attributes(:webconf_auto_record => false) }
+
+          context "and the room is set to record" do
+            before { room.update_attributes("record" => true) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => true }) }
+          end
+
+          context "and the room is not set to record" do
+            before { room.update_attributes("record" => false) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => false }) }
+          end
         end
+
       end
 
       context "when the user cannot record" do
         before { @ability.cannot :record_meeting, room }
 
-        context "and the room is set to record" do
-          before { room.update_attributes("record" => true) }
-          before(:each) { get :index, :room_id => room.id }
-          it { assigns(:result).should eql({ "record" => false }) }
+        context "and the site is set to auto set the record flag" do
+          before { Site.current.update_attributes(:webconf_auto_record => true) }
+
+          context "and the room is set to record" do
+            before { room.update_attributes("record" => true) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => false }) }
+          end
+
+          context "and the room is not set to record" do
+            before { room.update_attributes("record" => false) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => false }) }
+          end
         end
 
-        context "and the room is not set to record" do
-          before { room.update_attributes("record" => false) }
-          before(:each) { get :index, :room_id => room.id }
-          it { assigns(:result).should eql({ "record" => false }) }
+        context "and the site is not set to auto set the record flag" do
+          before { Site.current.update_attributes(:webconf_auto_record => false) }
+
+          context "and the room is set to record" do
+            before { room.update_attributes("record" => true) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => false }) }
+          end
+
+          context "and the room is not set to record" do
+            before { room.update_attributes("record" => false) }
+            before(:each) { get :index, :room_id => room.id }
+            it { assigns(:result).should eql({ "record" => false }) }
+          end
         end
+
       end
     end
   end