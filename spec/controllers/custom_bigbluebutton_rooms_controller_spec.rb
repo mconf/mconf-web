c002a62e95ab6ac499c52b5ead246fb131459b84
Fernando Bottin <fbottin@gmail.com>
2014-07-29 11:33:34 -0400

    Added the new allowed parameters in the controller to the tests.

--- spec/controllers/custom_bigbluebutton_rooms_controller_spec.rb
+++ spec/controllers/custom_bigbluebutton_rooms_controller_spec.rb
@@ -203,8 +203,8 @@ describe CustomBigbluebuttonRoomsController do
         before(:each) { login_as(user) }
 
         let(:allowed_params) {
-          [ :attendee_password, :moderator_password, :private, :record, :default_layout, :presenter_share_only,
-            :metadata_attributes => [ :id, :name, :content, :_destroy, :owner_id ] ]
+          [ :attendee_password, :moderator_password, :private, :record_meeting, :default_layout, :presenter_share_only,
+            :auto_start_video, :auto_start_audio, :welcome_msg, :metadata_attributes => [ :id, :name, :content, :_destroy, :owner_id ] ]
         }
         it {
           BigbluebuttonRoom.stub(:find_by_param).and_return(room)
