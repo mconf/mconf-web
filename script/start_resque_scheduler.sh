563e4858399b165699e09da5296962ed9b68bb62
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-08-20 16:28:01 -0300

    Update monit scripts to run resque and resque-scheduler

---
+++ script/start_resque_scheduler.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This file is part of Mconf-Web, a web application that provides access
+# to the Mconf webconferencing system. Copyright (C) 2010-2013 Mconf
+#
+# This file is licensed under the Affero General Public License version
+# 3 or later. See the LICENSE file.
+
+# Wrapper script to run resque-scheduler from monit
+# Arguments:
+#   $1: (start|stop)
+# Example:
+#   start_resque_scheduler.sh start
+
+USER="$(id -u -n)"
+APP_PATH="$(dirname $0)/.."
+PATH=/home/$USER/.rbenv/bin:/home/$USER/.rbenv/shims:$PATH
+RAILS_ENV=production
+PIDFILE=$APP_PATH/tmp/pids/resque_scheduler.pid
+LOGFILE=$APP_PATH/log/resque_scheduler.log
+
+cd $APP_PATH
+
+if [ "$1" != "stop" ]; then
+  /usr/bin/env bundle exec rake environment resque:scheduler RAILS_ENV=$RAILS_ENV VERBOSE=1 PIDFILE=$PIDFILE >> $LOGFILE 2>&1
+else
+  kill -9 $(cat $PIDFILE) && rm -f $PIDFILE; exit 0;
+fi
