39e93349fc4a9ea80f344af94a5c643052837128
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-08-31 16:40:29 -0300

    Tests for confirmation email, with changes in the test suite for it
    A continuation of 5125f60.
    Had to make some changes in the test suite to be able to send and test emails
    with devise and resque.
    Disabled use_transactional_fixtures and started using DatabaseCleaner to
    clean up the test database. Most of the times it will still use transactions,
    but it can be changed to truncation if we need it.
    
    refs #1322

--- spec/features/users/user_confirmation_email.rb
+++ spec/features/users/user_confirmation_email.rb
@@ -5,47 +5,58 @@ feature "Confirmation email" do
   background do
     @attributes = FactoryGirl.attributes_for(:user).slice(:username, :_full_name, :email, :password)
   end
-  scenario "register the user and check if the confirmation token is correct" do
-    skip
-    visit register_path
 
-    fill_in "user[email]", with: @attributes[:email]
-    fill_in "user[_full_name]", with: @attributes[:_full_name]
-    fill_in "user[username]", with: @attributes[:username]
-    fill_in "user[password]", with: @attributes[:password]
-    fill_in "user[password_confirmation]", with: @attributes[:password]
+  # devise triggers callbacks to send emails that will not be triggered if using
+  # transactions, so use truncation instead
+  scenario "sends the correct confirmation link in the confirmation email", with_truncation: true do
+    with_resque do
+      expect {
+        visit register_path
+        fill_in "user[email]", with: @attributes[:email]
+        fill_in "user[_full_name]", with: @attributes[:_full_name]
+        fill_in "user[username]", with: @attributes[:username]
+        fill_in "user[password]", with: @attributes[:password]
+        fill_in "user[password_confirmation]", with: @attributes[:password]
+        click_button "Register"
+      }.to change{ User.count }.by(1)
+    end
 
-    click_button "Register"
+    User.last.confirmed?.should be false
 
-    #show_page
-    token = extract_confirmation_token last_email
-    user = User.find_by_username(@attributes[:username])
-    #TODO: verify if the token sent in the email corresponds to that one in the database
+    # check the confirmation email and click on the link to confirm the account
+    last_email.should_not be_nil
+    confirmation_link = last_email.body.encoded.match(/http.*users\/confirmation[^" ]*/)[0]
+    last_email.body.encoded.should match(t('devise.mailer.confirmation_instructions.confirmation_ok'))
+    visit confirmation_link
+
+    User.last.confirmed?.should be true
+
+    # TODO: check that the user is not signed in
   end
 
-  scenario "verify if the email changes according to require_admin_approval" do
-    skip
-    visit register_path
+  scenario "send the correct message and no confirmation link if admin approval is required", with_truncation: true do
+    Site.current.update_attributes(require_registration_approval: true)
 
-    fill_in "user[email]", with: @attributes[:email]
-    fill_in "user[_full_name]", with: @attributes[:_full_name]
-    fill_in "user[username]", with: @attributes[:username]
-    fill_in "user[password]", with: @attributes[:password]
-    fill_in "user[password_confirmation]", with: @attributes[:password]
+    with_resque do
+      expect {
+        visit register_path
+        fill_in "user[email]", with: @attributes[:email]
+        fill_in "user[_full_name]", with: @attributes[:_full_name]
+        fill_in "user[username]", with: @attributes[:username]
+        fill_in "user[password]", with: @attributes[:password]
+        fill_in "user[password_confirmation]", with: @attributes[:password]
+        click_button "Register"
+      }.to change{ User.count }.by(1)
+    end
 
-    click_button "Register"
+    User.last.confirmed?.should be false
+    User.last.approved?.should be false
 
-    #show_page
-    token = extract_confirmation_token last_email
-    user = User.find_by_username(@attributes[:username])
-    #TODO: verify if the email content changes according to the flag require_admin_approval
+    # check the confirmation email
+    last_email.should_not be_nil
+    last_email.body.encoded.should_not match(/http.*users\/confirmation*/)
+    last_email.body.encoded.should match(t('devise.mailer.confirmation_instructions.confirmation_pending'))
+
+    # TODO: check that the user is not signed in
   end
-end
-
-def last_email
-  ActionMailer::Base.deliveries.last
-end
-
-def extract_confirmation_token(email)
-  email && email.body && email.body.match(/confirmation_token=(.+)">/x)[1]
 end
