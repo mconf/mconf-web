774aeac85a23ef19b42f260871bc7e36cecd6751
Lucas Zawacki <lfzawacki@gmail.com>
2014-09-03 15:41:19 -0300

    Change find syntax to where in spaces#webconf_room

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -233,7 +233,7 @@ class SpacesController < ApplicationController
     @webconf_attendees = []
     unless @webconf_room.attendees.nil?
       @webconf_room.attendees.each do |attendee|
-        profile = Profile.find(:all, :conditions => { "full_name" => attendee.full_name }).first
+        profile = Profile.where(:full_name => attendee.full_name).first
         unless profile.nil?
           @webconf_attendees << profile.user
         end
