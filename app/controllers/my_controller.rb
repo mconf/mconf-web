1c61f30145ea76da005f08b1495a82091ed9c529
Lucas Zawacki <lfzawacki@gmail.com>
2014-09-19 14:37:38 -0300

    Show temporary page for users not yet approved. refs #1062

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -8,7 +8,7 @@
 # This controller includes actions that are specific for the current user and shouldn't be
 # accessed by anybody else (e.g. home, recordings, activity, etc).
 class MyController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:approval_pending]
   respond_to :json, :only => [:rooms]
   respond_to :html, :except => [:rooms]
 
@@ -40,6 +40,8 @@ class MyController < ApplicationController
       else
         "application"
       end
+    when :pending_approval
+      "error"
     else
       "application"
     end
@@ -51,6 +53,13 @@ class MyController < ApplicationController
     @contents_per_page = 15
     @all_contents = current_user.all_activity.limit(@contents_per_page).order('updated_at DESC')
     @private_messages = current_user.unread_private_messages
+  end
+
+  def approval_pending
+    # Don't show it unless user is coming from a login or register
+    unless [login_url, register_url, root_url].include? request.referrer
+      redirect_to root_path
+    end
   end
 
   def activity
