57260a7ed99d47cebc15ad1f61544af026b528dc
Lucas Zawacki <lfzawacki@gmail.com>
2014-10-06 17:39:31 -0300

    Send devise emails using the correct locale. refs #1309

--- config/initializers/resque_mailer.rb
+++ config/initializers/resque_mailer.rb
@@ -7,3 +7,24 @@ Resque::Mailer.error_handler = lambda { |mailer, message, error, action, args|
     raise error
   end
 }
+
+# Wrap devise email calls in the current locale for the user being emailed
+# and in case of a new user, use the site's locale
+module Devise
+  module Async
+    module Backend
+      class Resque < Base
+        extend Mconf::LocaleControllerModule
+
+        def self.perform(*args)
+          klass = args[1].constantize
+          record = klass.find(args[2])
+          I18n.with_locale(get_user_locale(record, nil)) do
+            new.perform(*args)
+          end
+        end
+
+      end
+    end
+  end
+end
