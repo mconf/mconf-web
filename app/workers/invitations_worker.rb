f968947a212d41628c5d585ee745ae9026023e0a
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-10-25 19:09:05 -0200

    Organize workers and split the one for join requests in several
    Having multiple workers for join requests fixes the issue where the
    processing of one join request would fail and block all other notifications
    from being sent.
    Used this chance to also renamed the workers and the queues used and add
    more tests.

--- app/workers/invitations_worker.rb
+++ app/workers/invitations_worker.rb
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+# This file is part of Mconf-Web, a web application that provides access
+# to the Mconf webconferencing system. Copyright (C) 2010-2012 Mconf
+#
+# This file is licensed under the Affero General Public License version
+# 3 or later. See the LICENSE file.
+
 # Finds all Invitation objects not sent yet and ready to be sent and sends them.
-class Invitations
+class InvitationsWorker
   @queue = :invitations
 
   def self.perform
@@ -7,7 +14,7 @@ class Invitations
   end
 
   def self.all_invitations
-    invitations = Invitation.where :sent => false, :ready => true
+    invitations = Invitation.where sent: false, ready: true
 
     invitations.each do |invitation|
       result = invitation.send_invitation
