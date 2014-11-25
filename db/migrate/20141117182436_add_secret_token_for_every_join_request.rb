3e7585cd153e67281b3b5c0cabc0c05ecf075d02
Lucas Zawacki <lfzawacki@gmail.com>
2014-11-25 14:50:16 -0200

    Use sql syntax for join_request migration

--- db/migrate/20141117182436_add_secret_token_for_every_join_request.rb
+++ db/migrate/20141117182436_add_secret_token_for_every_join_request.rb
@@ -1,6 +1,12 @@
 class AddSecretTokenForEveryJoinRequest < ActiveRecord::Migration
   def up
-    JoinRequest.all.map(&:save)
+    connection = ActiveRecord::Base.connection()
+    ids = connection.execute("SELECT id from `join_requests`;").to_a
+
+    ids.each do |id|
+      tok = SecureRandom.urlsafe_base64(10)
+      connection.execute("UPDATE join_requests SET secret_token = '#{tok}' WHERE id = '#{id.first}'")
+    end
   end
 
   def down
