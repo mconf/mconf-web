1807eca5c7d6e9b4fca9c6f51a47176a489eb304
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-11-26 14:45:41 -0200

    Add tests for 404 on webconf rooms
    refs #1399

--- app/controllers/custom_bigbluebutton_rooms_controller.rb
+++ app/controllers/custom_bigbluebutton_rooms_controller.rb
@@ -1,4 +1,3 @@
-
 # This file is part of Mconf-Web, a web application that provides access
 # to the Mconf webconferencing system. Copyright (C) 2010-2012 Mconf
 #
---
+++ spec/features/rooms/not_found_errors_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'support/feature_helpers'
+
+feature 'Uses see the correct error for rooms that are not found' do
+
+  context 'When the room exists' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:room) { FactoryGirl.create(:bigbluebutton_room, owner: user) }
+
+    context "for actions that need extra information from the webconf server"  do
+      before {
+        BigbluebuttonRoom.any_instance.stub(:fetch_is_running?)
+        visit join_bigbluebutton_room_path(room)
+      }
+
+      it { page.status_code.should == 200 }
+      it { page.should have_content(room.name) }
+    end
+
+    context "for actions that don't need extra information from the webconf server"  do
+      before { visit join_webconf_path(room) }
+
+      it { page.status_code.should == 200 }
+      it { page.should have_content(room.name) }
+    end
+  end
+
+  context 'When the room does not exist' do
+    context "for actions that need extra information from the webconf server"  do
+      let(:url) { join_bigbluebutton_room_path(id: 'nonexistent') }
+      before { visit url }
+
+      it { page.status_code.should == 404 }
+      it { page.should have_title(t('error.e404.title')) }
+      it { page.should have_content(t('error.e404.title')) }
+      it { page.should have_content(t('error.e404.description', url: url)) }
+    end
+
+    context "for actions that don't need extra information from the webconf server"  do
+      let(:url) { join_webconf_path(id: 'nonexistent') }
+      before { visit url }
+
+      it { page.status_code.should == 404 }
+      it { page.should have_title(t('error.e404.title')) }
+      it { page.should have_content(t('error.e404.title')) }
+      it { page.should have_content(t('error.e404.description', url: url)) }
+    end
+  end
+
+end
