0900c6474087110e0e1359637fa53665a3c32a39
Lucas Zawacki <lfzawacki@gmail.com>
2014-11-27 13:55:10 -0200

    Remove unused user_mailer email type

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,21 +7,13 @@
 
 class UserMailer < BaseMailer
 
-  def default_email(user_id, email_name)
+  def registration_notification_email(user_id)
     user = User.find(user_id)
     I18n.with_locale(default_email_locale(user, nil)) do
       @user_name = user.name
-      @subject = t("user_mailer.#{email_name}.subject")
+      @subject = t("user_mailer.registration_notification_email.subject")
       create_email(user.email, Site.current.smtp_sender, @subject)
     end
-  end
-
-  def registration_notification_email(user_id)
-    default_email(user_id, 'registration_notification_email')
-  end
-
-  def waiting_for_approval_email(user_id)
-    default_email(user_id, 'waiting_for_approval_email')
   end
 
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -8,59 +8,6 @@ require "spec_helper"
 
 describe UserMailer do
 
-  describe '.new_user_waiting_for_approval' do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:mail) { UserMailer.waiting_for_approval_email(user.id) }
-
-    context "in the standard case" do
-      it("sets 'to'") { mail.to.should eql([user.email]) }
-      it("sets 'subject'") {
-        text = I18n.t("user_mailer.waiting_for_approval_email.subject")
-        text = "[#{Site.current.name}] #{text}"
-        mail.subject.should eql(text)
-      }
-      it("sets 'from'") { mail.from.should eql([Site.current.smtp_sender]) }
-      it("sets 'headers'") { mail.headers.should eql({}) }
-      it("sets 'reply_to'") { mail.reply_to.should eql([Site.current.smtp_sender]) }
-      it("assigns @user_name") {
-        mail.body.encoded.should match(user.name)
-      }
-    end
-
-    context "uses the receiver's locale" do
-      before {
-        Site.current.update_attributes(:locale => "en")
-        user.update_attribute(:locale, "pt-br")
-      }
-      it {
-        content = I18n.t("user_mailer.waiting_for_approval_email.subject", locale: "pt-br")
-        mail.body.encoded.should match(content)
-      }
-    end
-
-    context "uses the current site's locale if the receiver has no locale set" do
-      before {
-        Site.current.update_attributes(:locale => "pt-br")
-      }
-      it {
-        content = I18n.t('user_mailer.waiting_for_approval_email.subject', locale: "pt-br")
-        mail.body.encoded.should match(content)
-      }
-    end
-
-    context "uses the default locale if the site has no locale set" do
-      before {
-        Site.current.update_attributes(:locale => nil)
-        I18n.default_locale = "pt-br"
-        user.update_attribute(:locale, nil)
-      }
-      it {
-        content = I18n.t('user_mailer.waiting_for_approval_email.subject', locale: "pt-br")
-        mail.body.encoded.should match(content)
-      }
-    end
-  end
-
   describe '.registration_notification_email' do
     let(:user) { FactoryGirl.create(:user) }
     let(:mail) { UserMailer.registration_notification_email(user.id) }
@@ -122,13 +69,6 @@ describe UserMailer do
 
   context "calls the error handler on exceptions" do
     let(:exception) { Exception.new("test exception") }
-    it {
-      with_resque do
-        BaseMailer.any_instance.stub(:render) { raise exception }
-        Mconf::MailerErrorHandler.should_receive(:handle).with(UserMailer, nil, exception, "waiting_for_approval_email", anything)
-        UserMailer.waiting_for_approval_email(1).deliver
-      end
-    }
     it {
       with_resque do
         BaseMailer.any_instance.stub(:render) { raise exception }
