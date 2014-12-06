c900f2af45fb3442225efbf7bc1fa1f6c27673fd
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2014-12-06 16:02:24 -0200

    Migrate all old icons to font-awesome 4
    refs #1205
    refs #1087

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -8,68 +8,68 @@ module IconsHelper
 
   # Default icon that shows a tooltip with help about something
   def icon_help(title, options={})
-    icon_constructor title, "icon-awesome icon-question-sign icon-mconf-help", options
+    icon_constructor title, "fa fa-question-circle icon-awesome icon-mconf-help", options
   end
 
   # Default icon that shows a tooltip with information about something
   def icon_info(title, options={})
-    icon_constructor title, "icon-awesome icon-info-sign icon-mconf-info", options
+    icon_constructor title, "fa fa-info-circle icon-awesome icon-mconf-info", options
   end
 
   # Default icon to a feed (rss)
   def icon_feed(options={})
     options.merge!(:alt => t('RSS'), :title => t('RSS'))
-    icon_constructor t('RSS'), "icon-awesome icon-rss icon-mconf-rss", options
+    icon_constructor t('RSS'), "fa fa-rss icon-awesome icon-mconf-rss", options
   end
 
   def icon_attachment(title, options={})
-    icon_constructor title, "icon-awesome icon-paper-clip iconf-mconf-attachment", options
+    icon_constructor title, "fa fa-paperclip icon-awesome iconf-mconf-attachment", options
   end
 
   def icon_comment(title=nil, options={})
-    icon_constructor title, "icon-awesome fa fa-comment icon-mconf-comment", options
+    icon_constructor title, "fa fa-comment icon-awesome icon-mconf-comment", options
   end
 
   def icon_comments(title=nil, options={})
-    icon_constructor title, "icon-awesome fa fa-comments-o icon-mconf-comments", options
+    icon_constructor title, "fa fa-comments-o icon-awesome icon-mconf-comments", options
   end
 
   def icon_event(title=nil, options={})
-    icon_constructor title, "icon-awesome icon-calendar icon-mconf-event", options
+    icon_constructor title, "fa fa-calendar icon-awesome icon-mconf-event", options
   end
 
   def icon_news(title=nil, options={})
-    icon_constructor title, "icon-awesome icon-bullhorn icon-mconf-news", options
+    icon_constructor title, "fa fa-bullhorn icon-awesome icon-mconf-news", options
   end
 
   # Admin red label/icon
   # Usually shown on top of an avatar.
   def icon_superuser(options={})
-    icon_constructor nil, "icon-awesome icon-cogs icon-mconf-superuser", options
+    icon_constructor nil, "fa fa-cogs icon-awesome icon-mconf-superuser", options
   end
 
   def icon_guest(options={})
-    icon_constructor nil, "icon-awesome icon-meh icon-mconf-guest", options
+    icon_constructor nil, "fa fa-meh-o icon-awesome icon-mconf-guest", options
   end
 
   # Conference "in progress" icon.
   def icon_in_progress(options={})
-    icon_constructor nil, "icon-awesome fa fa-refresh icon-mconf-in-progress fa-spin", options
+    icon_constructor nil, "fa fa-refresh fa-spin icon-awesome icon-mconf-in-progress", options
   end
 
   def icon_loading(options={})
-    icon_constructor nil, "icon-awesome fa fa-refresh icon-mconf-loading fa-spin", options
+    icon_constructor nil, "fa-spin fa fa-refresh icon-awesome icon-mconf-loading", options
   end
 
   def icon_spam(options={})
-    icon_constructor t("_other.spam.tooltip"), "icon-awesome icon-warning-sign icon-mconf-spam", options
+    icon_constructor t("_other.spam.tooltip"), "fa fa-exclamation-triangle icon-awesome icon-mconf-spam", options
   end
 
   def icon_message(options={})
     if options.has_key?(:class) and options[:class].match(/active/)
-      icon_constructor nil, "icon-awesome icon-envelope icon-mconf-message", options
+      icon_constructor nil, "fa fa-envelope icon-awesome icon-mconf-message", options
     else
-      icon_constructor nil, "icon-awesome icon-envelope-alt icon-mconf-message", options
+      icon_constructor nil, "fa fa-envelope-o icon-awesome icon-mconf-message", options
     end
   end
 
@@ -79,168 +79,152 @@ module IconsHelper
     icon_message(options)
   end
 
-  def icon_home(options={})
-    icon_constructor nil, "icon-awesome icon-home icon-mconf-home", options
-  end
-
   def icon_logout(options={})
-    icon_constructor nil, "icon-awesome icon-off icon-mconf-logout", options
+    icon_constructor nil, "fa fa-power-off icon-awesome icon-mconf-logout", options
   end
 
   def icon_profile(options={})
-    icon_constructor nil, "icon-awesome fa fa-user icon-mconf-profile", options
+    icon_constructor nil, "fa fa-user icon-awesome icon-mconf-profile", options
   end
 
   def icon_account(options={})
-    icon_constructor nil, "icon-awesome fa fa-wrench icon-mconf-account", options
+    icon_constructor nil, "fa fa-wrench icon-awesome icon-mconf-account", options
   end
 
   def icon_activity(options={})
-    icon_constructor nil, "icon-awesome fa fa-list-alt icon-mconf-activity", options
+    icon_constructor nil, "fa-list-alt icon-awesome icon-mconf-activity", options
   end
 
   def icon_create(options={})
-    icon_constructor nil, "icon-awesome icon-plus-sign icon-mconf-create", options
+    icon_constructor nil, "fa fa-plus-square-o icon-awesome icon-mconf-create", options
   end
 
   def icon_delete(options={})
-    icon_constructor nil, "icon-awesome icon-remove-sign icon-mconf-delete", options
+    icon_constructor nil, "fa fa-times icon-awesome icon-mconf-delete", options
   end
 
   def icon_disable(options={})
-    icon_constructor nil, "icon-awesome icon-ban-circle icon-mconf-disable", options
-  end
-
-  def icon_confirm(options={})
-    icon_constructor nil, "icon-awesome icon-ok-sign icon-mconf-confirm", options
+    icon_constructor nil, "fa fa-arrow-down icon-awesome icon-mconf-disable", options
   end
 
   def icon_enable(options={})
-    icon_constructor nil, "icon-awesome icon-check icon-mconf-enable", options
+    icon_constructor nil, "fa fa-arrow-up icon-awesome icon-mconf-enable", options
+  end
+
+  def icon_confirm(options={})
+    icon_constructor nil, "fa fa-check-circle icon-awesome icon-mconf-confirm", options
   end
 
   def icon_users(options={})
-    icon_constructor nil, "icon-awesome fa fa-users icon-mconf-users", options
+    icon_constructor nil, "fa fa-users icon-awesome icon-mconf-users", options
   end
 
   def icon_edit(options={})
-    icon_constructor nil, "icon-awesome icon-edit icon-mconf-edit", options
+    icon_constructor nil, "fa fa-edit icon-awesome icon-mconf-edit", options
   end
 
   def icon_share(options={})
-    icon_constructor nil, "icon-awesome icon-share icon-mconf-share", options
+    icon_constructor nil, "fa fa-share-square-o icon-awesome icon-mconf-share", options
   end
 
   def icon_download(options={})
-    icon_constructor nil, "icon-awesome icon-download-alt icon-mconf-download", options
+    icon_constructor nil, "fa fa-download icon-awesome icon-mconf-download", options
   end
 
   def icon_attachment_new_version(options={})
-    icon_constructor nil, "icon-awesome icon-plus-sign icon-mconf-attachment-new-version", options
-  end
-
-  def icon_new_tag(options={})
-    icon_constructor nil, "icon-awesome icon-tags icon-mconf-new-tag", options
+    icon_constructor nil, "fa fa-plus-circle icon-awesome icon-mconf-attachment-new-version", options
   end
 
   def icon_space_private(options={})
-    icon_constructor nil, "icon-awesome fa fa-lock icon-mconf-space-private", options
+    icon_constructor nil, "fa fa-lock icon-awesome icon-mconf-space-private", options
   end
 
   def icon_space_public(options={})
-    icon_constructor nil, "icon-awesome fa fa-eye icon-mconf-space-public", options
+    icon_constructor nil, "fa fa-eye icon-awesome icon-mconf-space-public", options
   end
 
   def icon_webconf_start(options={})
-    icon_constructor nil, "icon-awesome icon-chevron-sign-right icon-mconf-webconf-start", options
+    icon_constructor nil, "fa fa-chevron-right icon-awesome icon-mconf-webconf-start", options
   end
 
   def icon_mobile(options={})
-    icon_constructor nil, "icon-awesome icon-mobile-phone icon-mconf-mobile", options
+    icon_constructor nil, "fa fa-mobile icon-awesome icon-mconf-mobile", options
   end
 
   def icon_order_number_asc(options={})
-    icon_constructor nil, "icon-awesome icon-sort-by-order icon-mconf-order-number-asc", options
+    icon_constructor nil, "fa fa-sort-numeric-asc icon-awesome icon-mconf-order-number-asc", options
   end
 
   def icon_order_number_desc(options={})
-    icon_constructor nil, "icon-awesome icon-sort-by-order-alt icon-mconf-order-number-desc", options
+    icon_constructor nil, "fa fa-sort-numeric-desc icon-awesome icon-mconf-order-number-desc", options
   end
 
   def icon_check(options={})
-    icon_constructor nil, "icon-awesome icon-check icon-mconf-check", options
-  end
-
-  def icon_list(options={})
-    icon_constructor nil, "icon-awesome icon-list icon-mconf-list", options
+    icon_constructor nil, "fa fa-check-square-o icon-awesome icon-mconf-check", options
   end
 
   def icon_picture(options={})
-    icon_constructor nil, "icon-awesome icon-picture icon-mconf-picture", options
+    icon_constructor nil, "fa fa-picture-o icon-awesome icon-mconf-picture", options
   end
 
   def icon_playback(options={})
-    icon_constructor nil, "icon-awesome icon-play-sign icon-mconf-playback", options
+    icon_constructor nil, "fa fa-play-circle icon-awesome icon-mconf-playback", options
   end
 
   def icon_date(options={})
-    icon_constructor nil, "icon-awesome icon-calendar-empty icon-mconf-date", options
+    icon_constructor nil, "fa fa-calendar-o icon-awesome icon-mconf-date", options
   end
 
   def icon_user(options={})
-    icon_constructor nil, "icon-awesome fa fa-user icon-mconf-user", options
+    icon_constructor nil, "fa fa-user icon-awesome icon-mconf-user", options
   end
 
   def icon_space(options={})
-    icon_constructor nil, "icon-awesome icon-group icon-mconf-space", options
+    icon_constructor nil, "fa fa-group icon-awesome icon-mconf-space", options
   end
 
   def icon_error(options={})
-    icon_constructor nil, "icon-awesome icon-meh icon-mconf-error", options
+    icon_constructor nil, "fa fa-meh-o icon-awesome icon-mconf-error", options
   end
 
   def icon_success(options={})
-    icon_constructor nil, "icon-awesome icon-ok icon-mconf-success", options
+    icon_constructor nil, "fa fa-check icon-awesome icon-mconf-success", options
   end
 
   def icon_meeting(options={})
-    icon_constructor nil, "icon-awesome fa fa-video-camera icon-mconf-meeting", options
+    icon_constructor nil, "fa fa-video-camera icon-awesome icon-mconf-meeting", options
   end
 
   def icon_recording(options={})
-    icon_constructor nil, "icon-awesome icon-youtube-play icon-mconf-recording", options
-  end
-
-  def icon_nested_child(options={})
-    icon_constructor nil, "icon-awesome icon-level-up icon-rotate-90 icon-mconf-nested-child", options
+    icon_constructor nil, "fa fa-youtube-play icon-awesome icon-mconf-recording", options
   end
 
   def icon_approve(options={})
-    icon_constructor nil, "icon-awesome icon-check-sign icon-mconf-approve", options
+    icon_constructor nil, "fa fa-thumbs-o-up icon-awesome icon-mconf-approve", options
   end
 
   def icon_disapprove(options={})
-    icon_constructor nil, "icon-awesome icon-remove-sign icon-mconf-disapprove", options
+    icon_constructor nil, "fa fa-thumbs-o-down icon-awesome icon-mconf-disapprove", options
   end
 
   def icon_is_member(options={})
-    icon_constructor nil, "icon-awesome fa fa-check icon-mconf-is-member", options
+    icon_constructor nil, "fa fa-check icon-awesome icon-mconf-is-member", options
   end
 
   def icon_options(options={})
-    icon_constructor nil, "icon-awesome fa fa-gears icon-mconf-options", options
+    icon_constructor nil, "fa fa-gears icon-awesome icon-mconf-options", options
   end
 
   def icon_join_request(options={})
-    icon_constructor nil, "icon-awesome icon-hand-up icon-mconf-join-request", options
+    icon_constructor nil, "fa fa-hand-o-up icon-awesome icon-mconf-join-request", options
   end
 
   def icon_join_space(options={})
-    icon_constructor nil, "icon-awesome icon-signin icon-mconf-join-space", options
+    icon_constructor nil, "fa fa-sign-in icon-awesome icon-mconf-join-space", options
   end
 
   def icon_leave_space(options={})
-    icon_constructor nil, "icon-awesome icon-signout icon-mconf-leave-space", options
+    icon_constructor nil, "fa fa-sign-out icon-awesome icon-mconf-leave-space", options
   end
 
   private
