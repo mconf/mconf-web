226cd850615786a4525a83d05eaf8c588da051e6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-01-21 17:05:18 -0200

    Make sure confirmation routes show a 404 when registrations are disabled
    When the flag site_registration_enabled was false, it would throw a 500
    in production when the user accessed any of the confirmation routes
    (e.g. /users/confirmation/new).
    Also, the action confirmations#show was not redirecting the user to his
    home if the user is already confirmed.
    Fixed both errors.
    
    refs #1413

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,7 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, :with => :render_500
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   rescue_from ActionController::UnknownController, :with => :render_404
+  rescue_from ActionController::RoutingError, :with => :render_404
   rescue_from ::AbstractController::ActionNotFound, :with => :render_404
   rescue_from CanCan::AccessDenied, :with => :render_403
 
