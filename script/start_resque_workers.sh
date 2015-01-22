b4556a5997a96539521213975631f2aed202f822
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-01-22 16:03:27 -0200

    Fix failing commands to stop resque workers
    Now we trigger multiple calls to resque:work instead of a single call to
    resque:workers. With this we can manage the pids and stop the processes
    correctly and monitor the processes individually.
    Made the calls to resque:work run in background, so the process that
    calls the script start_resque_workers.sh is free to finish.
    
    Also improved the format of the logs written by resque.
    
    refs #1424
    refs #1391

--- script/start_resque_workers.sh
+++ script/start_resque_workers.sh
@@ -10,31 +10,37 @@
 # Arguments:
 #   $1: (start|stop)
 #   $2: queue name, e.g. 'bigbluebutton_rails'
+#   $3: the number of the worker, only used if $2 is "all"
 # Example:
 #   start_resque_workers.sh start bigbluebutton_rails
 #   start_resque_workers.sh start all
+#   start_resque_workers.sh start all 2
 
 USER="$(id -u -n)"
 APP_PATH="$(dirname $0)/.."
 PATH=/home/$USER/.rbenv/bin:/home/$USER/.rbenv/shims:$PATH
 RAILS_ENV=production
+NUM_WORKERS=3
+if [ -z "$3" ]; then WORKERNUM=1; else WORKERNUM=$3; fi
 
 if [ "$2" == "all" ]; then
-  PIDFILE=$APP_PATH/tmp/pids/resque_workers_all.pid
+  PIDFILE=$APP_PATH/tmp/pids/resque_worker_$WORKERNUM.pid
   LOGFILE=$APP_PATH/log/resque_workers_all.log
 else
-  PIDFILE=$APP_PATH/tmp/pids/resque_workers_$2.pid
-  LOGFILE=$APP_PATH/log/resque_workers_$2.log
+  PIDFILE=$APP_PATH/tmp/pids/resque_worker_$2.pid
+  LOGFILE=$APP_PATH/log/resque_worker_$2.log
 fi
 
 cd $APP_PATH
 
 if [ "$1" != "stop" ]; then
   if [ "$2" == "all" ]; then
-    /usr/bin/env bundle exec rake environment resque:workers RAILS_ENV=$RAILS_ENV PIDFILE=$PIDFILE VERBOSE=1 QUEUE="*" COUNT="3" >> $LOGFILE 2>&1
+    /usr/bin/env bundle exec rake environment resque:work RAILS_ENV=$RAILS_ENV PIDFILE=$PIDFILE QUEUE="*" TERM_CHILD=1 >> $LOGFILE 2>&1 &
   else
-    /usr/bin/env bundle exec rake environment resque:workers RAILS_ENV=$RAILS_ENV PIDFILE=$PIDFILE VERBOSE=1 QUEUE=$2 COUNT="3" >> $LOGFILE 2>&1
+    # for specific queues we run a single worker
+    /usr/bin/env bundle exec rake environment resque:work RAILS_ENV=$RAILS_ENV PIDFILE=$PIDFILE QUEUE=$2 TERM_CHILD=1 >> $LOGFILE 2>&1 &
   fi
 else
-  kill -9 $(cat $PIDFILE) && rm -f $PIDFILE; exit 0;
+  echo "Stopping worker pid:$(cat $PIDFILE)" >> $LOGFILE
+  kill -s QUIT $(cat $PIDFILE) && rm -f $PIDFILE; exit 0;
 fi
