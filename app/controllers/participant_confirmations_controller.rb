20fad2108a7bb7ec0b7187a42d1c60d2b5335b66
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-01-29 13:05:01 -0200

    Better confirmation email and list of participants in events
    refs #1289

--- app/controllers/participant_confirmations_controller.rb
+++ app/controllers/participant_confirmations_controller.rb
@@ -3,7 +3,7 @@ class ParticipantConfirmationsController < ApplicationController
   def confirm
     @pc = ParticipantConfirmation.where(token: params[:token]).first
     @pc.confirm!
-    redirect_to mweb_events.event_path(@pc.participant.event), notice: t('participant_confirmation.confirmed')
+    redirect_to mweb_events.event_path(@pc.participant.event), notice: t('participant_confirmation.confirmed', email: @pc.participant.email)
   end
 
 end
