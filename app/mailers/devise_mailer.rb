1ac524c8fee7597962d07ceb2fe6fbd6dd206733
Fernando de Avila Bottin <fbottin@gmail.com>
2015-02-09 15:44:35 -0200

    Update devise_mailer.rb
    Added a new layout to standardize with the others emails of the site.
    
    refs #1421

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,6 +2,7 @@ class DeviseMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
+  layout 'mailers'
 
   def confirmation_instructions(record, token, opts={})
     opts[:subject] = "[#{Site.current.name}] #{t('devise.mailer.confirmation_instructions.subject')}"
