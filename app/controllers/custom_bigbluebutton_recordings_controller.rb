dc8445becb949e68409d0aaf9536db9838edd42a
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-03-28 15:56:14 -0300

    Simpler extension of CustomBigbluebuttonRecordingsController#play
    Use a before_filter instead of overriding the entire method.
    
    refs #1482

--- app/controllers/custom_bigbluebutton_recordings_controller.rb
+++ app/controllers/custom_bigbluebutton_recordings_controller.rb
@@ -2,38 +2,21 @@ class CustomBigbluebuttonRecordingsController < Bigbluebutton::RecordingsControl
   # need authentication even to play a recording
   before_filter :authenticate_user!
 
+  before_filter :set_parameters, only: :play
+
   load_and_authorize_resource :find_by => :recordid, :class => "BigbluebuttonRecording"
 
-  # Override the method used in Bigbluebutton::RecordingsController to set the 
-  # parameter who will determine the name of the video to download.
-  def play
-    if params[:type]
-      playback = @recording.playback_formats.where(:playback_type_id => BigbluebuttonPlaybackType.find_by_identifier(params[:type])).first
-    else
-      playback = @recording.default_playback_format || @recording.playback_formats.first
-    end
+  protected
 
-    if params[:name]
-      url = Addressable::URI.parse(playback.url)
+  # Checks the URL and sets the parameters as temporary parameters in the playback's URL.
+  def set_parameters
+    if params[:name] && @playback.present?
+      url = Addressable::URI.parse(@playback.url)
       url.query_values = { name: params[:name] }
       url = url.to_s
-    else
-      url = playback.url
-    end
-
-    respond_with do |format|
-      format.html {
-        if playback
-          redirect_to url
-        else
-          flash[:error] = t('bigbluebutton_rails.recordings.errors.play.no_format')
-          redirect_to_using_params bigbluebutton_recording_url(@recording)
-        end
-      }
+      @playback.url = url
     end
   end
-
-  protected
 
   # Override the method used in Bigbluebutton::RecordingsController to get the parameters the
   # user is allowed to use on update/create. Normal users can only update a few of the parameters
