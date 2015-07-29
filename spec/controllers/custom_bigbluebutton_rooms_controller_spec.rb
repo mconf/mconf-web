ff8f6523228a6ce614ba85597ef62ffc27413b06
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-07-29 15:48:44 -0300

    Update bigbluebutton_rails, fix tests
    Was causing errors when a room had no server set in it. Started after
    ca0c45a1da1469501e53c895e725a1d037b830b7.
    
    refs #1704

--- spec/controllers/custom_bigbluebutton_rooms_controller_spec.rb
+++ spec/controllers/custom_bigbluebutton_rooms_controller_spec.rb
@@ -310,7 +310,7 @@ describe CustomBigbluebuttonRoomsController do
           [ :name, :server_id, :meetingid, :attendee_key, :moderator_key, :welcome_msg,
             :private, :logout_url, :dial_number, :voice_bridge, :max_participants, :owner_id,
             :owner_type, :external, :param, :record_meeting, :duration, :default_layout, :presenter_share_only,
-            :auto_start_video, :auto_start_audio,
+            :auto_start_video, :auto_start_audio, :background,
             :moderator_only_message, :auto_start_recording, :allow_start_stop_recording,
             :metadata_attributes => [ :id, :name, :content, :_destroy, :owner_id ] ]
         }
