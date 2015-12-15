83623bf6b456a3d9cbd9708735b7b45f295dccfd
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-12-15 17:54:06 -0200

    Add migration to remove recent activities for private messages
    refs #1807

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029191439) do
+ActiveRecord::Schema.define(version: 20151215194513) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
