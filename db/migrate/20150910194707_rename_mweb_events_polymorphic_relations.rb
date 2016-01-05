9e3eec379b9d39d58967c270ea9644bd09555670
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2016-01-05 15:59:19 -0200

    Migrate keys for recent activities related to events
    refs #1808

--- db/migrate/20150910194707_rename_mweb_events_polymorphic_relations.rb
+++ db/migrate/20150910194707_rename_mweb_events_polymorphic_relations.rb
@@ -1,40 +1,17 @@
 class RenameMwebEventsPolymorphicRelations < ActiveRecord::Migration
   def up
-    puts "****** Migrating polymorphic types from 'MwebEvents::Events to Events"
-    RecentActivity.where(trackable_type: 'MwebEvents::Event').each do |act|
-      act.update_attributes(trackable_type: 'Event')
-    end
-
-    RecentActivity.where(trackable_type: 'MwebEvents::Participant').each do |act|
-      act.update_attributes(trackable_type: 'Participant')
-    end
-
-    Permission.where(subject_type: 'MwebEvents::Event').each do |p|
-      p.update_attributes(subject_type: 'Event')
-    end
-
-    Invitation.where(target_type: 'MwebEvents::Event').each do |inv|
-      inv.update_attributes(target_type: 'Event')
-    end
+    puts "Migrating polymorphic types from 'MwebEvents::Events to Events"
+    RecentActivity.where(trackable_type: 'MwebEvents::Event').update_all(trackable_type: 'Event')
+    RecentActivity.where(trackable_type: 'MwebEvents::Participant').update_all(trackable_type: 'Participant')
+    Permission.where(subject_type: 'MwebEvents::Event').update_all(subject_type: 'Event')
+    Invitation.where(target_type: 'MwebEvents::Event').update_all(target_type: 'Event')
   end
 
   def down
-    puts "****** Migrating polymorphic types back from 'Events to MwebEvents::Events"
-
-    RecentActivity.where(trackable_type: 'Event').each do |act|
-      act.update_attributes(trackable_type: 'MwebEvents::Event')
-    end
-
-    RecentActivity.where(trackable_type: 'Participant').each do |act|
-      act.update_attributes(trackable_type: 'MwebEvents::Participant')
-    end
-
-    Permission.where(subject_type: 'Event').each do |p|
-      p.update_attributes(subject_type: 'MwebEvents::Event')
-    end
-
-    Invitation.where(target_type: 'Event').each do |inv|
-      inv.update_attributes(target_type: 'MwebEvents::Event')
-    end
+    puts "Migrating polymorphic types back from 'Events to MwebEvents::Events"
+    RecentActivity.where(trackable_type: 'Event').update_all(trackable_type: 'MwebEvents::Event')
+    RecentActivity.where(trackable_type: 'Participant').update_all(trackable_type: 'MwebEvents::Participant')
+    Permission.where(subject_type: 'Event').update_all(subject_type: 'MwebEvents::Event')
+    Invitation.where(target_type: 'Event').update_all(target_type: 'MwebEvents::Event')
   end
 end
