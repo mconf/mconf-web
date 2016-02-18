5aeb72b46434db509ea7d722005480636a07b526
Lucas Zawacki <lfzawacki@gmail.com>
2016-02-18 14:28:05 -0200

    Extract error rendering to an external controller module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,9 @@ class ApplicationController < ActionController::Base
   helper_method :current_site
   helper_method :locale_i18n
 
+  # Includes methods like render_404 and render_500
+  include Mconf::ErrorRenderingModule
+
   # Handle errors - error pages
   rescue_from Exception, :with => :render_500
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
@@ -69,51 +72,6 @@ class ApplicationController < ActionController::Base
 
   def set_time_zone
     Time.zone = Mconf::Timezone.user_time_zone(current_user)
-  end
-
-  def render_error_page number
-    # If we're here because of an error in an after_fiter this will trigger a DoubleRender error.
-    # To prevent it we'll just clear the response_body before continuing
-    self.response_body = nil
-    render :template => "/errors/error_#{number}", :status => number, :layout => "error"
-  end
-
-  def render_404(exception)
-    @route ||= request.path
-    unless Rails.application.config.consider_all_requests_local
-      @exception = exception
-      render_error_page 404
-    else
-      raise exception
-    end
-  end
-
-  def render_500(exception)
-    unless Rails.application.config.consider_all_requests_local
-      @exception = exception
-      ExceptionNotifier.notify_exception exception
-      render_error_page 500
-    else
-      raise exception
-    end
-  end
-
-  def render_403(exception)
-    unless Rails.application.config.consider_all_requests_local
-      @exception = exception
-      render_error_page 403
-    else
-      raise exception
-    end
-  end
-
-  def render_400(exception)
-    unless Rails.application.config.consider_all_requests_local
-      self.response_body = nil
-      render(nothing: true, status: 400)
-    else
-      raise exception
-    end
   end
 
   def path_is_redirectable? path
---
+++ lib/mconf/error_rendering_module.rb
@@ -0,0 +1,50 @@
+module Mconf::ErrorRenderingModule
+
+  # TODO: refactor so that the 'unless all_resquests_local' is in
+  # render_error_page and things get a little more DRY
+  def render_error_page number
+    # If we're here because of an error in an after_fiter this will trigger a DoubleRender error.
+    # To prevent it we'll just clear the response_body before continuing
+    self.response_body = nil
+    render :template => "/errors/error_#{number}", :status => number, :layout => "error"
+  end
+
+  def render_404(exception)
+    @route ||= request.path
+    unless Rails.application.config.consider_all_requests_local
+      @exception = exception
+      render_error_page 404
+    else
+      raise exception
+    end
+  end
+
+  def render_500(exception)
+    unless Rails.application.config.consider_all_requests_local
+      @exception = exception
+      ExceptionNotifier.notify_exception exception
+      render_error_page 500
+    else
+      raise exception
+    end
+  end
+
+  def render_403(exception)
+    unless Rails.application.config.consider_all_requests_local
+      @exception = exception
+      render_error_page 403
+    else
+      raise exception
+    end
+  end
+
+  def render_400(exception)
+    unless Rails.application.config.consider_all_requests_local
+      self.response_body = nil
+      render(nothing: true, status: 400)
+    else
+      raise exception
+    end
+  end
+
+end
