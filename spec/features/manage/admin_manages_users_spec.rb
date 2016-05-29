069c3dc8790e28ecb15d891806bc11ca5f4ba20f
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2016-05-29 11:02:58 -0300

    Fix view for manage/users and tests after latest changes

--- spec/features/manage/admin_manages_users_spec.rb
+++ spec/features/manage/admin_manages_users_spec.rb
@@ -18,6 +18,7 @@ describe 'Admin manages users' do
 
       login_as(admin, :scope => :user)
       @user1 = FactoryGirl.create(:user)
+      @user_admin = FactoryGirl.create(:user, superuser: true)
       @unapproved_user = FactoryGirl.create(:user)
       @unapproved_user.update_attributes(:approved => false)
       @disabled_user1 = FactoryGirl.create(:user, disabled: true)
@@ -30,24 +31,17 @@ describe 'Admin manages users' do
     context 'listing users in management screen' do
       before { visit manage_users_path }
 
-      it { should have_css '.user-simple', :count => 7 }
-      it { should have_css '.icon-mconf-delete', :count => 6 }
+      it { should have_css '#users-list .list-item', count: 8 }
+      it { should have_css '#users-list .icon-mconf-delete', count: 7 }
 
-      it { should have_css '.user-disabled', :count => 2 }
-      it { should have_css '.icon-mconf-enable', :count => 2 }
+      it { should have_css '#users-list .list-item.list-item-disabled', count: 2 }
+      it { should have_css '#users-list .icon-mconf-enable', count: 2 }
 
-      it { should have_css '.icon-mconf-confirm-user', :count => 2 }
-      it { should have_css '.icon-mconf-approve', :count => 2 }
-      it { should have_css '.icon-mconf-superuser', :count => 1 }
+      it { should have_css '#users-list .icon-mconf-confirm-user', count: 2 }
+      it { should have_css '#users-list .icon-mconf-approve', count: 2 }
+      it { should have_css '#users-list .icon-mconf-superuser', count: 2 }
 
-      it { should have_content user_description(@user1) }
-      it { should have_content user_description(@unapproved_user) }
-      it { should have_content user_description(@disabled_user1) }
-      it { should have_content user_description(@disabled_user2) }
-      it { should have_content user_description(@unconfirmed_user) }
-      it { should have_content user_description(@unconfirmed_unapproved_user) }
-
-      context 'elements for an admin user (self)' do
+      context 'elements for the signed in user' do
         let(:user) { admin }
         subject { page.find("#user-#{user.permalink}") }
 
@@ -58,6 +52,9 @@ describe 'Admin manages users' do
         it { should_not have_link_to_destroy_user(user) }
         it { should_not have_link_to_disable_user(user) }
         it { should_not have_link_to_confirm_user(user) }
+        it { subject.find('.user-username').should have_content(user.username) }
+        it { subject.find('.user-name').should have_content(user.full_name) }
+        it { subject.find('.user-email').should have_content(user.email) }
       end
 
       context 'elements for a normal user' do
@@ -71,6 +68,25 @@ describe 'Admin manages users' do
         it { should have_link_to_destroy_user(user) }
         it { should have_link_to_disable_user(user) }
         it { should_not have_link_to_confirm_user(user) }
+        it { subject.find('.user-username').should have_content(user.username) }
+        it { subject.find('.user-name').should have_content(user.full_name) }
+        it { subject.find('.user-email').should have_content(user.email) }
+      end
+
+      context 'elements for an admin' do
+        let(:user) { @user_admin }
+        subject { page.find("#user-#{user.permalink}") }
+
+        it { should have_css '.icon-mconf-superuser' }
+        it { should have_css '.management-links' }
+        it { should have_content t('_other.user.administrator') }
+        it { should have_link_to_edit_user(user) }
+        it { should have_link_to_destroy_user(user) }
+        it { should have_link_to_disable_user(user) }
+        it { should_not have_link_to_confirm_user(user) }
+        it { subject.find('.user-username').should have_content(user.username) }
+        it { subject.find('.user-name').should have_content(user.full_name) }
+        it { subject.find('.user-email').should have_content(user.email) }
       end
 
       context 'elements for a disabled user' do
@@ -83,6 +99,9 @@ describe 'Admin manages users' do
         it { should_not have_link_to_edit_user(user) }
         it { should_not have_link_to_disable_user(user) }
         it { should_not have_link_to_confirm_user(user) }
+        it { subject.find('.user-username').should have_content(user.username) }
+        it { subject.find('.user-name').should have_content(user.full_name) }
+        it { subject.find('.user-email').should have_content(user.email) }
       end
 
       context 'elements for an unconfirmed normal user' do
@@ -93,6 +112,9 @@ describe 'Admin manages users' do
         it { should have_link_to_destroy_user(user) }
         it { should have_link_to_disable_user(user) }
         it { should have_link_to_confirm_user(user) }
+        it { subject.find('.user-username').should have_content(user.username) }
+        it { subject.find('.user-name').should have_content(user.full_name) }
+        it { subject.find('.user-email').should have_content(user.email) }
       end
     end
 
@@ -134,20 +156,16 @@ describe 'Admin manages users' do
       end
 
       context 'elements for a second approved admin user' do
-        let(:user) { @user1 }
-        before {
-          user.update_attributes(:superuser => true)
-          visit manage_users_path
-        }
+        let(:user) { @user_admin }
         subject { page.find("#user-#{user.permalink}") }
 
         it { should have_css '.management-links' }
         it { should have_link_to_edit_user(user) }
-        it { should_not have_link_to_destroy_user(user) }
-        it { should_not have_link_to_disapprove_user(user) }
+        it { should have_link_to_destroy_user(user) }
+        it { should have_link_to_disapprove_user(user) }
         it { should have_css '.icon-mconf-superuser' }
         it { should have_content t('_other.user.administrator') }
-        it { should_not have_link_to_disable_user(user) }
+        it { should have_link_to_disable_user(user) }
       end
 
       context 'elements for an unapproved and unconfirmed user' do
@@ -192,8 +210,4 @@ end
 
 def have_link_to_approve_user(user)
   have_link '', :href => approve_user_path(user)
-end
-
-def user_description(user)
-  "#{user.full_name} (#{user.username}, #{user.email})"
 end
