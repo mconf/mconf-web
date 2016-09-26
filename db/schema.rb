f8becc41aa2ae4f544acdd72c82b5dc7ee4618a5
RafaFP <rafapetersenpet@gmail.com>
2016-09-26 18:37:19 -0300

    added a field to User schema to keep the last time user signed in using local authentication current_local_sign_in_at works similarly to the fields added to shibtoken and ldaptoken this field will only be updated if the user logs via mconf-web portal (non-shib, non-ldap)
    TODO spec test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921200455) do
+ActiveRecord::Schema.define(version: 20160926154808) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -404,21 +404,21 @@ ActiveRecord::Schema.define(version: 20160921200455) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "email",                             default: "",    null: false
-    t.string   "encrypted_password",                default: "",    null: false
-    t.string   "password_salt",          limit: 40
+    t.string   "email",                               default: "",    null: false
+    t.string   "encrypted_password",                  default: "",    null: false
+    t.string   "password_salt",            limit: 40
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "superuser",                         default: false
-    t.boolean  "disabled",                          default: false
+    t.boolean  "superuser",                           default: false
+    t.boolean  "disabled",                            default: false
     t.datetime "confirmed_at"
     t.string   "timezone"
-    t.boolean  "expanded_post",                     default: false
+    t.boolean  "expanded_post",                       default: false
     t.string   "locale"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0
+    t.integer  "sign_in_count",                       default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -427,7 +427,8 @@ ActiveRecord::Schema.define(version: 20160921200455) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.boolean  "can_record"
-    t.boolean  "approved",                          default: false, null: false
+    t.boolean  "approved",                            default: false, null: false
+    t.datetime "current_local_sign_in_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
