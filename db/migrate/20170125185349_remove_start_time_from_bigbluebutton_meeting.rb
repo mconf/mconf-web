a4aa58354e678fbd9e6f5e270d44e7d6a9f544c7
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-02-07 17:49:46 -0200

    Replace multiple migrations of BigbluebuttonRails with a single one

--- db/migrate/20170125185349_remove_start_time_from_bigbluebutton_meeting.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStartTimeFromBigbluebuttonMeeting < ActiveRecord::Migration
-  def change
-    remove_column :bigbluebutton_meetings, :start_time, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131162943) do
+ActiveRecord::Schema.define(version: 20170207190227) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
