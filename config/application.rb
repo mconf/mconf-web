e4847bba1bd9eb2c2432094c72b610a910d9c66f
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-03-31 16:24:15 -0300

    Add configs for themes, makes it easier to customize the design
    Initial work to support themes in Mconf-Web.

--- config/application.rb
+++ config/application.rb
@@ -99,9 +99,12 @@ module Mconf
     config.conf_scope_rooms = ENV['MCONF_CONFERENCE_SCOPE_ROOMS'] || 'conf'
 
     # Redis configurations. Defaults to a localhost instance.
-    config.redis_host     = ENV['MCONF_REDIS_HOST'] || 'localhost'
-    config.redis_port     = ENV['MCONF_REDIS_PORT'] || 6379
-    config.redis_db       = ENV['MCONF_REDIS_DB'] || 0
-    config.redis_password = ENV['MCONF_REDIS_PASSWORD'] || nil
+    config.redis_host      = ENV['MCONF_REDIS_HOST'] || 'localhost'
+    config.redis_port      = ENV['MCONF_REDIS_PORT'] || 6379
+    config.redis_db        = ENV['MCONF_REDIS_DB'] || 0
+    config.redis_password  = ENV['MCONF_REDIS_PASSWORD'] || nil
+
+    # Themes: set to the theme name if using any!
+    config.theme           = ENV['MCONF_THEME']
   end
 end
