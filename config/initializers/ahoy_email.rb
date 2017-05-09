9d5448f026125ea35d55170332f9a22be8fdcb82
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-05-09 17:06:44 -0300

    Fix AhoyEmail initialization after 92d9514

--- config/initializers/ahoy_email.rb
+++ config/initializers/ahoy_email.rb
@@ -1,2 +1,6 @@
 # Start and configure email tracking
-AhoyEmail.track utm_params: false, open: config.email_track_opened, click: config.email_track_clicked
+AhoyEmail.track(
+  utm_params: false,
+  open: Rails.application.config.email_track_opened,
+  click: Rails.application.config.email_track_clicked
+)
