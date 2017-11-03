9bdee8a9526188ed16724b8dafe44a69e93afaec
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-11-03 17:42:59 -0200

    Small fixes and social login improvements after merging new-design

--- spec/features/site/flag_local_auth_enabled_spec.rb
+++ spec/features/site/flag_local_auth_enabled_spec.rb
@@ -20,19 +20,27 @@ feature 'Behaviour of the flag Site#local_auth_enabled' do
     end
 
     context 'allows facebook authentication for normal users' do
-      before { Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-               visit root_path }
+      before {
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+        visit login_path
+      }
 
-      it { page.find(:css, "#facebook-login a").click
-           expect(current_path).to eq(my_home_path) }
+      it {
+        page.find(:css, ".btn-facebook").click
+        expect(current_path).to eq(my_home_path)
+      }
     end
 
     context 'allows google authentication for normal users' do
-      before { Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
-               visit root_path }
+      before {
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        visit login_path
+      }
 
-      it { page.find(:css, "#google-login a").click
-           expect(current_path).to eq(my_home_path) }
+      it {
+        page.find(:css, ".btn-google").click
+        expect(current_path).to eq(my_home_path)
+      }
     end
 
     context "shows the 'recover password' link in the login page" do
@@ -82,19 +90,27 @@ feature 'Behaviour of the flag Site#local_auth_enabled' do
     end
 
     context 'allows facebook authentication for normal users' do
-      before { Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-               visit root_path }
+      before {
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+        visit register_path
+      }
 
-      it { page.find(:css, "#facebook-login a").click
-           expect(current_path).to eq(my_home_path) }
+      it {
+        page.find(:css, ".btn-facebook").click
+        expect(current_path).to eq(my_home_path)
+      }
     end
 
     context 'allows google authentication for normal users' do
-      before { Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
-               visit root_path }
+      before {
+        Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+        visit register_path
+      }
 
-      it { page.find(:css, "#google-login a").click
-           expect(current_path).to eq(my_home_path) }
+      it {
+        page.find(:css, ".btn-google").click
+        expect(current_path).to eq(my_home_path)
+      }
     end
 
     context "accessing from the admin sign in page" do
