5d7d846f22bcff0bf51db4edc52f38ebe7b44313
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2017-12-07 14:30:33 -0200

    Fix 'room.attendees', it's now 'room.current_attendees'
    And make it a bit cleaner in spaces/webconference the list of people
    currently in the meeting.
    
    # Conflicts:
    #	app/controllers/spaces_controller.rb
    #	app/views/spaces/webconference.html.haml
    #	config/locales/en/mconf.yml
    #	config/locales/pt-br/mconf.yml

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -160,15 +160,7 @@ class SpacesController < InheritedResources::Base
   # there, the before_filters and other methods don't really match. It's more related to spaces then
   # to webconference rooms.
   def webconference
-    # FIXME: Temporarily matching users by name, should use the userID
-    @webconf_attendees = []
-    unless @webconf_room.attendees.nil?
-      @webconf_room.attendees.each do |attendee|
-        user = User.where(id: attendee.user_id).first
-        @webconf_attendees << user unless user.nil?
-      end
-      @webconf_attendees.uniq!
-    end
+    @webconf_attendees = @webconf_room.current_attendees
   end
 
   # Action used to show the recordings of a space
